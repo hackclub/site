6a775e07dd66a5624df6ae91f28d1546552e17e4
Lachlan Campbell <lachlan@hackclub.com>
2020-04-24 12:11:45 -0400

    Fix CORS

--- pages/api/join.js
+++ pages/api/join.js
@@ -1,21 +1,22 @@
 const AirtablePlus = require('airtable-plus')
 
 const joinTable = new AirtablePlus({
-	apiKey: 'keysWpSXVV7AGLLAg',
-	baseID: 'appaqcJtn33vb59Au',
-	tableName: 'Join Requests'
+  apiKey: 'keysWpSXVV7AGLLAg',
+  baseID: 'appaqcJtn33vb59Au',
+  tableName: 'Join Requests'
 })
 
 export default async (req, res) => {
-	if (req.method === 'POST') {
-		const data = JSON.parse(req.body)
-		console.log(data)
-		await joinTable.create({
-			'Full Name': data.name,
-			'Email Address': data.email,
-			'Student': data.teen ? true : false,
-			'Reason': data.reason
-		})
-		res.json({ status: 'success' })
-	}
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  if (req.method === 'POST') {
+    const data = JSON.parse(req.body)
+    console.log(data)
+    await joinTable.create({
+      'Full Name': data.name,
+      'Email Address': data.email,
+      Student: data.teen ? true : false,
+      Reason: data.reason
+    })
+    res.json({ status: 'success' })
+  }
 }
