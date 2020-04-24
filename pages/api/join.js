2d438bcc2b86779e491089769521023a98eaeb17
Matthew Stanciu <mattbstanciu@gmail.com>
2020-04-23 20:48:52 -0400

    sdfggds

--- pages/api/join.js
+++ pages/api/join.js
@@ -1,15 +1,17 @@
 const AirtablePlus = require('airtable-plus')
 
 const joinTable = new AirtablePlus({
-	apiKey: process.env.AIRTABLE_API_KEY,
+	apiKey: 'keysWpSXVV7AGLLAg',
 	baseID: 'appaqcJtn33vb59Au',
 	tableName: 'Join Requests'
 })
-console.log(joinTable)
 
 export default async (req, res) => {
 	if (req.method === 'POST') {
+		console.log('hi')
 		const data = JSON.parse(req.body)
+		console.log(data)
+		console.log(data.name)
 		await joinTable.create({
 			'Full Name': data.name,
 			'Email Address': data.email,
