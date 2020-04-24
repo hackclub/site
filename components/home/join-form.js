785771a598af73403e049be926ad347d94135204
Matthew Stanciu <mattbstanciu@gmail.com>
2020-04-24 11:10:31 -0400

    checkbox debug

--- components/home/join-form.js
+++ components/home/join-form.js
@@ -50,7 +50,7 @@ const JoinForm = () => {
             body: JSON.stringify({ name, email, teen, reason })
           })
             .then((r) => r.json())
-            .then((r) => setStatus(r.status))
+            .then((r) => { console.log(r); setStatus(r.status) })
             .catch((e) => console.error(e))
         }}
       >
