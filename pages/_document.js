def02cf4d53a2d1b516d003f326f94d1b8cc11b3
Lachlan Campbell <lachlan@hackclub.com>
2020-04-24 11:32:23 -0400

    Add Schema org data

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import * as snippet from '@segment/snippet'
-import { InitializeColorMode } from 'theme-ui'
+// import { InitializeColorMode } from 'theme-ui'
+
+const org = {
+  '@context': 'http://schema.org',
+  '@type': 'Organization',
+  name: 'Hack Club',
+  url: 'https://hackclub.com/',
+  logo: 'https://hackclub.com/social.png',
+  sameAs: [
+    'https://twitter.com/hackclub',
+    'https://github.com/hackclub',
+    'https://www.instagram.com/starthackclub',
+    'https://www.facebook.com/Hack-Club-741805665870458',
+    'https://www.youtube.com/channel/UCQzO0jpcRkP-9eWKMpJyB0w'
+  ],
+  contactPoint: [
+    {
+      '@type': 'ContactPoint',
+      email: 'team@hackclub.com',
+      contactType: 'customer support',
+      url: 'https://hackclub.com/'
+    }
+  ]
+}
 
 const {
   ANALYTICS_WRITE_KEY = '35oTlU4UqlhIN8VGYmBxAzyDdfzhcscw',
@@ -22,9 +45,15 @@ export default class extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="format-detection" content="telephone=no" />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(org) }}
+          />
+        </Head>
         <body>
-          <InitializeColorMode />
+          {/* <InitializeColorMode /> */}
           <Main />
           <NextScript />
           <script dangerouslySetInnerHTML={{ __html: renderSnippet() }} />
