fe3208da3c92e97cc0bcb0f2e7625ab2eb149269
Matthew Stanciu <mattbstanciu@gmail.com>
2020-04-24 10:30:18 -0400

    addd annallitics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,17 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import * as snippet from '@segment/snippet'
 import { InitializeColorMode } from 'theme-ui'
+
+const {
+  ANALYTICS_WRITE_KEY = '35oTlU4UqlhIN8VGYmBxAzyDdfzhcscw',
+  NODE_ENV = 'development'
+} = process.env
+
+const renderSnippet = () => {
+  const opts = { apiKey: ANALYTICS_WRITE_KEY, page: true }
+  if (NODE_ENV === 'development') return snippet.max(opts)
+  return snippet.min(opts)
+}
 
 export default class extends Document {
   static async getInitialProps(ctx) {
@@ -15,6 +27,7 @@ export default class extends Document {
           <InitializeColorMode />
           <Main />
           <NextScript />
+          <script dangerouslySetInnerHTML={{ __html: renderSnippet() }} />
         </body>
       </Html>
     )
