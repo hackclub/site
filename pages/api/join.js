fb854dc383d472b9dd83109d3313d832b45ee611
Matthew Stanciu <mattbstanciu@gmail.com>
2020-04-27 15:32:28 -0400

    Check for duplicates & empty records

--- pages/api/join.js
+++ pages/api/join.js
@@ -10,13 +10,35 @@ export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   if (req.method === 'POST') {
     const data = JSON.parse(req.body)
-    console.log(data)
-    await joinTable.create({
-      'Full Name': data.name,
-      'Email Address': data.email,
-      Student: data.teen,
-      Reason: data.reason
-    })
+
+    const exists = await recordExists(data.name)
+    const empty = await isEmpty(data)
+
+    if (!exists && !empty) {
+      await joinTable.create({
+        'Full Name': data.name,
+        'Email Address': data.email,
+        Student: data.teen,
+        Reason: data.reason
+      })
+    }
     res.json({ status: 'success' })
   }
+}
+
+async function recordExists(name) {
+  const exists = await joinTable.read({
+    filterByFormula: `{Full Name} = '${name}'`
+  })
+  return typeof exists !== 'undefined'
+}
+
+function isEmpty(jsonObject) {
+  let empty = true
+  Object.entries(jsonObject).map(key => {
+    if (key[1] !== '') {
+      empty = false
+    }
+  })
+  return empty
 }
