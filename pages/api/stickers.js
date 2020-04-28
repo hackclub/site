4c8b8ce1cfc2ca57fb2b228f95ea722ee4748c2f
Matthew Stanciu <mattbstanciu@gmail.com>
2020-04-27 23:38:55 -0400

    Use fetch again

--- pages/api/stickers.js
+++ pages/api/stickers.js
@@ -1,10 +1,5 @@
 const AirtablePlus = require('airtable-plus')
 
-const mailScenariosTable = new AirtablePlus({
-  apiKey: process.env.AIRTABLE_API_KEY,
-  baseID: 'apptEEFG5HTfGQE7h',
-  tableName: 'Mail Scenarios'
-})
 const peopleTable = new AirtablePlus({
   apiKey: process.env.AIRTABLE_API_KEY,
   basedID: 'apptEEFG5HTfGQE7h',
@@ -37,38 +32,30 @@ export default async (req, res) => {
         'Address (zip code)': data.zipCode
       })
       address = await addressesTable.create({
-<<<<<<< HEAD
         'Address (first line)': data.addressFirst,
         'Address (second line)': data.addressSecond,
         'Address (city)': data.city,
         'Address (state)': data.state,
         'Address (zip code)': data.zipCode,
         'Person': personRecord.id
-=======
-        'Street (First Line)': data.addressFirst,
-        'Street (Second Line': data.addressSecond,
-        'City': data.city,
-        'State/Province': data.state,
-        'Postal Code': data.zipCode,
-        'Country': data.country,
-        'Person': [personRecord.id]
->>>>>>> 1f2caee (Add formatted countries)
       })
     }
     else {
       address = personRecord[0]
     }
 
-    fetch(`${process.env.MAIL_MISSION_WEBHOOK}`, {
+    fetch(`https://hooks.zapier.com/hooks/catch/507705/o2dbufn/`, {
       method: 'POST',
-      body: {
+      body: JSON.stringify({
         'test': false,
         'scenarioRecordID': 'recNDwjb7Zr04Szix',
         'receiverAddressRecordID': address.id,
         'missionNotes': 'Requested via hackclub.com'
       }
     })
-      .then(r => r.json())
+      .then(r => { console.log(r.statusText); res.json({ status: 'success' }) })
       .then(r => res.json({ status: r.status }))
+      })
+    })
   }
 }
