af388d8d47be2148360048b56c34d4a4eb8ac230
Matthew Stanciu <mattbstanciu@gmail.com>
2020-04-27 23:45:24 -0400

    Small edits

--- pages/api/stickers.js
+++ pages/api/stickers.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 const AirtablePlus = require('airtable-plus')
+=======
+import fetch from 'isomorphic-unfetch'
+import AirtablePlus from 'airtable-plus'
+>>>>>>> e36e684 (Small edits)
 
 const peopleTable = new AirtablePlus({
   apiKey: process.env.AIRTABLE_API_KEY,
@@ -12,6 +17,7 @@ const addressesTable = new AirtablePlus({
 })
 
 export default async (req, res) => {
+<<<<<<< HEAD
   if (req.method === 'POST') {
     const data = JSON.parse(body)
     let address
@@ -59,3 +65,48 @@ export default async (req, res) => {
     })
   }
 }
+=======
+  if (req.method !== 'POST') return
+  const data = JSON.parse(req.body)
+  let address
+
+  // fetch person record
+  let personRecord = await peopleTable.read({
+    filterByFormula: `{Email} = '${data.email}'`,
+    maxRecords: 1
+  }).catch(err => console.log(err))
+
+  if (personRecord.length === 0) {
+    personRecord = await peopleTable.create({
+      'Full Name': data.name,
+      'Email': data.email
+    })
+    address = await addressesTable.create({
+      'Street (First Line)': data.addressFirst,
+      'Street (Second Line': data.addressSecond,
+      'City': data.city,
+      'State/Province': data.state,
+      'Postal Code': data.zipCode,
+      'Country': data.country,
+      'Person': [personRecord.id]
+    })
+  }
+  else {
+    address = (await addressesTable.read({
+      filterByFormula: `{Person ID} = '${personRecord[0].fields['ID']}'`
+    }))[0]
+  }
+
+  fetch(`https://hooks.zapier.com/hooks/catch/507705/o2dbufn/`, {
+    method: 'POST',
+    body: JSON.stringify({
+      'test': false,
+      'scenarioRecordID': 'recNDwjb7Zr04Szix',
+      'receiverAddressRecordID': address.id,
+      'missionNotes': 'Requested via hackclub.com'
+    })
+  })
+    .then(r => { console.log(r.statusText); res.json({ status: 'success' }) })
+    .catch(err => console.log(err))
+}
+>>>>>>> e36e684 (Small edits)
