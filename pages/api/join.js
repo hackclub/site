176a686ee04d91e3a65e21a43cecddb05ef76301
Lachlan Campbell <lachlan@hackclub.com>
2020-05-14 05:03:01 +0000

    Disable CORS on join endpoint

--- pages/api/join.js
+++ pages/api/join.js
@@ -1,5 +1,6 @@
 import AirtablePlus from 'airtable-plus'
 import fetch from 'isomorphic-unfetch'
+/*
 import Cors from 'cors'
 import initMiddleware from '../../lib/init-middleware'
 
@@ -8,6 +9,7 @@ const cors = initMiddleware(
     methods: ['GET', 'POST']
   })
 )
+*/
 
 const joinTable = new AirtablePlus({
   apiKey: process.env.AIRTABLE_API_KEY,
@@ -18,83 +20,82 @@ const joinTable = new AirtablePlus({
 export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', '*')
-  await cors(req, res)
-  if (req.method === 'POST') {
-    let data
-    try {
-      data = JSON.parse(req.body)
-    } catch {
-      data = req.body
-    }
-    console.log(data)
-
-    const exists = await isDuplicate(data.name, data.email, data.reason)
-    const empty = await isEmpty(data)
-
-    if (!exists && !empty) {
-      await joinTable.create({
-        'Full Name': data.name,
-        'Email Address': data.email,
-        Student: data.teen,
-        Reason: data.reason
-      })
-      if (data.teen) {
-        let postData = {
-          channel: 'G0132DNFE7J', // G0147KPNHU0
-          blocks: [
-            {
-              "type": "section",
-              "text": {
-                "type": "mrkdwn",
-                "text": "Someone just requested to join the Slack."
-              }
-            },
-            {
-              "type": "section",
-              "text": {
-                "type": "mrkdwn",
-                "text": `*Full name:* ${data.name}\n*Email:* ${data.email}\n*Student:* ${data.teen ? 'true' : 'false'}\n*Reason:* ${data.reason}`
-              }
-            },
-            {
-              "type": "actions",
-              "elements": [
-                {
-                  "type": "button",
-                  "text": {
-                    "type": "plain_text",
-                    "emoji": true,
-                    "text": "Send Invitation"
-                  },
-                  "style": "primary",
-                  "action_id": "invite_member"
-                },
-                {
-                  "type": "button",
-                  "text": {
-                    "type": "plain_text",
-                    "emoji": true,
-                    "text": "Deny"
-                  },
-                  "style": "danger",
-                  "action_id": "deny"
-                }
-              ]
-            }
-          ]
-        }
-        await fetch('https://slack.com/api/chat.postMessage', {
-          method: 'post',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`
-          },
-          body: JSON.stringify(postData)
-        }).catch(err => console.error(err))
-      }
-    }
-    res.status(200).json({ status: 'success' })
+  // await cors(req, res)
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed, use POST' })
   }
+
+  let data = JSON.parse(req?.body || '{}')
+  const exists = await isDuplicate(data.name, data.email, data.reason)
+  const empty = await isEmpty(data)
+
+  if (!exists && !empty) {
+    await joinTable.create({
+      'Full Name': data.name,
+      'Email Address': data.email,
+      Student: data.teen,
+      Reason: data.reason
+    })
+    if (data.teen) {
+      let postData = {
+        channel: 'G0132DNFE7J', // G0147KPNHU0
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: 'Someone just requested to join the Slack.'
+            }
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `*Full name:* ${data.name}\n*Email:* ${
+                data.email
+              }\n*Student:* ${data.teen ? 'true' : 'false'}\n*Reason:* ${
+                data.reason
+              }`
+            }
+          },
+          {
+            type: 'actions',
+            elements: [
+              {
+                type: 'button',
+                text: {
+                  type: 'plain_text',
+                  emoji: true,
+                  text: 'Send Invitation'
+                },
+                style: 'primary',
+                action_id: 'invite_member'
+              },
+              {
+                type: 'button',
+                text: {
+                  type: 'plain_text',
+                  emoji: true,
+                  text: 'Deny'
+                },
+                style: 'danger',
+                action_id: 'deny'
+              }
+            ]
+          }
+        ]
+      }
+      await fetch('https://slack.com/api/chat.postMessage', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
+        },
+        body: JSON.stringify(postData)
+      }).catch(err => console.error(err))
+    }
+  }
+  res.status(200).json({ status: 'success' })
 }
 
 async function isDuplicate(name, email, reason) {
