de79c62a42422acf1162cba12448d0ab83f03d20
Lachlan Campbell <lachlan@hackclub.com>
2020-05-20 16:15:39 +0000

    Switch to @hackclub/meta 1.0.0

--- pages/elon.js
+++ pages/elon.js
@@ -4,37 +4,23 @@ import {
   BaseStyles,
   Box,
   Button,
-<<<<<<< HEAD
-  Card,
-=======
->>>>>>> 6bc33b9dd2124d06c7f84ca0ae18aa1395f5dbf5
   Container,
   Flex,
   Grid,
   Heading,
-<<<<<<< HEAD
   Image,
   Link,
   Text
 } from 'theme-ui'
 import { styled } from '@emotion/styled'
-=======
-  Text
-} from 'theme-ui'
->>>>>>> 6bc33b9dd2124d06c7f84ca0ae18aa1395f5dbf5
 import Head from 'next/head'
 import NextLink from 'next/link'
-import Meta from '../components/meta'
+import Meta from '@hackclub/meta'
 import Nav from '../components/nav'
 import Icon from '../components/icon'
 import ForceTheme from '../components/force-theme'
 import Footer from '../components/footer'
-<<<<<<< HEAD
-import Signature from '../components/signature'
-import ElonCopy from '../components/elon.mdx'
-=======
 import ElonCopy from '../components/elon/copy.mdx'
->>>>>>> 6bc33b9dd2124d06c7f84ca0ae18aa1395f5dbf5
 
 export default () => (
   <>
@@ -42,11 +28,7 @@ export default () => (
       <Meta
         title="Elon Musk announcement"
         description="We’re thrilled to announce Elon Musk has donated $500k to Hack Club, a global nonprofit network of high school hackers & coding clubs."
-<<<<<<< HEAD
-        image="https://cdn.glitch.com/a7605379-7582-4aac-8f44-45bbdfca0cfa%2F94239BAB-4660-4387-9A07-8F0585D584E8.jpeg?v=1589920219237"
-=======
         image="https://assets.hackclub.com/log/HC-500k@1080w.png"
->>>>>>> 6bc33b9dd2124d06c7f84ca0ae18aa1395f5dbf5
       />
     </Head>
     <ForceTheme theme="light" />
@@ -57,12 +39,7 @@ export default () => (
         pt: [5, 6],
         pb: [4, 5],
         bg: 'rgb(104, 41, 205)',
-<<<<<<< HEAD
-        backgroundImage: theme =>
-          theme.util.gradient('rgb(207, 45, 228)', 'rgb(40, 59, 205)')
-=======
         backgroundImage: theme => theme.util.gradient('yellow', 'green')
->>>>>>> 6bc33b9dd2124d06c7f84ca0ae18aa1395f5dbf5
       }}
     >
       <Container sx={{ textAlign: 'center', color: 'white' }}>
@@ -134,7 +111,9 @@ export default () => (
           />
           Zach Latta, founder
         </Badge>
-        <Badge variant="pill" px={3}>May 15, 2020</Badge>
+        <Badge variant="pill" px={3}>
+          May 15, 2020
+        </Badge>
       </Flex>
       <ElonCopy />
     </Container>
