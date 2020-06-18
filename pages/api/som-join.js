fcc7c0e830ea00d2ffaaf35ed926e619641547f9
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-18 10:28:06 -0400

    Fix channels?? ?

--- pages/api/som-join.js
+++ pages/api/som-join.js
@@ -43,6 +43,7 @@ export default async (req, res) => {
     `token=${process.env.SLACK_LEGACY_TOKEN}`,
     `real_name=${data.name}`,
     'restricted=true',
+    'channels=\'\''
     'resend=true'
   ].join('&')
   const url = `https://slack.com/api/users.admin.invite?${params}`
