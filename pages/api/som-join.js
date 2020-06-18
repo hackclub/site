fc5b5e3f05ada50cda5527dd00ff714fba9d4092
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-18 14:14:01 -0400

    Change default channel to #som-welcome

--- pages/api/som-join.js
+++ pages/api/som-join.js
@@ -43,7 +43,7 @@ export default async (req, res) => {
     `token=${process.env.SLACK_LEGACY_TOKEN}`,
     `real_name=${data.name}`,
     'restricted=true',
-    `channels=C015LQDP2Q2`,
+    `channels=C015MKW1A3D`,
     'resend=true'
   ].join('&')
   const url = `https://slack.com/api/users.admin.invite?${params}`
