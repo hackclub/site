c3a3848664a99bbe18a8c9ec8cddd6e9a10cbf01
Max Wofford <max@maxwofford.com>
2020-06-18 08:21:09 +0000

    Attempt to fix som form

--- pages/api/som-join.js
+++ pages/api/som-join.js
@@ -1,6 +1,5 @@
 import AirtablePlus from 'airtable-plus'
 import fetch from 'isomorphic-unfetch'
-import qs from 'qs'
 
 const joinTable = new AirtablePlus({
   apiKey: process.env.AIRTABLE_API_KEY,
@@ -35,21 +34,17 @@ export default async (req, res) => {
   // This is a private api method found in https://github.com/ErikKalkoken/slackApiDoc/blob/master/users.admin.invite.md
   // I only got a successful response by putting all the args in URL params
   // Giving JSON body DID NOT WORK when testing locally
-  const args = qs.stringify({
-    email: data.email,
-    channels: 'C015ZDB0GRF,C015LQDP2Q2,C01504DCLVD',
-    real_name: data.name,
-    restricted: true,
-    resend: true,
-  })
 
-  const slackReq = await fetch(`https://slack.com/api/users.admin.invite?${args}`, {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.SLACK_LEGACY_TOKEN}`
-    }
-  }).then(r => r.json())
+  const params = [
+    `email=${data.email}`,
+    `token=${process.env.SLACK_LEGACY_TOKEN}`,
+    `real_name=${data.name}`,
+    "channels=C015ZDB0GRF,C015LQDP2Q2,C01504DCLVD",
+    "restricted=true",
+    "resend=true",
+  ].join('&')
+  const url = `https://slack.com/api/users.admin.invite?${params}`
+  const slackReq = await fetch(url, { method: 'POST', }).then(r => r.json())
 
   res.json({ status: 'success', slackRes: slackReq })
 }
