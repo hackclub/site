9ae0c45f43823679eae211160e9844699dbd06ac
Lachlan Campbell <lachlan@hackclub.com>
2020-06-26 18:09:29 +0000

    Add Fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import NextApp from 'next/app'
 import Head from 'next/head'
 
@@ -6,15 +7,28 @@ import Meta from '@hackclub/meta'
 import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '../lib/theme'
 import { ThemeProvider } from 'theme-ui'
+import * as Fathom from 'fathom-client'
 
-export default class App extends NextApp {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <ThemeProvider theme={theme}>
-        <Meta as={Head} />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    )
-  }
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load('OGIMJEFA', {
+      includedDomains: ['hackclub.com', 'v3.hackclub.com']
+    })
+    const onRouteChangeComplete = () => Fathom.trackPageview()
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Meta as={Head} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+export default App
