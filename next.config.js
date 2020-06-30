d619075b07cfc17a8283e981a40d8576697534e7
Lachlan Campbell <lachlan@hackclub.com>
2020-06-30 17:35:45 +0000

    Enable Next trailing slash

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 module.exports = withMDX({
+  experimental: { trailingSlash: true },
   pageExtensions: ['js', 'jsx', 'mdx'],
   assetPrefix: isProd ? 'https://v3.hackclub.com' : ''
 })
