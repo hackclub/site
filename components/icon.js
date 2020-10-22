488062cd0f12b595e81998e1be9465684f9c49fc
Lachlan Campbell <lachlan@hackclub.com>
2020-10-22 16:33:26 -0400

    Remove Theme UI JSX

--- components/icon.js
+++ components/icon.js
@@ -1,5 +1,4 @@
-/** @jsx jsx */
-import { jsx } from 'theme-ui'
+import { Box } from 'theme-ui'
 import Icon from '@hackclub/icons'
 
-export default (props) => <Icon {...props} />
+export default (props) => <Box as={Icon} {...props} />
