018d7bc0fe8c8afdc11d0bcac3b2b9e9f7d30039
Matthew Stanciu <mattbstanciu@gmail.com>
2020-10-26 14:55:49 -0400

    Add dogs endpoint

---
+++ pages/api/channels/dogs.js
@@ -0,0 +1,10 @@
+export default async (req, res) => {
+  // returns a url_private for the latest image posted in #dogs
+
+  const fileList = await fetch(`https://slack.com/api/files.list?token=${process.env.SCRAPPY_TOKEN}&channel=CDJV1CXC2&count=1`)
+    .then(r => r.json())
+    .catch(err => res.status(400).send(err))
+
+  const file = fileList.files[0].url_private
+  res.status(200).send(file)
+}
