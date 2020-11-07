7589efb05dce0afb298df7c1f3f936c4bb8af011
Lachlan Campbell <lachlan@hackclub.com>
2020-11-07 02:19:25 +0000

    Refresh AMAs page with next/image

--- components/background-image.js
+++ components/background-image.js
@@ -1,6 +1,13 @@
 import { Box } from 'theme-ui'
 import Image from 'next/image'
 
+/*
+ * Use this component inside a container with CSS:
+ * `position: relative; overflow: hidden;`
+ * then pass width/height/alt/src as usual
+ * (you can pass `gradient` valueless to avoid gradient)
+ */
+
 const BGImg = ({
   gradient = 'linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.5))',
   ...props
