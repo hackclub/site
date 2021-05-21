870986810b52368cce91fe77d9e46c484914f518
zach latta <zach@zachlatta.com>
2021-05-21 12:49:45 -0400

    Add Bank Ops Assistant JD to website (#127)

---
+++ pages/jobs/bank-ops-assistant.js
@@ -0,0 +1,47 @@
+import { BaseStyles, Box, Container, Heading, Text } from 'theme-ui'
+import Head from 'next/head'
+import Nav from '../../components/nav'
+import Meta from '@hackclub/meta'
+import JobDescription from '../../components/jobs/bank-ops-assistant/jd.mdx'
+import ForceTheme from '../../components/force-theme'
+
+export default () => (
+  <>
+    <Meta
+      as={Head}
+      title="Bank Operations Assistant"
+      description="Hack Club is a hiring a Bank Operations Assistant as the 8th full-time member of our team in Burlington, Vermont."
+      image="https://workshop-cards.hackclub.com/Bank Ops Assistant @ Hack Club.png?fontSize=175px&brand=HQ"
+    />
+    <ForceTheme theme="light" />
+    <Nav />
+    <Box
+      as="section"
+      sx={{
+        pt: [5, 6],
+        pb: [4, 5],
+        backgroundImage: theme => theme.util.gx('yellow', 'red')
+      }}
+    >
+      <Container sx={{ textAlign: 'center', color: 'white' }}>
+        <Heading as="h1" variant="title">
+          Bank Operations Assistant
+        </Heading>
+        <Text variant="headline">
+          New job open as of May 21st, 2021.
+        </Text>
+      </Container>
+    </Box>
+    <Container
+      as={BaseStyles}
+      variant="copy"
+      sx={{
+        pt: [3, 4],
+        pb: [4, 5],
+        fontSize: [2, 3]
+      }}
+    >
+      <JobDescription />
+    </Container>
+  </>
+)
