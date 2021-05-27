d75113aa26ec4a46f15cb3cac8eb177e5b50bfe4
Zach Latta <zach@zachlatta.com>
2021-05-27 13:27:35 -0400

    Bank Operations Assistant -> Bank Operations Associate

--- pages/jobs/bank-ops-associate.js
+++ pages/jobs/bank-ops-associate.js
@@ -2,16 +2,16 @@ import { BaseStyles, Box, Container, Heading, Text } from 'theme-ui'
 import Head from 'next/head'
 import Nav from '../../components/nav'
 import Meta from '@hackclub/meta'
-import JobDescription from '../../components/jobs/bank-ops-assistant/jd.mdx'
+import JobDescription from '../../components/jobs/bank-ops-associate/jd.mdx'
 import ForceTheme from '../../components/force-theme'
 
 export default () => (
   <>
     <Meta
       as={Head}
-      title="Bank Operations Assistant"
-      description="Hack Club is a hiring a Bank Operations Assistant as the 8th full-time member of our team in Burlington, Vermont."
-      image="https://workshop-cards.hackclub.com/Bank Ops Assistant @ Hack Club.png?fontSize=175px&brand=HQ"
+      title="Bank Operations Associate"
+      description="Hack Club is a hiring a Bank Operations Associate as the 8th full-time member of our team in Burlington, Vermont."
+      image="https://workshop-cards.hackclub.com/Bank Ops Associate @ Hack Club.png?fontSize=175px&brand=HQ"
     />
     <ForceTheme theme="light" />
     <Nav />
@@ -25,7 +25,7 @@ export default () => (
     >
       <Container sx={{ textAlign: 'center', color: 'white' }}>
         <Heading as="h1" variant="title">
-          Bank Operations Assistant
+          Bank Operations Associate
         </Heading>
         <Text variant="headline">
           New job open as of May 21st, 2021.
