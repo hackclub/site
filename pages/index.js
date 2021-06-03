963bb1307a2e5ec152498520cb1d6bd640194f68
Zachary Fogg <zach.fogg@gmail.com>
2021-06-03 09:57:21 -0600

    copy fix

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ const Feature = ({ icon, color, name, desc, children, ...props }) => (
 
 const BANNER_CAPTIONS = [
   "A hackathon on a train, from sea to shining sea.",
-  "Hack your way across America by railroad, from coast to coast.",
+  "Hack your way across America by rail, from coast to coast.",
 //   "Coast-to-coast hackathon on a train? Let's break a world record!",
 ]
 
