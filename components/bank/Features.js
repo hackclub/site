e6ec40b31c66b21eed2b72c9cdb26baf5c90cfea
eilla1 <git@ella.cx>
2021-08-03 22:27:14 -0700

    Features section

--- components/bank/Features.js
+++ components/bank/Features.js
@@ -1,30 +1,121 @@
-import { Box, Heading, Text } from 'theme-ui'
+import { Box, Heading, Link, Text } from 'theme-ui'
 import Icon from '../icon'
 
 export default function Features() {
   return (
     <>
-      <Box>
-        <Heading variant="title">
-          A full-stack toolkit for organizing anything.
-        </Heading>
-        <Text
-          variant="lead"
-          fontSize={[3, 4]}
-          sx={{ color: 'muted', maxWidth: '48', marginX: '0' }}
-        >
-          Invoice sponsors, issue debit cards to your team, and view history.
-          <br />
-          Ongoing support so you can focus on organizing, not the paperwork.
-        </Text>
-      </Box>
-      <Box>
+      <Box
+        style={{
+          display: 'grid',
+          gridGap: 8,
+          gridTemplateColumns: 'repeat(3, 1fr)'
+        }}
+      >
+        <Box>
+          <Heading variant="title">
+            A full-stack toolkit for organizing anything.
+          </Heading>
+          <Text
+            variant="lead"
+            fontSize={[3, 4]}
+            sx={{ color: 'muted', maxWidth: '48', marginX: '0' }}
+          >
+            Invoice sponsors, issue debit cards to your team, and view history.
+            <br />
+            Ongoing support so you can focus on organizing, not the paperwork.
+          </Text>
+        </Box>
+
+        <Box>
+          <Module
+            icon="bank-account"
+            name="Bank account"
+            body="Backed by Silicon Valley Bank with a custom, beautiful dashboard."
+          />
+          <ModuleDetails>
+            <Document
+              name="501(c)(3) nonprofit status"
+              cost="Become part of Hack Club's legal entity, getting the benefits of our tax status."
+            />
+            <Document
+              name="Tax filings (990, end-of-year)"
+              cost="We handle all filings with the IRS, so you can focus on your event, not hiring CPAs."
+            />
+          </ModuleDetails>
+        </Box>
+        <Laptop
+          href="https://bank.hackclub.com/hackpenn"
+          title="See Hack Pennsylvania’s finances in public"
+        />
         <Module
-          icon="bank-account"
-          name="Bank account"
-          body="Backed by Silicon Valley Bank with a custom, beautiful dashboard."
+          icon="card"
+          name="Debit cards"
+          body={
+            <>
+              Issue physical debit cards to all your teammates, backed by{' '}
+              <Link
+                href="https://stripe.com/issuing"
+                color="smoke"
+                hoverline
+                target="_blank"
+              >
+                Stripe
+              </Link>
+              .
+            </>
+          }
+        />
+        <Module
+          icon="analytics"
+          name="Balance &amp; history"
+          body="Check real-time account balance + transaction history online anytime."
+        />
+        <Module
+          icon="payment"
+          name="Built-in invoicing"
+          body="Accept sponsor payments with low negotiated rates from Stripe."
+        />
+        <Module
+          icon="docs"
+          name="Pre-written forms"
+          body="Download liability + photo forms custom written by expert lawyers."
+        />
+        <Module
+          icon="explore"
+          name="Transparency Mode"
+          body="If you’d like, show your finances on public pages for full transparency."
+        />
+        <Module
+          icon="google"
+          name="Google Workspace"
+          body="Get instant, free accounts for your team (like joy@hackpenn.com)."
+        />
+        <Module
+          icon="support"
+          name="Support anytime"
+          body="We’ll never leave you hanging with best-effort 24hr response time."
         />
       </Box>
+      <Text
+        variant="lead"
+        sx={{
+          maxWidth: 28,
+          color: 'muted',
+          fontSize: 3,
+          textAlign: 'center',
+          marginTop: [4, 5]
+        }}
+      >
+        Have more questions? <br /> Check out the{' '}
+        <Link
+          href="https://bank.hackclub.com/faq"
+          target="_blank"
+          rel="noreferrer"
+          hoverline
+        >
+          Hack Club Bank FAQ
+        </Link>
+      </Text>
     </>
   )
 }
@@ -36,7 +127,7 @@ function Module({ icon, name, body }) {
         size={48}
         mr={3}
         glyph={icon}
-        color="primary"
+        color="red"
         style={{ flexShrink: 0 }}
       />
       <Box>
@@ -67,16 +158,17 @@ function ModuleDetails({ children }) {
 
 function Document({ name, cost }) {
   return (
-    <Box>
+    <Box sx={{ display: 'flex' }}>
       <Icon
         size={28}
         mr={1}
         glyph="payment"
-        color="success"
+        color="green"
         style={{ flexShrink: 0 }}
       />
-      <Box>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
         <Text fontSize={2} children={name} />
+
         {cost && (
           <Text
             fontSize={1}
@@ -87,5 +179,24 @@ function Document({ name, cost }) {
         )}
       </Box>
     </Box>
+  )
+}
+
+function Laptop({ href, title }) {
+  return (
+    <a href={href} title={title}>
+      <Box
+        style={{
+          display: 'block',
+          width: '100%',
+          height: '100%',
+          minHeight: '16rem',
+          backgroundSize: 'auto 115%',
+          backgroundImage: "url('/bank/laptop-light.png')",
+          backgroundPosition: 'center top',
+          backgroundRepeat: 'no-repeat'
+        }}
+      ></Box>
+    </a>
   )
 }
--- components/bank/Testimonials.js
+++ components/bank/Testimonials.js
@@ -1,9 +1,87 @@
-import { Box } from 'theme-ui'
+import { Box, Avatar, Button, Image, Text, Heading } from 'theme-ui'
+import { Slide } from 'react-reveal'
+import Stat from '../stat'
+
+const events = [
+  {
+    transparency: 'hackpenn',
+    name: 'Hack Pennsylvania',
+    location: 'State College, PA',
+    organizer: 'Joy Liu',
+    budget: 15,
+    attendees: 115,
+    testimonial:
+      'For me, Hack Club Bank unlocked organizing hackathons. Even after as a club leader, raising money seemed insurmountable. Bank directly enabled organizing events in my community with event bank accounts & a supportive community. I couldn’t recommend it more highly.'
+  },
+  {
+    name: 'Teenhacks LI',
+    location: 'Long Island, NY',
+    organizer: 'Wesley Pergament',
+    budget: 35,
+    attendees: 300,
+    testimonial:
+      'For our hackathon, Hack Club Bank has given us the tools to make sure our organization is professional with sponsors. Bank and their team have created an easily manageable resource to make sure any event is run successfully. We would highly recommend any organization be a part of the Hack Club ecosystem.'
+  },
+  {
+    name: 'Los Altos Hacks',
+    location: 'Sunnyvale, CA',
+    organizer: 'Jamsheed Mistri',
+    budget: 30,
+    attendees: 350,
+    testimonial:
+      'Hack Club Bank has made it incredibly easy to handle our event’s funds and has provided countless tools to increase our productivity. With Bank, I can focus on making the event the best it can be.'
+  },
+  {
+    name: 'SLO Hacks',
+    location: 'San Luis Obispo, CA',
+    organizer: 'Selynna Sun',
+    budget: 50,
+    attendees: 300,
+    testimonial:
+      'Hack Club Bank significantly improved the fiscal sponsorship process for SLO Hacks, through a beautifully-designed platform full of useful features, in addition to a responsive team addressed our questions as quickly as possible.'
+  },
+  {
+    name: 'MAHacks',
+    location: 'Boston, MA',
+    organizer: 'Kat Huang',
+    budget: 1.5,
+    attendees: 70,
+    testimonial:
+      'Hack Club Bank removed the barriers to starting fundraising for MAHacks. In mere days, vs months of nonprofit paperwork, Bank enabled my team to invoice sponsors professionally and manage our finances on a clear, up-to-date dashboard. I highly recommend using Bank & joining the Hack Club community.'
+  },
+  {
+    name: 'DV Hacks',
+    location: 'Santa Clara, CA',
+    organizer: 'Khushi Wadhwa',
+    budget: 12,
+    attendees: 150,
+    testimonial:
+      'Hack Club Bank is an essential platform for any hackathon organizer! It made us look both professional and credible in front of our sponsors and it relieved us of legal/financial burdens. Hack Club Bank was there for us every step of the way and for a first-year hackathon, that support was priceless.'
+  }
+]
 
 export default function Testimonials() {
   return (
     <>
-      <Box>Testimonials</Box>
+      <Box>
+        <Box>
+          <Heading variant="title">
+            The best events across the country run on Bank.
+          </Heading>
+          <Text variant="lead" color="muted">
+            Everywhere from Philadelphia to Phoenix to Portland,
+            Hack&nbsp;Club&nbsp;Bank powers events of all sizes.
+          </Text>
+        </Box>
+      </Box>
     </>
+  )
+}
+
+function Event() {
+  return (
+    <Slide bottom>
+      <Box></Box>
+    </Slide>
   )
 }
