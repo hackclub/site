c8b3261f18aac74a9e2f20765fb74e1f3fa95e9d
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-05 17:06:10 +0800

    Update nav.js

--- components/nav.js
+++ components/nav.js
@@ -148,7 +148,9 @@ const Navigation = props => (
     <Link href="https://workshops.hackclub.com/" children="Workshops" />
     <Link href="https://scrapbook.hackclub.com/" children="Scrapbook" />
     <Link href="https://hackclub.com/bank/" children="Bank" />
-    <Link href="https://hackclub.com/donate/" children="Donate" />
+    <NextLink href="/donate" passHref>
+      <Link children="Donate" />
+    </NextLink>
   </NavBar>
 )
 
