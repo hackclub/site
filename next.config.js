e22ed9256848097c7f9b3c0df6852a1956f9fe9c
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-07 23:56:18 +0800

    Revert "Leo copy 2021 club launch (#162)" (#163)
    This reverts commit b3e7bfb73e1d62fcb48c3a3bd8e743e7fc2667e5.

--- next.config.js
+++ next.config.js
@@ -7,9 +7,7 @@ module.exports = withMDX({
       'hackclub.com',
       'dl.airtable.com',
       'emoji.slack-edge.com',
-      'cdn.glitch.com',
-      'github.com',
-      'localhost'
+      'cdn.glitch.com'
     ]
   },
   webpack: (config, { isServer }) => {
