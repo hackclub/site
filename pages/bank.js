bc72d8cbe39e582aea960096dca7b5da7d49854d
eilla1 <git@ella.cx>
2021-08-14 15:51:41 -0700

    Add selection color

--- pages/bank.js
+++ pages/bank.js
@@ -7,7 +7,8 @@ import {
   Heading,
   Image,
   Text,
-  Grid
+  Grid,
+  theme
 } from 'theme-ui'
 import ForceTheme from '../components/force-theme'
 import Meta from '@hackclub/meta'
@@ -21,20 +22,18 @@ import Testimonials from '../components/bank/Testimonials'
 import Everything from '../components/bank/Everything'
 import Start from '../components/bank/Start'
 
+const styles = `
+  ::selection {
+    background-color: #e42d42;
+    color: 'white';
+    text-shadow: none;
+  }
+`
+
 export default function Bank() {
   return (
     <>
-      <Box
-        as="main"
-        key="main"
-        sx={{
-          '::selection': {
-            backgroundColor: 'red',
-            color: 'white',
-            textShadow: 'none'
-          }
-        }}
-      >
+      <Box as="main" key="main">
         <Nav dark />
         <ForceTheme theme="dark" />
         <Meta
@@ -42,6 +41,7 @@ export default function Bank() {
           title="Bank"
           description="Hack Club Bank provides a 501(c)(3) status-backed bank account optimized for high school hackathons including invoicing, debit cards, check sending, pre-written legal forms, automated tax filing, and transparent finances. Get fiscal sponsorship designed to help you run great events."
         />
+        <style children={styles} />
         <Box>
           <Landing />
           <Features />
