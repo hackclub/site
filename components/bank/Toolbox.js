b576f69364a12fbc08f834e012e360d92c9f9b38
eilla1 <git@ella.cx>
2021-08-26 23:39:13 -0700

    Toolbox item cards

--- components/bank/Toolbox.js
+++ components/bank/Toolbox.js
@@ -13,9 +13,55 @@ import {
 import { Slide } from 'react-reveal'
 import Icon from '@hackclub/icons'
 
+const perks = [
+  {
+    icon: 'slack-fill',
+    color: 'purple',
+    name: 'Founders Community',
+    desc: `<>
+      In our{' '}
+      <Link href="/slack" passHref>
+        <a>Slack community</a>
+      </Link>
+      , you’ll join a private space for founders to ask questions, chat,
+      share advice, and events.
+    </>`
+  },
+  {
+    icon: 'flag',
+    color: 'primary',
+    name: 'PVSA',
+    desc: 'Earn national recognition for volunteer service through the Presidential Volunteer Service Award'
+  },
+  {
+    icon: 'sam',
+    color: 'blue',
+    name: 'Zoom Pro',
+    desc: 'Stream events virtually and host meetings with Zoom Pro through slash-z'
+  },
+  {
+    icon: 'sticker',
+    color: 'orange',
+    name: 'Sticker Mule',
+    desc: 'Get up to $400 of Sticker Mule credits  for custom stickers that kick ass'
+  },
+  {
+    icon: 'email',
+    color: 'secondary',
+    name: 'Repl.it Hacker',
+    desc: 'Free Hacker Plans for your  team on repl.it, an amazing web IDE'
+  },
+  {
+    icon: 'email',
+    color: 'slate',
+    name: 'Sendy',
+    desc: 'Access to a straight-forward, reliable mass-emailing software for sending newsletters'
+  }
+]
+
 export default function Toolbox() {
   return (
-    <Box sx={{ pt: 4}}>
+    <Box sx={{ pt: 4 }}>
       <Container>
         <Text variant="heading" sx={{ fontSize: 50 }}>
           The Founder's Toolbox.
@@ -26,129 +72,107 @@ export default function Toolbox() {
           organization of your dreams.
         </Text>
       </Container>
-      <Grid columns={1} sx={{ mx: [3, null, 7]}}>
-      <Item
-            icon="slack-fill"
-            color="#5d114c"
-            name="Chats with 100s of club leaders"
-            desc={
-              <>
-                In our{' '}
-                <Link href="/slack" passHref>
-                  <a>Slack community</a>
-                </Link>
-                , you’ll join a private space for founders to ask
-                questions, chat, share advice, and events.
-              </>
-            }
-          />
+      <Grid columns={[1, null, 2]} sx={{ mx: [2, null, 4] }}>
+        <Item
+          icon="slack-fill"
+          color="purple"
+          name="Founders community"
+          desc={
+            <>
+              In our{' '}
+              <Link href="/slack" passHref>
+                <a>Slack community</a>
+              </Link>
+              , you’ll join a private space for founders to ask questions, chat,
+              share advice, and events.
+            </>
+          }
+        />
+        {perks.map(perk => {
+          return (
+            <Item
+              icon={perk.icon}
+              color={perk.color}
+              name={perk.name}
+              desc={perk.desc}
+              key={perk.name}
+            />
+          )
+        })}
+        {/* <Item
+          icon="flag"
+          color="primary"
+          name="PVSA"
+          desc="Earn national recognition for volunteer service through the Presidential Volunteer Service Award"
+        />
+        <Item
+          icon="sam"
+          color="blue"
+          name="Zoom Pro"
+          desc="Stream events virtually and host meetings with Zoom Pro through slash-z"
+        /> */}
       </Grid>
-
     </Box>
   )
 }
 
 function Item({ icon, color, name, desc, children, sx, ...props }) {
   return (
-    <Box
-    sx={{
-      display: 'grid',
-      gridGap: [0, 4],
-      gridTemplateColumns: [null, 'auto 1fr'],
-      alignItems: 'start',
-      justifyContent: 'start',
-      bg: 'darkless',
-      p: [3, 4],
-      mt: [1, 1],
-      borderRadius: 'extra',
-      span: { transform: 'none', width: 'min-intrinsic' },
-      svg: { color: 'white' },
-      ...sx
-    }}
-  >
-    {children || (
-      <Box
-        as="span"
+    <Slide left>
+      <Card
         sx={{
-          width: 'fit-content',
-          bg: color,
-          borderRadius: 18,
-          lineHeight: 0,
-          p: 2,
-          mb: 1,
-          display: 'inline-block',
-          transform: ['scale(0.75)', 'none'],
-          transformOrigin: 'bottom left',
-          boxShadow:
-            'inset 2px 2px 6px rgba(255,255,255,0.2), inset -2px -2px 6px rgba(0,0,0,0.1), 0 1px 4px rgba(0,0,0,0.1), 0 4px 8px rgba(0,0,0,0.1)'
+          display: 'grid',
+          gridGap: [0, 3],
+          gridTemplateColumns: [null, 'auto 1fr'],
+          alignItems: 'center',
+          justifyContent: 'start',
+          bg: 'darkless',
+          p: 0,
+          borderRadius: 'extra',
+          span: { transform: 'none', width: 'min-intrinsic' },
+          svg: { color: 'white' },
+          ...sx
         }}
       >
-        <Icon glyph={icon} size={40} />
-      </Box>
-    )}
-    <Box>
-      <Heading as="h3" variant="headline" mb={2} mt={0}>
-        {name}
-      </Heading>
-      <Text
-        as="p"
-        sx={{  fontSize: 19,color: "muted", mt: 0, pb: 2, a: { variant: 'styles.a', color: 'primary' } }}
-      >
-        {desc}
-      </Text>
-    </Box>
-  </Box> 
+        {children || (
+          <Box
+            as="span"
+            sx={{
+              width: 'fit-content',
+              bg: color,
+              borderRadius: 18,
+              lineHeight: 0,
+              p: 2,
+              mb: 1,
+              display: 'inline-block',
+              transform: ['scale(0.75)', 'none'],
+              transformOrigin: 'bottom left',
+              boxShadow:
+                'inset 2px 2px 6px rgba(255,255,255,0.2), inset -2px -2px 6px rgba(0,0,0,0.1), 0 1px 4px rgba(0,0,0,0.1), 0 4px 8px rgba(0,0,0,0.1)'
+            }}
+          >
+            <Icon glyph={icon} size={50} />
+          </Box>
+        )}
+        <Box>
+          <Heading variant="headline" sx={{ mb: 0, fontSize: 26 }}>
+            {name}
+          </Heading>
+          <Text
+            as="p"
+            sx={{
+              fontSize: 18,
+              color: 'muted',
+              mt: 0,
+              pb: 1,
+              lineHeight: 1.175,
+              a: { variant: 'styles.a', color: 'primary' }
+            }}
+          >
+            {desc}
+          </Text>
+        </Box>
+      </Card>
+    </Slide>
   )
 }
-
-// const Feature = ({ icon, color, name, desc, children, sx, ...props }) => (
-//   <Box
-//     sx={{
-//       display: 'grid',
-//       gridGap: [0, 4],
-//       gridTemplateColumns: [null, 'auto 1fr'],
-//       alignItems: 'start',
-//       justifyContent: 'start',
-//       bg: 'rgba(224, 230, 237, 0.25)',
-//       p: [3, 4],
-//       mt: [1, 1],
-//       borderRadius: 'extra',
-//       span: { transform: 'none', width: 'min-intrinsic' },
-//       svg: { color: 'white' },
-//       ...sx
-//     }}
-//   >
-//     {children || (
-//       <Box
-//         as="span"
-//         sx={{
-//           width: 'fit-content',
-//           bg: color,
-//           borderRadius: 18,
-//           lineHeight: 0,
-//           p: 2,
-//           mb: 1,
-//           display: 'inline-block',
-//           transform: ['scale(0.75)', 'none'],
-//           transformOrigin: 'bottom left',
-//           boxShadow:
-//             'inset 2px 2px 6px rgba(255,255,255,0.2), inset -2px -2px 6px rgba(0,0,0,0.1), 0 1px 4px rgba(0,0,0,0.1), 0 4px 8px rgba(0,0,0,0.1)'
-//         }}
-//       >
-//         <Icon glyph={icon} size={48} />
-//       </Box>
-//     )}
-//     <Box>
-//       <Heading as="h3" variant="headline" mb={2} mt={0}>
-//         {name}
-//       </Heading>
-//       <Text
-//         as="p"
-//         variant="subtitle"
-//         sx={{ mt: 0, pb: 2, a: { variant: 'styles.a', color: 'blue' } }}
-//       >
-//         {desc}
-//       </Text>
-//     </Box>
-//   </Box>
-// )
