a0ee7f0a3073e56f2e3ec9cef3d6d4bb1ef50083
bajpai244 <bajpaiharsh244@gmail.com>
2021-10-08 00:15:34 +0530

    fix: add option to remove forced light mode on certain pages.
    This pr ads an option to ignore forced light mode on certain pages,
    any path that will be added to pathsToIgnore array will be ignored!

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,25 @@ import Meta from '@hackclub/meta'
 import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '../lib/theme'
 import { ThemeProvider } from 'theme-ui'
+import { useRouter } from 'next/router'
 
-const App = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <Meta as={Head} />
-    <Component {...pageProps} />
-  </ThemeProvider>
-)
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  const pathsToIgnore = ['bank']
+  const path = router.asPath.slice(1, -1) // remove `/` from start and end, i.e /bank/ => bank
+
+  if (!pathsToIgnore.includes(path)) {
+    /**Disable dark mode */
+    theme.useColorSchemeMediaQuery = false
+    theme.colors.modes = {}
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Meta as={Head} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
 
 export default App
