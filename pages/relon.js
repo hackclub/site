9398f3f9bf841d3046890541e80659b9e4451e41
Zachary Fogg <zach.fogg@gmail.com>
2021-10-08 19:37:22 -0400

    absolute link for opengraph meta tags oops heh (#218)

--- pages/relon.js
+++ pages/relon.js
@@ -47,7 +47,7 @@ const RelonPage = () => (
       as={Head}
       title="Elon Musk's $1M Donation"
       description="We’re thrilled to announce Elon Musk has donated $1M to Hack Club, a global nonprofit network of high school hackers & coding clubs."
-      image="/relon/social-card.png"
+      image="https://cloud-6w46cupdh-hack-club-bot.vercel.app/0social-card.png"
     />
     <ForceTheme theme="light" />
     <Nav />
