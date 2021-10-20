c697e8bd386f3837ecbcd4f9eb82d110401269f2
Abby Fischler <abigail.fischler@gmail.com>
2021-10-20 09:54:07 -0700

    Added page for Event Designer Job Description
    This PR adds a page with the text for the Event Designer job description, so we can start posting for it. Don't merge yet even if coded correctly waiting for Christina to edit text.

---
+++ pages/jobs/events-designer.js
@@ -0,0 +1,47 @@
+import { BaseStyles, Box, Container, Heading, Text } from 'theme-ui'
+import Head from 'next/head'
+import Nav from '../../components/nav'
+import Meta from '@hackclub/meta'
+import JobDescription from '../../components/jobs/events-designer/jd.mdx'
+import ForceTheme from '../../components/force-theme'
+
+export default () => (
+  <>
+    <Meta
+      as={Head}
+      title="Events Designer"
+      description="Hack Club is looking for someone to make Hack Club a greater place for girls."
+      image="https://workshop-cards.hackclub.com/Events Designer@ Hack Club.png?fontSize=185x&brand=HQ"
+    />
+    <ForceTheme theme="light" />
+    <Nav />
+    <Box
+      as="section"
+      sx={{
+        pt: [5, 6],
+        pb: [4, 5],
+        backgroundImage: theme => theme.util.gx('#516395', '#614385')
+      }}
+    >
+      <Container sx={{ textAlign: 'center', color: 'white' }}>
+        <Heading as="h1" variant="title">
+          Events Designer @&nbsp;Hack&nbsp;Club
+        </Heading>
+        <Text variant="headline">
+          New job open as of October 20th, 2021.
+        </Text>
+      </Container>
+    </Box>
+    <Container
+      as={BaseStyles}
+      variant="copy"
+      sx={{
+        pt: [3, 4],
+        pb: [4, 5],
+        fontSize: [2, 3]
+      }}
+    >
+      <JobDescription />
+    </Container>
+  </>
+)
