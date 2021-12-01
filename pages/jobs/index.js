b55663674b37202be52d00644c1c8a7fca790f8f
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-12-01 13:43:43 +0800

    Update index.js

--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -2,11 +2,9 @@ import React from 'react'
 import styled from '@emotion/styled'
 import {
   Box,
-  Button,
   Container,
   Heading,
   Card,
-  Link as A,
   Text,
   Grid
 } from 'theme-ui'
@@ -16,18 +14,6 @@ import ForceTheme from '/components/force-theme'
 import Nav from '../../components/nav'
 import Footer from '../../components/footer'
 import theme from '../../lib/theme'
-
-const Header = styled(Box)`
-  background: url('/pattern.svg');
-  > div {
-    display: grid;
-    grid-gap: 24px;
-    align-items: center;
-    @media screen and (min-width: 48em) {
-      grid-template-columns: 3fr 2fr;
-    }
-  }
-`
 
 const Sheet = styled(Card)`
   position: relative;
@@ -57,23 +43,23 @@ const JobListing = ({
     bg="primary"
     color="white"
     align="center"
-    style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-      alignItems: 'center',
+    as="a"
+    href={positionLink}
+    sx={{
       boxShadow:
         '0 4px 8px rgba(0, 0, 0, 0.0625), 0 16px 32px rgba(0, 0, 0, 0.125) !important;',
       backgroundImage: `radial-gradient( ellipse farthest-corner at bottom right, ${color}, ${color1})`,
       position: 'relative',
       width: '100%',
-      color: 'white'
+      color: 'white',
+      textDecoration: 'none'
     }}
+    variant="interactive"
   >
     <Heading
       mb={1}
       sx={{
-        fontSize: ['36px', '48px', '60px'],
+        fontSize: '42px',
         color: 'white'
       }}
     >
@@ -82,29 +68,14 @@ const JobListing = ({
     <Text
       color="snow"
       sx={{
-        fontSize: ['24px'],
-        m: 3,
+        fontSize: ['22px'],
+        my: 2,
         display: 'block',
         textAlign: 'left'
       }}
     >
       {positionDesc}
     </Text>
-
-    <Button
-      as="a"
-      href={positionLink}
-      bg="snow"
-      color="primary"
-      width={['auto', '100%']}
-      sx={{
-        alignSelf: ['center', 'normal'],
-        px: [5, 2, 0],
-        py: 3
-      }}
-    >
-      Read more »
-    </Button>
   </Sheet>
 )
 
@@ -116,112 +87,110 @@ export default () => (
       description="Hack Club is looking to hire please find our job listings below."
     />
     <ForceTheme theme="light" />
-    <Nav light />
+    <Nav />
     <Box
       as="main"
       key="main"
       sx={{
-        textAlign: 'center',
         color: 'black'
       }}
     >
       <Box
-        as="article"
         sx={{
-          position: 'relative',
-          overflow: 'hidden',
-          py: [3, 4],
-          px: 4
+          py: [4, 6],
+          background:
+            'linear-gradient(90deg, rgba(2,0,36,0.53) 0%, rgba(2,0,36,0.46) 100%), url(https://pbs.twimg.com/media/E25MY-EXIAcfwfM?format=jpg&name=4096x4096)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          textAlign: 'center'
         }}
-      ></Box>
-      <Nav color="muted" />
-      <Container sx={{ py: [4, 5], px: [0, 'auto'] }}>
-        <Heading
-          as="h1"
-          sx={{
-            fontSize: ['48px', '48px', '60px']
-          }}
-        >
-          Hack Club is hiring!
-        </Heading>
-
-        <Heading
-          sx={{
-            fontSize: [null, null, '48px'],
-            textTransform: 'uppercase'
-          }}
-          color="primary"
-          caps
-        >
-          We want you to join our team.
-        </Heading>
-        <Box
+      >
+        <Container>
+          <Heading
+            as="h1"
+            sx={{
+              fontSize: ['48px', '48px', '72px'],
+              color: 'white'
+            }}
+          >
+            Join the Hack Club Team
+          </Heading>
+          <Heading sx={{ color: 'smoke', mt: 3 }}>
+            <Text
+              sx={{
+                bg: 'dark',
+                color: 'green',
+                width: 'fit-content',
+                px: 3,
+                py: 2,
+                borderRadius: 6,
+                mr: 1
+              }}
+            >
+              $ ssh jobs.hackclub.com
+            </Text>{' '}
+            or scroll down to learn more...
+          </Heading>
+        </Container>
+      </Box>
+      <Container sx={{ py: [3, 4], px: [0, 'auto'] }}>
+        <Grid
           sx={{
             maxWidth: '64rem',
-            mx: 'auto',
-            py: 3,
-            px: [0, 3]
+            mx: [0, 'auto']
           }}
           align="left"
+          columns={['1fr 1fr']}
         >
-          <Grid
-            sx={{
-              maxWidth: '64rem',
-              mx: [0, 'auto']
-            }}
-            align="left"
-            columns={['1fr', null, null, '1fr 1fr']}
-          >
-            <JobListing
-              positionName="Executive Assistant"
-              positionDesc="Someone to play a role in supporting the leadership in administrative management of day-to-day operations."
-              positionLink="https://hackclub.com/jobs/executive-assistant/"
-              color={theme.util.cx('blue')}
-              color1={theme.util.cx('green')}
-            ></JobListing>
-            <JobListing
-              positionName="Events Designer"
-              positionDesc="We're looking for someone fun, creative, and technical to excite and grow the community."
-              positionLink="https://hackclub.com/jobs/events-designer/"
-              color="#516395"
-              color1="#614385"
-            ></JobListing>
-            <JobListing
-              positionName="Philanthropy Position"
-              positionDesc="We are looking for a highly-experienced senior executive, who has advised donors, and who has proven success in stewarding collaborations and closing major gifts."
-              positionLink="https://hackclub.com/jobs/philanthropy-position/"
-              color={theme.util.cx('yellow')}
-              color1={theme.util.cx('pink')}
-            ></JobListing>
-            <JobListing
-              positionName="Education Engineer"
-              positionDesc="We are looking for an engineer & educator to create and lead on technical projects for Hack Clubbers, with a strong focus on young women and non-binary individuals at Hack&nbsp;Club."
-              positionLink="https://hackclub.com/jobs/education-engineer/"
-              color="#ffafbd"
-              color1="#ffc3a0"
-            ></JobListing>
-            <JobListing
-              positionName="Bank Tech Lead"
-              positionDesc="Hack Club Bank is our in-house financial software used by 1,500 Hack Clubbers to financially power their Hack Clubs, hackathons, and student-organized nonprofits."
-              positionLink="https://hackclub.com/jobs/bank-tech-lead/"
-              color={theme.util.cx('purple')}
-              color1={theme.util.cx('red')}
-            ></JobListing>
-            <JobListing
-              positionName="Communications Manager"
-              positionDesc={
-                <>
-                  Your main goal will be to make sure every teenager who{' '}
-                  <em>would</em> want to know about Hack Club knows about Hack
-                  Club, and create comms that meet teens where they're at.
-                </>
-              }
-              positionLink="https://hackclub.com/jobs/communications-manager/"
-              color="#FC5C7D"
-              color1="#6A82FB"
-            ></JobListing>
-          </Grid>
-        </Box>
+          <JobListing
+            positionName="Executive Assistant"
+            positionDesc="Someone to play a role in supporting the leadership in administrative management of day-to-day operations."
+            positionLink="/jobs/executive-assistant/"
+            color={theme.util.cx('blue')}
+            color1={theme.util.cx('green')}
+          ></JobListing>
+          <JobListing
+            positionName="Events Designer"
+            positionDesc="We're looking for someone fun, creative, and technical to excite and grow the community."
+            positionLink="/jobs/events-designer/"
+            color="#516395"
+            color1="#614385"
+          ></JobListing>
+          <JobListing
+            positionName="Philanthropy Position"
+            positionDesc="We are looking for a highly-experienced senior executive, who has advised donors, and who has proven success in stewarding collaborations and closing major gifts."
+            positionLink="/jobs/philanthropy-position/"
+            color={theme.util.cx('purple')}
+            color1={theme.util.cx('blue')}
+          ></JobListing>
+          <JobListing
+            positionName="Education Engineer"
+            positionDesc="We are looking for an engineer & educator to create and lead on technical projects for Hack Clubbers, with a strong focus on young women and non-binary individuals at Hack&nbsp;Club."
+            positionLink="/jobs/education-engineer/"
+            color={theme.util.cx('red')}
+            color1={theme.util.cx('orange')}
+          ></JobListing>
+          <JobListing
+            positionName="Bank Tech Lead"
+            positionDesc="Hack Club Bank is our in-house financial software used by 1,500 Hack Clubbers to financially power their Hack Clubs, hackathons, and student-organized nonprofits."
+            positionLink="/jobs/bank-tech-lead/"
+            color={theme.util.cx('red')}
+            color1={theme.util.cx('purple')}
+          ></JobListing>
+          <JobListing
+            positionName="Communications Manager"
+            positionDesc={
+              <>
+                Your main goal will be to make sure every teenager who{' '}
+                <em>would</em> want to know about Hack Club knows about Hack
+                Club, and create comms that meet teens where they're at.
+              </>
+            }
+            positionLink="/jobs/communications-manager/"
+            color={theme.util.cx('blue')}
+            color1={theme.util.cx('cyan')}
+          ></JobListing>
+        </Grid>
       </Container>
     </Box>
 
