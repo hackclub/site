11e91f9d6a7e1dcdacd0b9fedeb96975dfc2215e
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-01-14 13:51:18 +1100

    please fix

--- components/mention.js
+++ components/mention.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { memo, useState, useEffect } from 'react'
+import { memo } from 'react'
 
 export const StaticMention = memo(({ avatar, username, link }) => (
   <Link href={link}>
