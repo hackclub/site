733ba63aff102cdf10de10c5643bc779a20fa0c6
Caleb Denio <cjdenio44@gmail.com>
2022-01-13 22:19:02 -0500

    Fix StaticMention component name (#297)

--- components/mention.js
+++ components/mention.js
@@ -1,32 +1,34 @@
 import Link from 'next/link'
 import { memo } from 'react'
 
-export const StaticMention = memo(({ avatar, username, link }) => (
-  <Link href={link}>
-    <a
-      className={`mention`}
-      style={{
-        display: 'inline-flex',
-        alignItems: 'baseline',
-        textDecoration: 'none'
-      }}
-    >
-      {avatar && (
-        <img
-          src={avatar}
-          alt={username}
-          width={24}
-          className="mention-avatar"
-          style={{
-            borderRadius: '50%',
-            marginRight: '4px',
-            alignSelf: 'flex-end'
-          }}
-        />
-      )}
-      @{username}
-    </a>
-  </Link>
-))
+const StaticMention = memo(function StaticMention({ avatar, username, link }) {
+  return (
+    <Link href={link}>
+      <a
+        className={`mention`}
+        style={{
+          display: 'inline-flex',
+          alignItems: 'baseline',
+          textDecoration: 'none'
+        }}
+      >
+        {avatar && (
+          <img
+            src={avatar}
+            alt={username}
+            width={24}
+            className="mention-avatar"
+            style={{
+              borderRadius: '50%',
+              marginRight: '4px',
+              alignSelf: 'flex-end'
+            }}
+          />
+        )}
+        @{username}
+      </a>
+    </Link>
+  )
+})
 
-StaticMention.displayName = 'StaticMention';
+export default StaticMention
