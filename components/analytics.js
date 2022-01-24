d3ced1864392fa3dfeaa823d8093d0694a6a3971
leomcelroy <leomcelroy@gmail.com>
2022-01-24 15:22:50 -0500

    added plausible (#316)

---
+++ components/analytics.js
@@ -0,0 +1,9 @@
+
+export default () => (
+  <script 
+    defer 
+    data-domain="hackclub.com" 
+    src="https://plausible.io/js/plausible.js">
+  </script>
+
+)
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 
+import Analytics from "../components/analytics.js";
+
 import Meta from '@hackclub/meta'
 import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '../lib/theme'
@@ -10,6 +12,7 @@ const App = ({ Component, pageProps }) => (
   <ThemeProvider theme={theme}>
     <Meta as={Head} />
     <Component {...pageProps} />
+    <Analytics />
   </ThemeProvider>
 )
 
