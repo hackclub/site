0b544fb021d0f64725fec98dac8c5384a7f3db82
Caleb Denio <cjdenio44@gmail.com>
2022-02-06 21:00:23 -0500

    Hmm

--- pages/bank/_middleware.js
+++ pages/bank/_middleware.js
@@ -5,14 +5,13 @@ const partners = ['gb_help_desk']
 export function middleware(req) {
   const url = req.nextUrl.clone()
 
-  if (
-    url.pathname === '/bank/' &&
-    partners.includes(url.searchParams.get('ref'))
-  ) {
-    url.pathname = '/bank/partner'
-  } else if (url.pathname === '/bank/') {
-    url.pathname = '/bank/index'
-  }
+  if (url.pathname === '/bank/') {
+    if (partners.includes(url.searchParams.get('ref'))) {
+      url.pathname = '/bank/partner'
+    } else {
+      url.pathname = '/bank/index'
+    }
 
-  return NextResponse.rewrite(url)
+    return NextResponse.rewrite(url)
+  }
 }
