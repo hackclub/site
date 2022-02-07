ce8da9a8f02affc0e8ba126c3ea336ed2e563f1d
Caleb Denio <cjdenio44@gmail.com>
2022-02-06 21:33:24 -0500

    Fix middleware-related warning (#344)
    * Fix middleware-related warning
    
    * Hmm

--- pages/bank/_middleware.js
+++ pages/bank/_middleware.js
@@ -3,15 +3,15 @@ import { NextResponse } from 'next/server'
 const partners = ['gb_help_desk']
 
 export function middleware(req) {
-  const url = req.nextUrl
-  if (
-    url.pathname == '/bank/' &&
-    partners.includes(url.searchParams.get('ref'))
-  ) {
-    let res = NextResponse.rewrite('/bank/partner')
-    return res
-  } else if (url.pathname == '/bank/') {
-    let res = NextResponse.rewrite('/bank/index')
-    return res
+  const url = req.nextUrl.clone()
+
+  if (url.pathname === '/bank/') {
+    if (partners.includes(url.searchParams.get('ref'))) {
+      url.pathname = '/bank/partner'
+    } else {
+      url.pathname = '/bank/index'
+    }
+
+    return NextResponse.rewrite(url)
   }
 }
