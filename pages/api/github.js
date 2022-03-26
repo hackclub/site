5ad39958e995c3f0d11fd73442e1d9b7188f018c
Jessica Card <jessicard@mac.com>
2022-03-25 21:54:25 -0400

    Use GraphQL API

--- pages/api/github.js
+++ pages/api/github.js
@@ -1,14 +1,42 @@
-import { Octokit } from '@octokit/rest'
+import { graphql } from '@octokit/graphql'
+import { createAppAuth } from '@octokit/auth-app'
 
-const octokit = new Octokit({
-  auth: process.env.GITHUB
+const auth = createAppAuth({
+  appId: process.env.GITHUB_APP_ID,
+  privateKey: process.env.GITHUB_PRIVATE_KEY,
+  installationId: process.env.GITHUB_INSTALLATION_ID,
 })
 
 export default async function handler(_, res) {
-  const { data } = await octokit.repos.listForOrg({
-    org: 'hackclub',
-    type: 'public',
-    per_page: 100
-  })
-  res.status(200).json(data)
+  const { organization } = await graphql(`
+    query orgQuery($login: String!) {
+      organization(login: $login) {
+        repositories(first: 20, privacy: PUBLIC, orderBy: {
+          field: PUSHED_AT, direction: DESC
+        }){
+          nodes {
+            name
+            description
+            languages(first: 1) {
+              nodes {
+                name
+              }
+            }
+            pushedAt
+            url
+            issues(states: OPEN) {
+              totalCount
+            }
+          }
+        }
+      }
+    }`,
+    {
+      login: 'hackclub',
+      request: {
+        hook: auth.hook,
+      }
+    }
+  )
+  res.status(200).json(organization);
 }
