5a5ade0417574b1ef7793c8fc9d38ba7eb4d279f
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-04-04 13:56:57 +0800

    Update contribute.js

--- pages/api/contribute.js
+++ pages/api/contribute.js
@@ -17,7 +17,6 @@ export default async function handler(req, res) {
           nodes {
             name
             description
-            isArchived
             languages(first: 1) {
               nodes {
                 name
@@ -40,6 +39,7 @@ export default async function handler(req, res) {
           nodes {
             name
             description
+            isArchived
             languages(first: 1) {
               nodes {
                 name
