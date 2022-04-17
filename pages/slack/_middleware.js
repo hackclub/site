af1a9ef14373635e14dc852db982fe2cc2ecec81
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-04-17 15:47:55 +0800

    Update _middleware.js

--- pages/slack/_middleware.js
+++ pages/slack/_middleware.js
@@ -4,8 +4,11 @@ import country from 'country-list-js';
 export function middleware(req) {
   let url = req.nextUrl
   if(!url.searchParams.get("continent")){
-    let continent = country.findByIso2(req.geo.country || "US").continent
+    let continent = country.findByIso2(req.geo.country || "AU").continent
+    if(continent == "Oceania"){
+      continent = "Australia"
+    }
     url.searchParams.set("continent", continent)
-    return NextResponse.rewrite(url)
+    return NextResponse.redirect(url)
   }
 }
