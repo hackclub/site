2ae28f761d3d938fa42337e5c1ae4ebb8f15009b
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-04-17 14:57:19 +0800

    Hmm

--- pages/slack/_middleware.js
+++ pages/slack/_middleware.js
@@ -1,8 +1,12 @@
 import { NextResponse } from 'next/server'
-
 import country from 'country-list-js';
 
 export function middleware(req) {
   let continent = country.findByIso2(req.geo.country || "US").continent
-  console.log(continent)
+  let url = req.nextUrl
+  let originalURL = req.nextUrl
+  url.searchParams.set("continent", continent)
+  if(url != originalURL){
+    return NextResponse.redirect(url)
+  }
 }
