f61658fbd68b286b47ae0cc315e63b147e5ed055
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-04-17 15:30:50 +0800

    Update _middleware.js

--- pages/slack/_middleware.js
+++ pages/slack/_middleware.js
@@ -3,6 +3,7 @@ import country from 'country-list-js';
 
 export function middleware(req) {
   let continent = country.findByIso2(req.geo.country || "US").continent
+  console.log(continent)
   let url = req.nextUrl
   let originalURL = req.nextUrl
   url.searchParams.set("continent", continent)
