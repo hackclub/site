39b5440d90bfb048c9fbb9fc298561006ae66271
Abby Fischler <abigail.fischler@gmail.com>
2022-04-24 13:45:47 -0700

    Changed Club operations to customer success lead
    Changed Club operations to customer success lead. Re did job description based off new one from Charlene.

--- pages/jobs/customer-success-lead.js
+++ pages/jobs/customer-success-lead.js
@@ -2,16 +2,16 @@ import { BaseStyles, Box, Container, Heading, Text } from 'theme-ui'
 import Head from 'next/head'
 import Nav from '../../components/nav'
 import Meta from '@hackclub/meta'
-import JobDescription from '../../components/jobs/club-operations-lead/jd.mdx'
+import JobDescription from '../../components/jobs/customer-success-lead/jd.mdx'
 import ForceTheme from '../../components/force-theme'
 
 const Page = () => (
   <>
     <Meta
       as={Head}
-      title="Club Operations Lead"
-      description="Hack Club is a hiring a Club Operations Lead to join our team in Shelburne, Vermont."
-      image="https://workshop-cards.hackclub.com/Clubs%20Operations%20Lead%20%40%20Hack%20Club.png?fontSize=200px&brand=HQ"
+      title="Customer Success Lead"
+      description="Hack Club is a hiring a Customer Success Lead to join our team in Shelburne, Vermont."
+      image="https://workshop-cards.hackclub.com/Customer%20Success%20Lead%20%40%20Hack%20Club.png?fontSize=175px&brand=HQ"
     />
     <ForceTheme theme="light" />
     <Nav />
@@ -25,10 +25,10 @@ const Page = () => (
     >
       <Container sx={{ textAlign: 'center', color: 'white' }}>
         <Heading as="h1" variant="title" mb={30}>
-        Club Operations Lead @&nbsp;Hack&nbsp;Club
+        Customer Success Lead @&nbsp;Hack&nbsp;Club
         </Heading>
         <Text variant="headline" sx={{ fontWeight: 400 }}>
-          New job open as of April 12th, 2022.
+          New job open as of April 24th, 2022.
         </Text>
       </Container>
     </Box>
