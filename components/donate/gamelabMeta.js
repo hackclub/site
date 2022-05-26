ef239b7bf0402eecf0b3d45a7cd05624b0cc1fc9
Max Wofford <max@maxwofford.com>
2022-05-26 19:47:48 -0400

    Improve social tags for gl donations

--- components/donate/gamelabMeta.js
+++ components/donate/gamelabMeta.js
@@ -4,8 +4,8 @@ import Head from 'next/head'
 export default () => {
   return <Meta
     as={Head}
-    title="Donate a kit"
-    description="Inspiring teens to both make and code."
+    title="Donate a Game Console kit"
+    description="Inspiring teens to both make and code. Your $100 donation will send a teenager a Hack Club Game Console Kit!"
     image="https://cloud-kcloydjv0-hack-club-bot.vercel.app/0image_from_ios__1_.jpg"
   />
 }
