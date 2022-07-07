b235667a2e06c0a11a830c7feb4b6b3d2cba668c
Ella <git@ella.cx>
2022-07-07 09:35:44 -0400

    Pass on JD for Executive Producer (#471)
    * Pass on JD copy
    
    * Update meta stuff
    
    * Dropout as a noun is one word
    
    https://english.stackexchange.com/questions/496894/drop-out-vs-dropout-vs-drop-out

--- pages/jobs/executive-producer.js
+++ pages/jobs/executive-producer.js
@@ -9,9 +9,9 @@ const Page = () => (
   <>
     <Meta
       as={Head}
-      title="Executive Producer, Hack Club Communications"
-      description="HHack Club is actively looking now to hire its first executive producer/ senior editor to tell its story across various platforms, including social media, newsletters, op-docs, blog/oped posts and direct emails, and curated partnerships with major tech companies."
-      image="https://workshop-cards.hackclub.com/Executive%20Producer%40%20Hack%20Club.png?fontSize=175px&brand=HQ"
+      title="VP of Marketing / Executive Producer"
+      description="Hack Club is actively looking to hire its first VP of Marketing / Executive Producer to tell its story across various platforms, including social media, newsletters, op-docs, blog/Op-Ed posts, direct emails, and curated partnerships with major tech companies."
+      image="https://workshop-cards.hackclub.com/VP%20of%20Marketing%20%40%20Hack%20Club.png?fontSize=175px&brand=HQ"
     />
     <ForceTheme theme="light" />
     <Nav />
@@ -25,7 +25,7 @@ const Page = () => (
     >
       <Container sx={{ textAlign: 'center', color: 'white' }}>
         <Heading as="h1" variant="title" mb={30}>
-        Executive Producer
+          VP of Marketing / Executive Producer
         </Heading>
         <Text variant="headline" sx={{ fontWeight: 400 }}>
           New job open as of July 6th, 2022.
