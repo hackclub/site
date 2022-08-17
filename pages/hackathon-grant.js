2e4fca3ab6480bf55537e7176b3b8edfaf010c94
Belle <bellesee1212@gmail.com>
2022-08-17 10:07:47 -0400

    meta-2

--- pages/hackathon-grant.js
+++ pages/hackathon-grant.js
@@ -54,12 +54,18 @@ const HackathonGrant = () => {
 
   return (
     <>
-    <Meta
-          as={Head}
-          title="Hackathon Grant"
-          description="Hack Club is partnering with FIRST to provide $500 grants to in-person high-school hackathons happening this semester."
-          image="https://cloud-50ga5vjj0-hack-club-bot.vercel.app/0frame_1.png"
-        />
+      <Meta
+        as={Head}
+        title="Hackathon Grant"
+        description="Hack Club is partnering with FIRST to provide $500 grants to in-person high-school hackathons happening this semester."
+        image="https://cloud-123vvl4d8-hack-club-bot.vercel.app/0frame_1__1_.png"
+      />
+      <Head>
+      <meta
+        name="twitter:image"
+        content="https://cloud-123vvl4d8-hack-club-bot.vercel.app/0frame_1__1_.png"
+      />
+      </Head>
       <Box as="main" key="main">
         <Nav dark />
         <ForceTheme theme="dark" />
