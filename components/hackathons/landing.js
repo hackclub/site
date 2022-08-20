34d301f4b2513e6075c4f8484fc7a03c83232ff4
Mutammim <58490040+mutammim@users.noreply.github.com>
2022-08-20 12:12:18 -0400

    Better resemble original positioning

--- components/hackathons/landing.js
+++ components/hackathons/landing.js
@@ -28,7 +28,7 @@ export default function Landing() {
             color="primary"
             sx={{
               position: 'absolute',
-              top: [2, 4],
+              top: [-2, 4],
               left: '0',
               right: '0',
               mx: 'auto',
