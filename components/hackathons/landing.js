554d836935d0e77f385a10f914371948eb583f4b
Ella <git@ella.cx>
2022-08-21 07:43:46 -0400

    Refactor code for hackathons/grant pages (#515)

--- components/hackathons/landing.js
+++ components/hackathons/landing.js
@@ -75,9 +75,7 @@ export default function Landing() {
                   Looking for hackathons?{' '}
                   <Icon glyph="external" size={30} sx={{ pl: 1 }} />
                 </Button>
-                {/* <Text as="p" variant="headline" sx={{ mt: 0, color: 'muted' }}>
-                  Leading text
-                </Text> */}
+
                 <Heading
                   as="h2"
                   variant="title"
