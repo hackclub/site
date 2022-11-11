b11e3f1fba052e8f2d25fe889cd67fe6247c9743
Ella <git@ella.cx>
2022-11-10 23:12:30 -0500

    hackclub.com/bank/first (#585)

---
+++ components/bank/first/form.js
@@ -0,0 +1,178 @@
+import { Box, Input, Label, Button, Select, Text } from 'theme-ui'
+import { useState } from 'react'
+import theme from '@hackclub/theme'
+import Icon from '../../icon'
+import { keyframes } from '@emotion/react'
+
+const hideAnimation = keyframes({
+  from: { display: 'flex' },
+  to: { display: 'none', opacity: 0, padding: 0, position: 'absolute' }
+})
+
+function Base({ children, action, target, method, onSubmit, id }) {
+  return (
+    <Box
+      as="form"
+      sx={{ display: 'grid', gridTemplateColumns: '1fr' }}
+      id={id}
+      action={action}
+      target={target}
+      method={method}
+      onSubmit={onSubmit}
+    >
+      {children}
+    </Box>
+  )
+}
+
+function Field({
+  placeholder,
+  label,
+  name,
+  type,
+  value,
+  onChange,
+  required = true
+}) {
+  return (
+    <Box sx={{ my: 2 }}>
+      <Label htmlFor={name} sx={{ color: 'muted', fontSize: 18 }}>
+        {label}
+      </Label>
+      <Input
+        id={name}
+        placeholder={placeholder}
+        name={name}
+        type={type}
+        sx={{
+          bg: 'dark'
+        }}
+        onChange={onChange}
+        value={value}
+        required={required}
+      />
+    </Box>
+  )
+}
+
+export default function Signup() {
+  const [submitted, setSubmitted] = useState(false)
+
+  const [values, setValues] = useState({
+    locationState: '',
+    locationCountry: '',
+    teamName: '',
+    teamType: '',
+    teamNumber: '',
+    userEmail: ''
+  })
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    await fetch('/api/bank/demo', {
+      method: 'POST',
+      body: JSON.stringify(values)
+    })
+
+    setSubmitted(true)
+
+    // clear form
+    setValues({
+      locationState: '',
+      locationCountry: '',
+      eventName: '',
+      teamType: '',
+      teamNumber: '',
+      userEmail: ''
+    })
+  }
+
+  return (
+    <>
+      <Base
+        id="form"
+        method="POST"
+        action="/api/bank/demo"
+        onSubmit={handleSubmit}
+      >
+        <Field
+          label="Team Name"
+          name="eventName"
+          placeholder="Poseidon Robotics"
+          value={values.eventName}
+          onChange={e => setValues({ ...values, eventName: e.target.value })}
+        />
+        <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2, w: '100%' }}>
+          <Box sx={{ my: 2 }}>
+            <Label htmlFor="teamType" sx={{ color: 'muted', fontSize: 18 }}>
+              Level
+              <Select
+                name="teamType"
+                defaultValue="Select"
+                sx={{ bg: 'dark' }}
+                onChange={e =>
+                  setValues({ ...values, teamType: e.target.value })
+                }
+              >
+                <option value="Select" selected disabled>
+                  Select
+                </option>
+                <option value="FLL">FLL</option>
+                <option value="FTC">FTC</option>
+                <option value="FRC">FRC</option>
+              </Select>
+            </Label>
+          </Box>
+
+          <Field
+            label="Team number (optional)"
+            name="teamNumber"
+            placeholder="12345"
+            value={values.teamNumber}
+            onChange={e => setValues({ ...values, teamNumber: e.target.value })}
+            required={false}
+          />
+        </Box>
+        <Field
+          label="Email address"
+          name="userEmail"
+          placeholder="fiona@hackclub.com"
+          type="email"
+          value={values.userEmail}
+          onChange={e => setValues({ ...values, userEmail: e.target.value })}
+        />
+        <Button
+          sx={{
+            backgroundImage: theme.util.gx('orange', 'red'),
+            mt: [2, 3],
+            py: 2
+          }}
+          type="submit"
+        >
+          Create new account
+        </Button>
+      </Base>
+      {submitted && (
+        <Box
+          sx={{
+            mt: 2,
+            px: 2,
+            py: 2,
+            borderRadius: 'default',
+            color: 'white',
+            bg: 'green',
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'start',
+            animation: `${hideAnimation} 0s ease-in 15s`,
+            animationFillMode: 'forwards'
+          }}
+        >
+          <Icon glyph="send" size={24} />
+          <Text>Submitted! Check your email for a sign in link.</Text>
+        </Box>
+      )}
+    </>
+  )
+}
---
+++ pages/api/bank/apply.js
@@ -0,0 +1,58 @@
+import AirtablePlus from 'airtable-plus'
+
+const applicationsTable = new AirtablePlus({
+  baseID: 'apppALh5FEOKkhjLR',
+  apiKey: process.env.AIRTABLE_API_KEY,
+  tableName: 'Events'
+})
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body
+
+    const application = await applicationsTable.create({
+      'First Name': data.firstName,
+      'Last Name': data.lastName,
+      'Email Address': data.userEmail,
+      'Phone Number': data.userPhone,
+      'Date of Birth': data.userBirthday,
+      'Event Name': data.eventName,
+      'Event Website': data.eventWebsite,
+      'Tell us about your event': data.eventDescription,
+      'Mailing Address': data.mailingAddress,
+      'Address Line 1': data.addressLine1,
+      'Address Line 2': data.addressLine2,
+      City: data.addressCity,
+      State: data.addressState,
+      'Zip Code': data.addressZip,
+      'Address Country': data.addressCountry,
+      Country: data.eventCountry,
+      'Event Location': data.eventLocation,
+      'Have you used Hack Club Bank for any previous events?':
+        data.returningUser,
+      'How did you hear about HCB?': data.referredBy,
+      Transparent: data.transparent
+    })
+    const url = process.env.BANK_NOTIFS_WEBHOOK
+    const body = JSON.stringify({
+      application
+    })
+    fetch(url, {
+      body,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(r => {
+        res.writeHead(302, { Location: '/bank/apply/success' }).end()
+        console.log(r.statusText)
+      })
+      .catch(error => {
+        console.log(error)
+        res.json({ status: 'Something went wrong', error })
+      })
+  } else {
+    res.status(405).json({ status: 'error', error: 'Must send POST request' })
+  }
+}
