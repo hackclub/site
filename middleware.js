3992b26ed750042ec88572f21549e4d71d53c912
Ella <git@ella.cx>
2022-11-18 16:38:48 -0500

    Remove Partner version of the Bank page

--- middleware.js
+++ middleware.js
@@ -1,27 +1,16 @@
 import { NextResponse } from 'next/server'
-import country from 'country-list-js';
+import country from 'country-list-js'
 const partners = ['gb_help_desk']
 
 export function middleware(request) {
-  if (request.nextUrl.pathname.startsWith('/bank')) {
-    const url = request.nextUrl.clone()
-    if (url.pathname === '/bank/') {
-      if (partners.includes(url.searchParams.get('ref'))) {
-        url.pathname = '/bank/partner'
-      } else {
-        url.pathname = '/bank/index'
-      }
-      return NextResponse.rewrite(url)
-    }
-  }
   if (request.nextUrl.pathname.startsWith('/slack')) {
     let url = request.nextUrl
-    if(!url.searchParams.get("continent")){
-      let continent = country.findByIso2(request.geo.country || "AU").continent
-      if(continent == "Oceania"){
-        continent = "Australia"
+    if (!url.searchParams.get('continent')) {
+      let continent = country.findByIso2(request.geo.country || 'AU').continent
+      if (continent == 'Oceania') {
+        continent = 'Australia'
       }
-      url.searchParams.set("continent", continent)
+      url.searchParams.set('continent', continent)
       return NextResponse.redirect(url)
     }
   }
