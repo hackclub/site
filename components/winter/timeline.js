d0f846f4686141acd3e9397e3e1548ade42b8396
Ella <git@ella.cx>
2022-11-25 17:57:10 -0500

    Add timeline (jank)

--- components/winter/timeline.js
+++ components/winter/timeline.js
@@ -1,1 +1,130 @@
 // yeah
+
+import { Box, Flex, Container, Text, Badge, Link } from 'theme-ui'
+import { Slide } from 'react-reveal'
+import Icon from '../icon'
+
+function TimelineStep({ children }) {
+  return (
+    <Flex
+      sx={{
+        marginX: 4,
+        paddingY: 4,
+        flexDirection: 'row',
+        alignItems: 'center',
+        '&:before': {
+          content: '""',
+          background: 'snow',
+          height: '320px',
+          width: '4px',
+          marginLeft: 36,
+          position: 'absolute',
+          zIndex: 0
+        },
+        '&:first-of-type:before': {
+          top: [0, null, 'auto'],
+          width: [0, null, 0],
+          left: [0, null, 0]
+        },
+        '&:last-of-type:before': {
+          bottom: [0, null, 'auto'],
+          left: [null, null, 0],
+          width: [null, null, 0]
+        }
+      }}
+    >
+      {children}
+    </Flex>
+  )
+}
+
+function Circle({ children }) {
+  return (
+    <Box
+      sx={{
+        p: 14,
+        background: 'red',
+
+        color: 'white',
+        backgroundImage:
+          'radial-gradient(ellipse farthest-corner at top left, #5bc0de, #338eda)',
+        borderRadius: '100%',
+        display: 'inline-block',
+        lineHeight: 0,
+        position: 'relative',
+        zIndex: 999
+      }}
+    >
+      {children}
+    </Box>
+  )
+}
+
+function Step({ icon, name, duration, href }) {
+  return (
+    <TimelineStep sx={{ pb: 1 }}>
+      <Slide left>
+        <Circle>
+          {href ? (
+            <Link href={href} sx={{ cursor: 'pointer' }}>
+              <Icon glyph={icon} size={48} color="white" />
+            </Link>
+          ) : (
+            <Icon glyph={icon} size={48} />
+          )}
+        </Circle>
+        <Container
+          sx={{
+            mt: 0,
+            display: 'flex',
+            justifyContent: 'left',
+            flexDirection: 'column',
+            textAlign: 'left'
+          }}
+        >
+          <Badge
+            variant="pill"
+            sx={{
+              bg: 'smoke',
+              color: 'darker',
+              fontWeight: 'normal',
+              textTransform: 'uppercase',
+              width: 'fit-content',
+              fontSize: 18,
+              px: 3
+            }}
+          >
+            {duration}
+          </Badge>
+          <Text
+            sx={{ color: 'white', fontSize: 24, maxWidth: [300, null, 550] }}
+          >
+            {name}
+          </Text>
+        </Container>
+      </Slide>
+    </TimelineStep>
+  )
+}
+
+export default function RealTimeline() {
+  return (
+    <Flex sx={{ flexDirection: 'column', justifyContent: 'center' }}>
+      <Step
+        icon="post"
+        name="Project propsals open. Submit it via GitHub TODO: woof"
+        duration="Decemeber 1"
+      />
+      <Step
+        icon="send"
+        name="Project proposals close. Make sure to order your hardware by this time!"
+        duration="January 15"
+      />
+      <Step
+        icon="slack"
+        name="Join the #scrapbook channel on Slack for 10 consecutive days of bulding your project in public, alongisde other teenagers in the community."
+        duration="February 3 - 12"
+      />
+    </Flex>
+  )
+}
--- pages/winter.js
+++ pages/winter.js
@@ -10,18 +10,26 @@ import {
   Image,
   Input,
   Label,
-  Link
+  Link,
+  Flex
 } from 'theme-ui'
 // import Snow from 'resnow'
 import Footer from '../components/footer'
 import FadeIn from '../components/fade-in'
 import { useState } from 'react'
 import ForceTheme from '../components/force-theme'
+import RealTimeline from '../components/winter/timeline'
 
 export default function Winter() {
   return (
     <>
-      <Box as="main">
+      <Box as="main" sx={{ bg: 'blue' }}>
+        <Meta
+          as={Head}
+          title="Winter Hardware Wonderland"
+          description="Join the Hack Club community for a winter of hardware hacking, and get a $250 grant to build your project."
+          image="/winter/og-image.png" // TODO: add og image
+        />
         <Nav light />
         <ForceTheme theme="light" />
         {/* hero */}
@@ -39,7 +47,36 @@ export default function Winter() {
         {/* grid section thing with the requirements, info etc. */}
 
         {/* timeline */}
-
+        <Flex
+          sx={{
+            flexDirection: ['column', null, 'row'],
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <Box sx={{ display: 'flex', flexDirection: ['row', null, 'column'] }}>
+            <Heading
+              variant="title"
+              sx={{
+                color: 'white',
+                textTransform: 'uppercase',
+                transform: ['none', null, 'rotate(-90deg)']
+              }}
+            >
+              Timeline
+            </Heading>
+            <Image
+              src="https://cloud-lbajgdi3a-hack-club-bot.vercel.app/0fox_1.png"
+              alt="Illustrated orange fox sleeping in a curled position"
+              sx={{
+                width: ['100px', null, '80%'],
+                pt: [null, null, 6],
+                ml: [2, null, null]
+              }}
+            />
+          </Box>
+          <RealTimeline />
+        </Flex>
         {/* bottom section */}
 
         {/* footer */}
