1425a2e9b88d18479219f2da6bf5bdc02fc2b420
Belle <65808924+bellesea@users.noreply.github.com>
2022-11-25 19:06:47 -0500

    Update breakdown-box.js

--- components/winter/breakdown-box.js
+++ components/winter/breakdown-box.js
@@ -1,14 +1,15 @@
 import { Box, Flex, Container, Heading, Text, Badge, Link } from 'theme-ui'
 import { Zoom } from 'react-reveal'
+import Icon from '@hackclub/icons'
 
 function BreakdownBox({ subtitle, icon, text, description, delay }) {
   return (
     <Zoom delay={delay}>
       <Box sx={{ color: 'white' }}>
-        {subtitle != 'null' ? (
+        {subtitle != null ? (
           <Text as="h1">{subtitle}</Text>
         ) : (
-          <Icon glyph={icon} sx={{ ml: 2, color: 'white' }} />
+          <Icon glyph={icon} sx={{ ml: 2, color: '#5BC0DE' }} />
         )}
         <Heading
           sx={{
