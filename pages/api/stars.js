249ebe4d82df7d5622da0f166b03bfa492228ee7
Belle <bellesee1212@gmail.com>
2022-12-03 01:50:32 -0500

    github graphql

--- pages/api/stars.js
+++ pages/api/stars.js
@@ -1,17 +1,9 @@
 import { graphql } from '@octokit/graphql'
-import { createAppAuth } from '@octokit/auth-app'
-
-const auth = createAppAuth({
-  appId: process.env.GITHUB_APP_ID,
-  privateKey: 'process.env.GITHUB_PRIVATE_KEY',
-  installationId: process.env.GITHUB_INSTALLATION_ID
-})
 
 export default async function handler(req, res) {
   const { organization } = await graphql(
-    !req.query.admin
-      ? `
-  query {
+ `
+  {
     organization(login: "hackclub") {
       sinerider: repository(name: "sinerider") {
         stargazerCount
@@ -20,20 +12,10 @@ export default async function handler(req, res) {
         stargazerCount
       }
     }
-  }`
-      : `query {
-      organization(login: "hackclub") {
-        sinerider: repository(name: "sinerider") {
-          stargazerCount
-        },
-        sprig: repository(name: "sprig") {
-          stargazerCount
-        }
-      }
-    }`,
+  }`,
     {
-      request: {
-        hook: auth.hook
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`
       }
     }
   )
