f13551d0d56e8c385ec3fcce4fe1154c8636646a
Ella <git@ella.cx>
2023-01-23 14:27:57 -0500

    Add meta title for /bank/apply

--- pages/bank/apply.js
+++ pages/bank/apply.js
@@ -1,23 +1,28 @@
 import BankApplyForm from '../../components/bank/form'
 import { Box, Container, Card } from 'theme-ui'
 import ForceTheme from '../../components/force-theme'
+import Head from 'next/head'
+import Meta from '@hackclub/meta'
 
 export default function Apply() {
   return (
-    <Box
-      sx={{
-        backgroundImage:
-          'linear-gradient(to bottom,rgba(0, 0, 0, .3),rgba(0, 0, 0, 0.45) 25%,rgba(0, 0, 0, 0.6) 50%, rgba(0, 0, 0, 0.7) 100%), url("https://cloud-knk7z9mq5-hack-club-bot.vercel.app/0geo-pattern__2_.svg")',
-        py: 4,
-        backgroundAttachment: 'fixed'
-      }}
-    >
-      <ForceTheme theme="dark" />
-      <Container variant="copy">
-        <Card variant="primary">
-          <BankApplyForm />
-        </Card>
-      </Container>
-    </Box>
+    <>
+      <Meta as={Head} title="Apply for Hack Club Bank" />
+      <Box
+        sx={{
+          backgroundImage:
+            'linear-gradient(to bottom,rgba(0, 0, 0, .3),rgba(0, 0, 0, 0.45) 25%,rgba(0, 0, 0, 0.6) 50%, rgba(0, 0, 0, 0.7) 100%), url("https://cloud-knk7z9mq5-hack-club-bot.vercel.app/0geo-pattern__2_.svg")',
+          py: 4,
+          backgroundAttachment: 'fixed'
+        }}
+      >
+        <ForceTheme theme="dark" />
+        <Container variant="copy">
+          <Card variant="primary">
+            <BankApplyForm />
+          </Card>
+        </Container>
+      </Box>
+    </>
   )
 }
