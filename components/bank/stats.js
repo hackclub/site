1e56492219f8910194d560a548220ebf4b19fc1a
Malted <malted@malted.dev>
2023-02-20 15:52:39 +0000

    ✨ New statistics view

--- components/bank/stats.js
+++ components/bank/stats.js
@@ -1,53 +1,22 @@
 import { Text, Box, Flex } from 'theme-ui'
-import { keyframes } from '@emotion/react'
-import { timeSince } from '../../lib/helpers'
 import useSWR from 'swr'
-import Stat from '../stat'
 import fetcher from '../../lib/fetcher'
 
-const renderMoney = amount =>
-  Math.floor(amount / 100)
-    .toLocaleString('en-US', {
-      style: 'currency',
-      currency: 'USD'
-    })
-    .replace('.00', '')
-
-const flashing = keyframes({
-  from: { opacity: 0 },
-  '50%': { opacity: 1 },
-  to: { opacity: 0 }
-})
-
-function Dot() {
-  return (
-    <Text
-      sx={{
-        bg: 'green',
-        color: 'white',
-        borderRadius: 'circle',
-        display: 'inline-block',
-        lineHeight: 0,
-        width: '.4em',
-        height: '.4em',
-        marginRight: '.4em',
-        marginBottom: '.12em',
-        animationName: `${flashing}`,
-        animationDuration: '3s',
-        animationTimingFunction: 'ease-in-out',
-        animationIterationCount: 'infinite'
-      }}
-    />
-  )
+function renderMoney(amount) {
+  const normalisedAmount = amount / 100
+  return normalisedAmount
+    .toLocaleString('en-US', { style: 'currency', currency: 'USD' })
 }
 
-const Stats = props => {
+const Stats = () => {
   const { data } = useSWR('https://bank.hackclub.com/stats', fetcher, {
     fallbackData: {
-      transactions_volume: 500 * 1000 * 1000,
+      transactions_volume: '771911351', // As of 20-02-23
       last_transaction_date: Date.now()
     }
   })
+
+  const transacted = renderMoney(data.transactions_volume).split('.');
 
   return (
     <Box>
@@ -56,38 +25,17 @@ const Stats = props => {
           So far we have enabled
         </Text>
         <Text variant='heading' sx={{
-          color: 'green',
-          fontSize: 84
-        }}>
-          $7,711,681
+            color: 'green',
+            fontSize: 84
+          }}
+        >
+          { transacted[0] }
+          <Text sx={{ fontSize: 4 }}>.{ transacted[1] }</Text>
         </Text>
         <Text sx={{fontSize: 4}}>
           in transactions
         </Text>
       </Flex>
-      {/* <Text
-        variant="lead"
-        fontSize={[2, 3]}
-        color="muted"
-        mt={[2, 4]}
-        mb={[2, 3]}
-      >
-        <Dot />
-        As of {timeSince(data?.last_transaction_date * 1000, false, true)}...
-      </Text>
-      <Box>
-        <Stat
-          {...props}
-          value={renderMoney(data?.raised)}
-          label="raised on Hack Club Bank"
-        />
-        <Stat
-          {...props}
-          fontSize={[3, 4, 5]}
-          value={renderMoney(data?.transactions_volume)}
-          label="total amount transacted"
-        />
-      </Box> */}
     </Box>
   )
 }
