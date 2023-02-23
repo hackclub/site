30c810f1b9ab36fa8557ecfac413de1eaee3f43c
Malted <malted@malted.dev>
2023-02-23 05:44:37 +0000

    ✨ Start address autocomplete support!

---
+++ components/bank/apply/address-autocomplete.js
@@ -0,0 +1,111 @@
+import { useEffect, useRef, useState } from 'react'
+import { Box, Divider, Input, Text } from 'theme-ui'
+import FlexCol from '../../flex-col'
+
+export default function AutoComplete({ name }) {
+    const input = useRef()
+    const [predictions, setPredictions] = useState([])
+
+    const optionClicked = (e) => {
+        input.current.value = e.target.innerText
+        setPredictions([])
+    }
+    const clickOutside = (e) => {        
+        if (input.current && !input.current.contains(e.target)) {
+            setPredictions([])
+        }
+    }
+
+    useEffect(() => {
+        if (!window.google || !input.current) return
+
+        const service = new window.google.maps.places.AutocompleteService()
+        
+        const onInput = (e) => {
+            if (!e.target.value) {
+                setPredictions([])
+            } else {
+                service.getPlacePredictions(
+                    { input: e.target.value },
+                    (predictions, status) => {
+                        setPredictions(predictions)
+                        if (status !== window.google.maps.places.PlacesServiceStatus.OK) {
+                            alert(status)
+                        }
+                    }
+                )
+            }
+        }
+
+        document.addEventListener('click', clickOutside)
+
+        input.current.addEventListener('input', onInput)
+        input.current.addEventListener('focus', onInput)
+        return () => {
+            input.current.removeEventListener('input', onInput)
+            input.current.removeEventListener('focus', onInput)
+            document.removeEventListener('click', clickOutside)
+        }
+    }, [])
+
+    return (
+        <>
+            <script
+                async
+                src='https://maps.googleapis.com/maps/api/js?key=AIzaSyAFRkv4LUuUGZOWbPGjX4ksMHSOrRTjHjo&libraries=places'
+            ></script>
+
+            <FlexCol gap={2} width='250px' position='relative'>
+                <Input
+                    ref={input}
+                    name={name}
+                    id={name}
+                    autoComplete="off"
+                />
+                {
+                    predictions && predictions.length > 0 &&
+                    <Box sx={{
+                        background: '#252429',
+                        width: '100%',
+                        p: 3,
+                        borderRadius: '4px',
+
+                    }}>
+                        <FlexCol gap={1}>
+                            {predictions.map((prediction, idx) => (
+                                <>
+                                    <Text
+                                        as='button'
+                                        onClick={optionClicked}
+                                        sx={{
+                                            border: 'none',
+                                            // outline: 'none',
+                                            background: 'none',
+                                            color: 'white',
+                                            fontFamily: 'inherit',
+                                            fontSize: 'inherit',
+                                            textAlign: 'inherit',
+                                        }}
+                                        key={prediction.id}
+                                    >
+                                        {prediction.description}
+                                    </Text>
+
+                                    {
+                                        idx < predictions.length - 1 &&
+                                        <hr
+                                            style={{
+                                                width: '100%',
+                                                color: '#8492a6',
+                                            }}
+                                        />
+                                    }
+                                </>
+                            ))}
+                        </FlexCol>
+                    </Box>
+                }
+            </FlexCol>
+        </>
+    )
+}
