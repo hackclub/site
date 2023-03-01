bb91efc515381382af150bff69c91ad94ee5e4d5
Malted <malted@malted.dev>
2023-03-01 22:59:40 +0000

    :dizzy: Animate transaction stats display

--- components/bank/stats.js
+++ components/bank/stats.js
@@ -1,40 +1,97 @@
-import { Text, Box, Flex, Heading } from 'theme-ui'
+import { Text, Box, Flex } from 'theme-ui'
+import { useEffect, useState } from 'react'
 import useSWR from 'swr'
 import fetcher from '../../lib/fetcher'
 
-function renderMoney(amount) {
+function startMoneyAnimation(setBalance, amount, duration = 2_000, moneyFormatter) {
+  const easeInOutExpo = (x) =>
+		x === 0
+			? 0
+			: x === 1
+			? 1
+			: x < 0.5
+			? Math.pow(2, 20 * x - 10) / 2
+			: (2 - Math.pow(2, -20 * x + 10)) / 2;
+
+  
+  const startTime = performance.now();
+
+  function animate() {
+    const time = performance.now() - startTime;
+    const progress = time / duration;
+    const easedProgress = easeInOutExpo(progress);
+
+    setBalance(moneyFormatter(amount * easedProgress))
+
+    if (progress < 1) {
+      requestAnimationFrame(animate);
+    } else {
+      setBalance(moneyFormatter(amount))
+    }
+  }
+
+  requestAnimationFrame(animate);
+}
+  
+function formatMoney(amount) {
   const normalisedAmount = amount / 100
   return normalisedAmount
     .toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+    .split('.')
 }
 
 const Stats = () => {
-  const { data } = useSWR('https://bank.hackclub.com/stats', fetcher, {
+  const [balance, setBalance] = useState() // A formatted balance string, split by decimal
+
+  const transactedRaw = useSWR('https://bank.hackclub.com/stats', fetcher, {
     fallbackData: {
-      transactions_volume: '771911351', // As of 20-02-23
+      transactions_volume: '779707769', // As of 01-03-2023
       last_transaction_date: Date.now()
     }
-  })
+  }).data.transactions_volume
 
-  const transacted = renderMoney(data.transactions_volume).split('.');
+  useEffect(() => {
+    let observer = new IntersectionObserver(
+      (e) => {
+        if (e[0].isIntersecting) {
+          startMoneyAnimation(setBalance, transactedRaw, 2_500, formatMoney)
+        }
+      },
+      { threshold: 1.0 }
+    );
+    observer.observe(document.querySelector("#parent"));
+
+    return () => observer.disconnect();
+  }, [])
 
   return (
-    <Box>
+    <Box id='parent'>
       <Flex sx={{ flexDirection: 'column', alignItems: 'center' }}>
         <Text sx={{ fontSize: [3, 4] }}>
           So far we have enabled
         </Text>
-        <Text variant='title' color='green' sx={{
+        { !balance &&
+          <Text variant='title' color='green' sx={{
             color: 'green',
             fontSize: [5, 6]
-          }}
-        >
-          { transacted[0] }
-          <Text sx={{ fontSize: [3, 4] }}>.{ transacted[1] }</Text>
-        </Text>
-        <Text sx={{ fontSize: [3, 4] }}>
-          in transactions
-        </Text>
+            }}
+          >
+            ...
+          </Text>
+        }
+        { balance && <>
+          <Text variant='title' color='green' sx={{
+              color: 'green',
+              fontSize: [5, 6]
+            }}
+          >
+            { balance[0] }
+            <Text sx={{ fontSize: [3, 4] }}>.{ balance[1] }</Text>
+          </Text>
+          <Text sx={{ fontSize: [3, 4] }}>
+            in transactions
+          </Text>
+        </>}
       </Flex>
     </Box>
   )
