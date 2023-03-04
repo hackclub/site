e2cddfbc82be0b9c3cfbbc3c1c51ef4fb016d6c2
Malted <malted@malted.dev>
2023-03-04 18:28:05 +0000

    ✨ Rework success page

--- pages/bank/success.js
+++ pages/bank/success.js
@@ -1,77 +1,94 @@
-import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { useEffect, useRef, useState } from 'react'
-import { Container, Text, Flex, Link, Image } from 'theme-ui'
+import { useEffect } from 'react'
+import { Box, Button, Card, Container, Divider, Text, Link, Flex, Image } from 'theme-ui'
 import ForceTheme from '../../components/force-theme'
+import JSConfetti from 'js-confetti'
+import Icon from '../../components/icon'
+import FlexCol from '../../components/flex-col'
+
+function Option({ icon, label, link }) {
+  return (
+    <Button
+      variant='outline'
+      as='a'
+      href={link}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: 'fit-content',
+      }}
+    >
+      <Flex sx={{ alignItems: 'center', gap: [0, null, 1], px: 2 }}>
+        <Icon glyph={icon} sx={{ width: [32, null, 46], height: [32, null, 46] }} /> 
+        <Text sx={{ fontSize: [2, null, 3] }}>{ label }</Text>
+      </Flex>
+    </Button>
+  )
+}
 
 export default function ApplicationSuccess() {
   const router = useRouter()
 
-  const [counter, setCounter] = useState(15)
-
-  const interval = useRef()
-
   useEffect(() => {
-    interval.current = setInterval(() => {
-      setCounter(c => {
-        if (c <= 1) {
-          clearInterval(interval.current)
-          router.push('/bank')
-          return 0
-        } else {
-          return c - 1
-        }
-      })
-    }, 1000)
-
-    return () => {
-      clearInterval(interval.current)
-    }
+    const jsConfetti = new JSConfetti()
+    jsConfetti.addConfetti({
+      confettiColors: [ // Hack Club colours!
+        '#ec3750',
+        '#ff8c37',
+        '#f1c40f',
+        '#33d6a6',
+        '#5bc0de',
+        '#338eda',
+        '#a633d6',
+      ],
+    })
   }, [router])
-
-  const cancelRedirect = () => {
-    clearInterval(interval.current)
-    setCounter(null)
-  }
 
   return (
     <Container variant="copy">
       <ForceTheme theme="dark" />
-      <Flex
-        sx={{
-          flexDirection: 'column',
-          justifyContent: 'center',
-          textAlign: 'center',
-          pt: 6
-        }}
+      <FlexCol
+        height='100vh'
+        textAlign='center'
+        alignItems='center'
+        justifyContent='space-between'
+        py={4}
+        gap={4}
       >
-        <Image
-          src="https://cloud-5mgrt4f4s-hack-club-bot.vercel.app/0frame.svg"
-          alt="Dinosaur partying"
-          sx={{ width: '40%', mx: 'auto', mb: 4 }}
-        />
-        <Text variant="title" sx={{ textAlign: 'center' }}>
-          Thanks for applying!
-        </Text>
-        <Text variant="lead">
-          We’ll reach out to schedule your introductory call within 24 hours on
-          weekdays. If you have any questions about your application, please
-          don’t hesitate to reach out at{' '}
-          <Link as="a" href="mailto:bank@hackclub.com">
-            bank@hackclub.com
-          </Link>{' '}
-          or on the <Link href="/slack">Hack Club Slack</Link> in the{' '}
-          <strong>#bank</strong> channel.
-        </Text>
-        {counter !== null && (
-          <Flex sx={{ justifyContent: 'center' }}>
-            <Text sx={{ mr: 3 }}>Redirecting in {counter} seconds.</Text>
-            <Link sx={{ cursor: 'pointer' }} onClick={() => cancelRedirect()}>
-              Cancel
-            </Link>
+        <FlexCol gap={4} alignItems='center'>
+          <Image
+            src='/bank/apply/party-orpheus.svg'
+            alt="Dinosaur partying"
+            sx={{ width: '40%' }}
+          />
+          <FlexCol gap={2}>
+            <Text variant='title'>
+              Thanks for applying!
+            </Text>
+            <Text variant='lead'>
+              We’ll reach out to schedule your introductory call within 24 hours on
+              weekdays.
+            </Text>
+          </FlexCol>
+        </FlexCol>
+
+        <FlexCol gap={4} width='100%'>
+          <Text sx={{ fontSize: [3, null, 4] }}>Questions about your application?</Text>
+          <Flex sx={{ flexDirection: ['column', null, 'row'], justifyContent: 'space-evenly', alignItems: 'center', gap: [3, null, 0] }}>
+            <Option icon='email' label='Mail' link='mailto:bank@hackclub.com'>bank@hackclub.com</Option>
+            <Option icon='slack' label='Slack' link='https://hackclub.slack.com/channels/bank'>#bank</Option>
+            <Option icon='help' label='FAQ' link='https://bank.hackclub.com/faq'>FAQ</Option>
           </Flex>
-        )}
-      </Flex>
+        </FlexCol>
+
+        <Button as='a' href='/bank'>
+          <Flex sx={{ alignItems: 'center', px: [2, null, 3], py: 2 }}>
+            <Icon glyph='home' size={36} />
+            <Text sx={{ fontSize: 3 }}>Back to home</Text>
+          </Flex>
+        </Button>
+      </FlexCol>
     </Container>
   )
 }
