347863bf3fc79e6ec657d414fe782154e1e3e930
Malted <malted@malted.dev>
2023-03-05 16:18:19 +0000

    💄 Remove redundant style

--- components/bank/apply/form-container.js
+++ components/bank/apply/form-container.js
@@ -10,7 +10,6 @@ export default forwardRef(({ children }, ref) => {
                 height: '100%',
                 width: ['100%', null, null, '50ch'],
                 flex: '1',
-                // overflowX: 'visible',
                 overflowY: ['none', null, null, 'auto'],
                 pr: [0, null, '2ch'],
                 pl: [0, null, 1],
