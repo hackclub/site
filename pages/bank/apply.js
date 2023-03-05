dc02a8eb18c64697ca9c014c75315d488ccfd700
Malted <malted@malted.dev>
2023-03-05 16:20:14 +0000

    🔥 Remove address alert code

--- pages/bank/apply.js
+++ pages/bank/apply.js
@@ -12,14 +12,12 @@ import FormContainer from '../../components/bank/apply/form-container'
 import BankInfo from '../../components/bank/apply/bank-info'
 import OrganizationInfoForm from '../../components/bank/apply/org-form'
 import PersonalInfoForm from '../../components/bank/apply/personal-form'
-import AddressAlert from '../../components/bank/apply/address-alert'
 
 export default function Apply() {
   const router = useRouter()
   const [step, setStep] = useState(1)
   const formContainer = useRef()
   const [validationResult, setValidationResult] = useState()
-  const [shouldShowAddressAlert, setShouldShowAddressAlert] = useState(false)
 
   useEffect(() => {
     if (!router.isReady) return
@@ -49,9 +47,7 @@ export default function Apply() {
       <Box
         sx={{
           display: 'grid',
-          gridTemplateColumns: ['1fr', null, null, '1fr 1fr'],
-          gridTemplateRows: ['auto auto auto auto', null, '1fr 1fr 1fr'],
-          gap: '64px',
+          gap: 5,
           gridTemplateAreas: [
             '"title" "form" "form" "nav"',
             null,
@@ -59,7 +55,7 @@ export default function Apply() {
             '"title form" "title form" "nav form"',
           ],
           height: ['auto', null, null, '100vh'],
-          p: ['32px', '64px', null, '100px']
+          p: [4, 5]
         }}
       >
         <Box sx={{ gridArea: 'title' }}>
@@ -87,21 +83,19 @@ export default function Apply() {
             // mt: 5
           }}
         >
-          <NavButton isBack={true} form={formContainer} minStep={1} maxStep={3} />
+          <NavButton isBack={true} form={formContainer} />
           <NavButton
             isBack={false}
             form={formContainer}
-            minStep={1}
-            maxStep={3}
             clickHandler={async () => {
               // Validate the address
               if (step === 3) {
-                const key = atob('QUl6YVN5QXB4Wlo4LUVoXzZSZ0hVdTgtQkFPcHgzeGhmRjJ5SzlV')
-          
                 // Get the raw personal address input
                 const userAddress = sessionStorage.getItem('bank-signup-userAddress')
                 if (!userAddress) return false
 
+                //#region Address Validation API
+                const key = atob('QUl6YVN5QXB4Wlo4LUVoXzZSZ0hVdTgtQkFPcHgzeGhmRjJ5SzlV')
                 const res = await fetch(`https://addressvalidation.googleapis.com/v1:validateAddress?key=${key}`, {
                   method: 'POST',
                   headers: {
@@ -115,37 +109,23 @@ export default function Apply() {
                 })
                 const resJson = await res.json()
                 const { result } = resJson
+                //#endregion
                 setValidationResult(result)
 
-                if (!result) return false
-                console.info(result)
+                const addrComp = (type) =>
+                    result.address.addressComponents.find(el => el.componentType === type)?.componentName.text
+                const streetNumber = addrComp('street_number')
+                const road = addrComp('route')
+                const city = addrComp('postal_town') || addrComp('locality')
+                const state = addrComp('administrative_area_level_1')
+                const postalCode = addrComp('postal_code')
+                const country = addrComp('country')
 
-                if (result.address.missingComponentTypes || result.verdict.hasUnconfirmedComponents) {
-                  setShouldShowAddressAlert(true)
-                  return false
-                } else {
-                  /* If the address is valid, the returned address can be 
-                  inserted into the hidden fields. Let's change the backend 
-                  so that the user address is just a string? */
-
-                  /*TODO: Replace null with the returned address components.
-                  This could be tricky since the returned address components
-                  could not always be in the same order or format,
-                  so doing addressLine1=result[0]; addressLine2=result[1] ..etc
-                  would be unreliable. One workaround to simply accepting a string
-                  for the whole address would be using the address component types
-                  provided in the result to work out which to concat and in which
-                  fields to put the components. */
-                  sessionStorage.setItem('bank-signup-addressLine1', null)
-                  sessionStorage.setItem('bank-signup-addressLine2', null)
-                  sessionStorage.setItem('bank-signup-addressCity', null)
-                  sessionStorage.setItem('bank-signup-addressState', null)
-                  sessionStorage.setItem('bank-signup-addressZip', null)
-                  sessionStorage.setItem('bank-signup-addressCountry', null)
-
-                  setShouldShowAddressAlert(false)
-                  return true
-                }
+                sessionStorage.setItem('bank-signup-addressLine1', streetNumber ?? '' + (streetNumber && road ? ' ' : '') + road ?? '')
+                sessionStorage.setItem('bank-signup-addressCity', city ?? '')
+                sessionStorage.setItem('bank-signup-addressState', state ?? '')
+                sessionStorage.setItem('bank-signup-addressZip', postalCode ?? '')
+                sessionStorage.setItem('bank-signup-addressCountry', country ?? '')
               }
 
               return true
@@ -258,10 +238,6 @@ export default function Apply() {
 
 
       </Flex> */}
-      { shouldShowAddressAlert && <AddressAlert
-        validationResult={validationResult}
-        setShouldShowAddressAlert={setShouldShowAddressAlert}
-      /> }
       <Watermark />
     </>
   )
