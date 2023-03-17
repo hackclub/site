a0a0fd444dcc168ecbd3d2b6953c3f5bfe691280
Malted <malted@malted.dev>
2023-03-17 18:51:19 +0000

    ✨ Boop

--- pages/api/bank/apply.js
+++ pages/api/bank/apply.js
@@ -10,8 +10,8 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body
 
-    // https://bank.hackclub.com
-    await fetch('http://127.0.0.1:3005/api/v1/events/create_demo', {
+    // await fetch('http://127.0.0.1:3005/api/v1/events/create_demo', {
+    await fetch('https://bank.hackclub.com/api/v1/events/create_demo', {
       body: JSON.stringify({
         email: data.userEmail,
         name: data.eventName
@@ -26,30 +26,29 @@ export default async function handler(req, res) {
       .then(async r => {
         console.log(data)
 
-        // const application = await applicationsTable.create({
-        //   'First Name': data.firstName,
-        //   'Last Name': data.lastName,
-        //   'Email Address': data.userEmail,
-        //   'Phone Number': data.userPhone,
-        //   'Date of Birth': data.userBirthday,
-        //   'Event Name': data.eventName,
-        //   'Event Website': data.eventWebsite,
-        //   'Tell us about your event': data.eventDescription,
-        //   'Mailing Address': data.mailingAddress,
-        //   'Address Line 1': data.addressLine1,
-        //   'Address Line 2': data.addressLine2,
-        //   City: data.addressCity,
-        //   State: data.addressState,
-        //   'Zip Code': data.addressZip,
-        //   'Address Country': data.addressCountry,
-        //   Country: data.eventCountry,
-        //   'Event Location': data.eventLocation,
-        //   'Have you used Hack Club Bank for any previous events?':
-        //     data.returningUser,
-        //   'How did you hear about HCB?': data.referredBy,
-        //   Transparent: data.transparent,
-        //   'HCB account URL': `https://bank.hackclub.com/${r.slug}`
-        // })
+        await applicationsTable.create({
+          'First Name': data.firstName,
+          'Last Name': data.lastName,
+          'Email Address': data.userEmail,
+          'Phone Number': data.userPhone,
+          'Date of Birth': data.userBirthday,
+          'Event Name': data.eventName,
+          'Event Website': data.eventWebsite,
+          'Tell us about your event': data.eventDescription,
+          'Mailing Address': data.mailingAddress,
+          'Address Line 1': data.addressLine1,
+          'Address Line 2': data.addressLine2,
+          'City': data.addressCity,
+          'State': data.addressState,
+          'Zip Code': data.addressZip,
+          'Address Country': data.addressCountry,
+          'Country': data.eventCountry,
+          'Event Location': data.eventLocation,
+          'Have you used Hack Club Bank for any previous events?': data.returningUser,
+          'How did you hear about HCB?': data.referredBy,
+          'Transparent': data.transparent,
+          'HCB account URL': `https://bank.hackclub.com/${r.slug}`
+        })
         res.writeHead(302, { Location: '/bank/apply/success' }).end()
         console.log(r)
       })
