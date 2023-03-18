82efdf3f8a2c333c47a88be8c6cfc593a80124c8
Malted <malted@malted.dev>
2023-03-18 16:05:39 +0000

    Link to givebutter instead

--- components/bank/apply/bank-info.js
+++ components/bank/apply/bank-info.js
@@ -7,41 +7,58 @@ export default function BankInfo() {
     <Box>
       <FlexCol gap={5}>
         <FlexCol gap={4}>
-          <Text sx={{ fontSize: 36 }}>What Hack Club Bank <i>is</i></Text>
+          <Text sx={{ fontSize: 36 }}>
+            What Hack Club Bank <i>is</i>
+          </Text>
           <FlexCol gap={3} ml={3}>
-            <FlexCol gap={2} >
-              <Flex sx={{ alignItems: 'center', gap: 2 }}>
+            <FlexCol gap={2}>
+              <Flex sx={{ alignItems: "center", gap: 2 }}>
                 <Link
-                  color='white'
-                  href='https://en.wikipedia.org/wiki/501(c)(3)_organization'
+                  color="white"
+                  href="https://givebutter.com/blog/fiscal-sponsorship"
                   sx={{ fontSize: 3 }}
                 >
                   A fiscal sponsor
                 </Link>
-                <Icon glyph='external' />
+                <Icon glyph="external" />
               </Flex>
-              <Text sx={{ color: 'muted' }}>This means you won’t have to pay tax to the government on the funds you raise.</Text>
+              <Text sx={{ color: "muted" }}>
+                This means you won’t have to pay tax to the government on the
+                funds you raise.
+              </Text>
             </FlexCol>
             <FlexCol gap={2}>
               <Text sx={{ fontSize: 3 }}>A card issuer</Text>
-              <Text sx={{ color: 'muted' }}>Each team member can have their own physical card to spend your organization’s funds.</Text>
+              <Text sx={{ color: "muted" }}>
+                Each team member can have their own physical card to spend your
+                organization’s funds.
+              </Text>
             </FlexCol>
           </FlexCol>
         </FlexCol>
         <FlexCol gap={4}>
-          <Text sx={{ fontSize: 36 }}>What Hack Club Bank <i>is not</i></Text>
+          <Text sx={{ fontSize: 36 }}>
+            What Hack Club Bank <i>is not</i>
+          </Text>
           <FlexCol gap={3} ml={3}>
             <FlexCol gap={2}>
               <Text sx={{ fontSize: 3 }}>A traditional bank</Text>
-              <Text sx={{ color: 'muted' }}>You will not get a routing or account number.  Instead, you’ll get a donations page and invoicing system, through the Hack Club Bank dashboard.</Text>
+              <Text sx={{ color: "muted" }}>
+                You will not get a routing or account number. Instead, you’ll
+                get a donations page and invoicing system, through the Hack Club
+                Bank dashboard.
+              </Text>
             </FlexCol>
             <FlexCol gap={2}>
               <Text sx={{ fontSize: 3 }}>For-profit</Text>
-              <Text sx={{ color: 'muted' }}>If you’re a for-profit entity, then Bank is not for you. Consider setting up a business.</Text>
+              <Text sx={{ color: "muted" }}>
+                If you’re a for-profit entity, then Bank is not for you.
+                Consider setting up a business.
+              </Text>
             </FlexCol>
           </FlexCol>
         </FlexCol>
       </FlexCol>
     </Box>
-  )
+  );
 }
