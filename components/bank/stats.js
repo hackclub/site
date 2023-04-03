90d34dc414b4b26a4f11006dd13a8224ee8b3ac5
Malted <malted@malted.dev>
2023-04-03 11:49:51 +0100

    🐛 Fix stats view

--- components/bank/stats.js
+++ components/bank/stats.js
@@ -1,19 +1,16 @@
 import { Text, Box, Flex } from 'theme-ui'
 import { useEffect, useState } from 'react'
-import useSWR from 'swr'
-import fetcher from '../../lib/fetcher'
 
-function startMoneyAnimation(setBalance, amount, duration = 2_000, moneyFormatter) {
-  const easeInOutExpo = (x) =>
-		x === 0
-			? 0
-			: x === 1
-			? 1
-			: x < 0.5
-			? Math.pow(2, 20 * x - 10) / 2
-			: (2 - Math.pow(2, -20 * x + 10)) / 2;
-
-  
+const easeInOutExpo = (x) =>
+  x === 0
+    ? 0
+    : x === 1
+    ? 1
+    : x < 0.5
+    ? Math.pow(2, 20 * x - 10) / 2
+      : (2 - Math.pow(2, -20 * x + 10)) / 2;
+    
+function startMoneyAnimation(setBalance, amount, duration = 2_000, moneyFormatter) {  
   const startTime = performance.now();
 
   function animate() {
@@ -41,19 +38,24 @@ function formatMoney(amount) {
 }
 
 const Stats = () => {
-  const [balance, setBalance] = useState() // A formatted balance string, split by decimal
-
-  const transactedRaw = useSWR('https://bank.hackclub.com/stats', fetcher, {
-    fallbackData: {
-      transactions_volume: '779707769', // As of 01-03-2023
-      last_transaction_date: Date.now()
-    }
-  }).data.transactions_volume
+  const [transactedRaw, setTransactedRaw] = useState() // The raw amount transacted (in cents).
+  const [balance, setBalance] = useState(0) // A formatted balance string, split by decimal  
 
   useEffect(() => {
+    if (!transactedRaw) {
+      fetch('https://bank.hackclub.com/stats')
+        .then(res => res.json())
+        .then(data => setTransactedRaw(data.transactions_volume))
+        .catch(err => {
+          console.error(err)
+          setTransactedRaw(830796389)
+        })
+    }
+
     let observer = new IntersectionObserver(
       (e) => {
         if (e[0].isIntersecting) {
+          console.info('intersecting')
           startMoneyAnimation(setBalance, transactedRaw, 2_500, formatMoney)
         }
       },
@@ -62,7 +64,7 @@ const Stats = () => {
     observer.observe(document.querySelector("#parent"));
 
     return () => observer.disconnect();
-  }, [])
+  }, [transactedRaw])
 
   return (
     <Box id='parent'>
@@ -70,7 +72,7 @@ const Stats = () => {
         <Text sx={{ fontSize: [3, 4] }}>
           So far we have enabled
         </Text>
-        { balance ?
+        { transactedRaw ?
           <>
             <Text variant='title' color='green' sx={{
                 color: 'green',
@@ -80,9 +82,6 @@ const Stats = () => {
               { balance[0] }
               <Text sx={{ fontSize: [3, 4] }}>.{ balance[1] }</Text>
             </Text>
-            <Text sx={{ fontSize: [3, 4] }}>
-              in transactions
-            </Text>
           </>
           :
           <Text variant='title' color='green' sx={{
@@ -90,6 +89,7 @@ const Stats = () => {
             fontSize: [5, 6]
           }}>...</Text>  
         }
+        <Text sx={{ fontSize: [3, 4] }}>in transactions</Text>
       </Flex>
     </Box>
   )
