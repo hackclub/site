d0dc739236bb3bd671c196515d35fc47e522923d
Malted <malted@malted.dev>
2023-04-06 22:58:08 +0100

    🚨 Add emoji to deps array

--- components/posts/emoji.js
+++ components/posts/emoji.js
@@ -30,21 +30,22 @@ export const EmojiImg = ({ name, ...props }) => (
 const CustomEmoji = memo(function CustomEmoji({ name }) {
   const emoji = stripColons(name)
   let [image, setImage] = useState()
+
   useEffect(() => {
-    try {
-      fetch('https://scrapbook.hackclub.com/api/emoji')
-        .then(r => r.json())
-        .then(emojis => {
-          if (emojis[emoji]) {
-            setImage(emojis[emoji])
-            return
-          }
-          setImage(
-            'https://emoji.slack-edge.com/T0266FRGM/parrot/c9f4fddc5e03d762.gif'
-          )
-        })
-    } catch (e) {}
-  }, [])
+    fetch('https://scrapbook.hackclub.com/api/emoji')
+      .then(r => r.json())
+      .then(emojis => {
+        if (emojis[emoji]) {
+          setImage(emojis[emoji])
+          return
+        }
+        setImage(
+          'https://emoji.slack-edge.com/T0266FRGM/parrot/c9f4fddc5e03d762.gif'
+        )
+      })
+      .catch(console.error)
+  }, [emoji])
+
   return image ? (
     <EmojiImg className="post-emoji" src={image} name={emoji} />
   ) : (
