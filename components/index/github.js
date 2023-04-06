0ad0fd7f7273a24b2807e34fcd0ac608220f30d7
Malted <malted@malted.dev>
2023-04-07 00:36:43 +0100

    ♻️ Refactor

--- components/index/github.js
+++ components/index/github.js
@@ -1,4 +1,4 @@
-import { Badge, Link, Text } from 'theme-ui'
+import { Badge, Flex, Link, Image, Text, Box } from 'theme-ui'
 import RelativeTime from 'react-relative-time'
 
 export default function GitHub({
@@ -11,130 +11,89 @@ export default function GitHub({
   message,
   ...props
 }) {
+  if (!user) return null
+
   return (
     <Badge
       variant="pill"
       bg="snow"
       sx={{
         color: 'black',
-        fontWeight: '400 !important',
+        fontWeight: '400',
         zIndex: 4,
         px: '4px !important',
         py: '2px !important',
         // my: [1, 1, 2],
         height: 'fit-content',
-        width: 'fit-content'
+        width: ['fit-content', '100%'],
       }}
       {...props}
     >
       <Link
-        href="https://github.com/hackclub"
+        href='https://github.com/hackclub'
+        target='_blank'
+        rel='noopener'
         sx={{
           textDecoration: 'none',
           color: 'inherit',
-          fontWeight: '400 !important',
           display: 'flex',
           fontSize: ['11px', '11px', '14px'],
           height: ['15px', '15px', '25px'],
           position: 'relative',
-          alignItems: 'center'
+          alignItems: 'center',
+          justifyContent: 'flex-start',
         }}
-        target="_blank"
-        rel="noopener"
       >
-        {user ? (
-          user !== 'dependabot[bot]' ? (
-            user !== 'github-actions[bot]' ? (
-              <img
-                alt=''
-                src={img}
-                sx={{ borderRadius: '100%', height: '90%', mr: 2 }}
-              />
-            ) : (
-              <></>
-            )
-          ) : (
-            <></>
-          )
-        ) : (
-          <></>
-        )}
-        {user ? (
-          user !== 'dependabot[bot]' ? (
-            user !== 'github-actions[bot]' ? (
-              <Text
-                sx={{
-                  mr: 2,
-                  textOverflow: 'ellipsis',
-                  maxWidth: '80px',
-                  display: 'inline-block',
-                  overflow: 'hidden',
-                  whiteSpace: 'nowrap',
-                  display: [
-                    'none',
-                    'inline-block',
-                    'inline-block',
-                    'inline-block'
-                  ]
-                }}
-              >
-                {user}
+        {!user.endsWith('[bot]') && (
+          <>
+            <Image
+              alt='GitHub user avatar'
+              src={img}
+              sx={{ borderRadius: '100%', height: '90%', mr: 2 }}
+            />
+            <Text
+              sx={{
+                mr: 2,
+                textOverflow: 'ellipsis',
+                maxWidth: '80px',
+                display: 'inline-block',
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                display: [
+                  'none',
+                  'inline-block',
+                  'inline-block',
+                  'inline-block'
+                ]
+              }}
+            >
+              {user}
               </Text>
-            ) : (
-              <></>
-            )
-          ) : (
-            <></>
-          )
-        ) : (
-          <></>
-        )}
-        {user ? (
-          user !== 'dependabot[bot]' ? (
-            user !== 'github-actions[bot]' ? (
-              <Text
-                sx={{
-                  textOverflow: 'ellipsis',
-                  maxWidth: '150px',
-                  display: 'inline-block',
-                  overflow: 'hidden',
-                  whiteSpace: 'nowrap'
-                }}
-              >
-                {message}
-              </Text>
-            ) : (
-              <></>
-            )
-          ) : (
-            <></>
-          )
-        ) : (
-          <></>
-        )}
-        {user ? (
-          user !== 'dependabot[bot]' ? (
-            user !== 'github-actions[bot]' ? (
-              <Text
-                as="span"
-                sx={{
-                  fontSize: ['8px', '8px', '10px'],
-                  color: 'inherit',
-                  mx: 2,
-                  float: 'right'
-                }}
-              >
-                <RelativeTime value={time} titleformat="iso8601" />
-              </Text>
-            ) : (
-              <></>
-            )
-          ) : (
-            <></>
-          )
-        ) : (
-          <></>
-        )}
+
+          <Text
+            sx={{
+              textOverflow: 'ellipsis',
+              maxWidth: '150px',
+              display: 'inline-block',
+              overflow: 'hidden',
+              whiteSpace: 'nowrap'
+            }}
+          >
+            {message}
+          </Text>
+
+          <Text
+            as="span"
+            sx={{
+              fontSize: ['8px', '8px', '10px'],
+              color: 'inherit',
+              mx: 2,
+              ml: 'auto',
+            }}
+          >
+            <RelativeTime value={time} titleformat="iso8601" />
+          </Text>
+        </>)}
       </Link>
     </Badge>
   )
