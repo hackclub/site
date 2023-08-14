68022195fbc458482d7585f608d94931f3bfeb66
Malted <malted@malted.dev>
2023-08-14 23:49:30 +0100

    ♻️ Refactor & fix errors in GitHub API helper

--- pages/api/github.js
+++ pages/api/github.js
@@ -1,48 +1,35 @@
+const isRelevantEventType = (type) => ['PushEvent', 'PullRequestEvent', 'WatchEvent'].includes(type);
+
+const getMessage = (type, payload, repo) => {
+  switch (type) {
+    case 'PushEvent':
+      return payload.commits?.[0]?.message || 'No commit message';
+    case 'PullRequestEvent':
+      return payload.pull_request.title;
+    case 'WatchEvent':
+      return `starred ${repo.name}`;
+    default:
+      return null;
+  }
+}
+
 export async function fetchGitHub() {
-  let initialGitHubData = await fetch(
-    'https://api.github.com/orgs/hackclub/events'
-  ).then(r => r.json())
+  const initialGitHubData = await fetch('https://api.github.com/orgs/hackclub/events').then(r => r.json());
 
-  let gitHubData = initialGitHubData.map(x => ({
-    type: x.type,
-    user:
-      x.type === 'PushEvent'
-        ? x.actor.login
-        : x.type === 'PullRequestEvent'
-        ? x.actor.login
-        : x.type === 'WatchEvent'
-        ? x.actor.login
-        : null,
-    userImage:
-      x.type === 'PushEvent'
-        ? x.actor.avatar_url
-        : x.type === 'PullRequestEvent'
-        ? x.actor.avatar_url
-        : x.type === 'WatchEvent'
-        ? x.actor.avatar_url
-        : null,
-    message:
-      x.type === 'PushEvent'
-        ? x.payload.commits[0]?.message
-        : x.type === 'PullRequestEvent'
-        ? x.payload.pull_request.title
-        : x.type === 'WatchEvent'
-        ? `starred ${x.repo.name}`
-        : null,
-    time: x.created_at
-  }))
+  const gitHubData = initialGitHubData
+    .filter(({ type }) => isRelevantEventType(type))
+    .map(({ type, actor, payload, repo, created_at }) => ({
+      type,
+      user: actor.login,
+      userImage: actor.avatar_url,
+      message: getMessage(type, payload, repo),
+      time: created_at
+    }));
 
-  gitHubData = gitHubData.filter(
-    x =>
-      x.type === 'PushEvent' ||
-      x.type === 'PullRequestEvent' ||
-      x.type === 'WatchEvent'
-  )
-
-  return gitHubData
+  return gitHubData;
 }
 
 export default async function github(req, res) {
-  const git = await fetchGitHub(req, res)
-  res.json(git)
+  const git = await fetchGitHub(req, res);
+  res.json(git);
 }
