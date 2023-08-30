b382cd09361e8818f4029cc30d05bc13b769d41c
Malted <malted@malted.dev>
2023-08-30 18:06:58 +0100

    ✨ Add accommodations field

--- pages/api/bank/apply.js
+++ pages/api/bank/apply.js
@@ -50,7 +50,8 @@ export default async function handler(req, res) {
             data.transparent === 'true' ? 'Yes, please!' : 'No, thanks.',
           'HCB account URL': `https://bank.hackclub.com/${r.slug}`,
           'Contact Option': data.contactOption,
-          'Slack Username': data.slackUsername
+          'Slack Username': data.slackUsername,
+		  'Accommodations': data.accommodations,
         })
         res.writeHead(302, { Location: '/bank/apply/success' }).end()
       })
