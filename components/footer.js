65d314570b564f767fdda6cbdf6cd2724b862ca2
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2023-09-11 14:08:29 -0400

    added directory

--- components/footer.js
+++ components/footer.js
@@ -112,6 +112,7 @@ const Footer = ({ dark = false, children, ...props }) => (
           <Link href="https://events.hackclub.com/">Community Events</Link>
           <Link href="https://jams.hackclub.com/">Jams</Link>
           <Link href="https://toolbox.hackclub.com/">Toolbox</Link>
+          <Link href="https://directory.hackclub.com/">Clubs Directory</Link>
           <Link href="https://hackclub.com/conduct/">Code of Conduct</Link>
         </Box>
         <Box sx={{ gridColumn: ['span 2', 'span 1'] }}>
