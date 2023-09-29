ee979701f7ea9936e05647e9a7d9abdfedfd4d61
Toby Brown <hi@tobyb.dev>
2023-09-29 20:17:17 +0100

    further fancifying of the mail card

--- components/mail-card.js
+++ components/mail-card.js
@@ -1,15 +1,19 @@
 import { Card, Text } from "theme-ui";
+import Link from "next/link";
 
-export default function MailCard({ mail }) {
-  return (
-    <Card className="mail-card" sx={{ maxWidth: "1500px" }}>
-      <Text as="h1" sx={{ textTransform: "uppercase" }}>{/*{mail.subject || "No Subject"}*/} No Subject</Text>
-        <Text as="p">
-            26/04/2021 — To Orpheus, From Hack Club
-        </Text>
-      <Text as="h2" sx={{ fontWeight: 'normal' }}>
-          Incididunt ipsum mollit occaecat et est magna pariatur elit nulla. Commodo aliqua aliquip exercitation minim veniam aliqua aliqua qui eiusmod do.
-      </Text>
-    </Card>
+export default function MailCard(mail) {
+    const body = mail.body.length > 250 ? mail.body.substring(0, 100) + "..." : mail.body;
+    return (
+        <Link href="#" sx={{ cursor: "pointer" }}>
+            <Card className="mail-card" sx={{ maxWidth: "1500px" }}>
+              <Text as="h1" sx={{ textTransform: "uppercase" }}>{mail.subject || "No Subject"}</Text>
+                <Text as="p">
+                    {mail.date} — To Orpheus, From Hack Club
+                </Text>
+              <Text as="h2" sx={{ fontWeight: 'normal' }}>
+                  {body || "We're no strangers to love. You know the rules and so do I."}
+              </Text>
+            </Card>
+        </Link>
   );
 }
