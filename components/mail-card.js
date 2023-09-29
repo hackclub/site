805e82c96dc5671d15bbd15e0bdefc8e891d14b8
Toby Brown <hi@tobyb.dev>
2023-09-29 22:16:40 +0100

    experimenting with different backgrounds + start of pattern!

--- components/mail-card.js
+++ components/mail-card.js
@@ -1,11 +1,15 @@
 import { Card, Text } from "theme-ui";
 import Link from "next/link";
+import Image from "next/image";
+
+import pattern from "../public/../public/home/letter-pattern.svg"
 
 export default function MailCard(mail) {
     const body = mail.body.length > 250 ? mail.body.substring(0, 100) + "..." : mail.body;
     return (
-        <Link href="#" sx={{ cursor: "pointer" }}>
-            <Card className="mail-card" sx={{ maxWidth: "1500px" }}>
+        <Link href="#">
+            <Card className="mail-card" sx={{ maxWidth: "1500px", cursor: "pointer" }}>
+                <Image src={pattern} alt="red and blue letter pattern" sx={{ maxWidth: "100%" }} />
               <Text as="h1" sx={{ textTransform: "uppercase" }}>{mail.subject || "No Subject"}</Text>
                 <Text as="p">
                     {mail.date} — To Orpheus, From Hack Club
