baf8d2cb73d6610d44e7e65f6d231cbc801c80fd
Toby Brown <hi@tobyb.dev>
2023-09-30 16:06:41 +0100

    further fanciness

--- components/mail-card.js
+++ components/mail-card.js
@@ -1,22 +1,21 @@
-import { Card, Text } from "theme-ui";
+import {Box, Card, Text} from "theme-ui";
 import Link from "next/link";
-import Image from "next/image";
-
-import pattern from "../public/../public/home/letter-pattern.svg"
 
 export default function MailCard(mail) {
     const body = mail.body.length > 250 ? mail.body.substring(0, 100) + "..." : mail.body;
     return (
-        <Link href="#">
+        <Link href={mail.link || "/newsletter"}>
             <Card className="mail-card" sx={{ maxWidth: "1500px", cursor: "pointer" }}>
-                <Image src={pattern} alt="red and blue letter pattern" sx={{ maxWidth: "100%" }} />
-              <Text as="h1" sx={{ textTransform: "uppercase" }}>{mail.subject || "No Subject"}</Text>
+                <Box>
+                    <Box sx={{ width: "100%", height: "20px", backgroundImage: "url('https://cloud-jopr4l3j5-hack-club-bot.vercel.app/0letter-pattern.svg')", backgroundRepeat: 'repeat-x', }} />
+                    <Text as="h1" sx={{ textTransform: "uppercase" }}>{mail.subject || "No Subject"}</Text>
                 <Text as="p">
                     {mail.date} — To Orpheus, From Hack Club
                 </Text>
               <Text as="h2" sx={{ fontWeight: 'normal' }}>
                   {body || "We're no strangers to love. You know the rules and so do I."}
               </Text>
+                </Box>
             </Card>
         </Link>
   );
