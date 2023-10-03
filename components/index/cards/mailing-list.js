efb88cd958dabf08e0b6e878874bc3fd8e084869
Toby Brown <hi@tobyb.dev>
2023-10-03 18:59:13 +0100

    mobile support!

--- components/index/cards/mailing-list.js
+++ components/index/cards/mailing-list.js
@@ -1,14 +1,6 @@
 import Icon from "@hackclub/icons";
 import { useRef, useState } from "react";
-import {
-  Box,
-  Input,
-  Button,
-  Text,
-  Card,
-  Flex,
-  Grid,
-} from "theme-ui";
+import { Box, Input, Button, Text, Card, Flex, Grid } from "theme-ui";
 import BGImg from "../../background-image";
 import FooterImgFile from "../../../public/outernet/hack.jpg";
 import Link from "next/link";
@@ -26,6 +18,7 @@ const Loading = () => (
       width: "16px",
       height: "16px",
       animation: "spin 2s linear infinite",
+      mr: "5px",
       "@keyframes spin": {
         "0%": { transform: "rotate(0deg)" },
         "100%": { transform: "rotate(360deg)" },
@@ -73,122 +66,128 @@ const MailingList = () => {
           position: "relative",
           zIndex: 2,
           backdropFilter: "blur(8px)",
-          display: "flex",
-            gridGap: 5,
         }}
       >
-        <Flex
-          sx={{
+        <Flex sx={{ flexDirection: ["column", "row"], gridGap: [0, 5] }}>
+          <Flex
+            sx={{
               placeItems: "center",
               justifyContent: "center",
-            alignItems: ["left", "left", "center"],
-            flexDirection: "column",
-            gap: "10px",
+              alignItems: ["left", "left", "center"],
+              flexDirection: "column",
+              gap: "10px",
               width: "75%",
-          }}
-        >
-          <Box>
-            <Text
-              variant="title"
-              sx={{
-                fontSize: [4, "36px", "42px", 6],
-                zIndex: 2,
-                textAlign: "left",
-              }}
-            >
-              Join the newsletter
-            </Text>
-            <Text
-              sx={{
-                color: "darkless",
-                mt: 2,
-                fontSize: 3,
-                textAlign: "left",
-              }}
-              as="p"
-            >
-              We’ll send you an email no more than once a month, when we work on
-              something cool for you. Check out{" "}
-              <Link href="/newsletter">past issues</Link>.
-            </Text>
-          </Box>
-          <Grid
-            as="form"
-            ref={formRef}
-            onSubmit={handleSubmit}
-            gap={[2, 3]}
-            sx={{
-              textAlign: "center",
-              alignItems: "end",
-              input: { bg: "sunken" },
-              width: "100%",
             }}
           >
-            <Box sx={{ width: "100%" }}>
-              <Input
-                autofillBackgroundColor="highlight"
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Your Name"
-                required
+            <Box>
+              <Text
+                variant="title"
                 sx={{
-                  width: "100%",
-                  textAlign: "center",
-                  fontSize: 2,
+                  fontSize: [4, "36px", "42px", 6],
+                  zIndex: 2,
+                  textAlign: "left",
                 }}
-              />
+              >
+                Join the newsletter
+              </Text>
+              <Text
+                sx={{
+                  color: "darkless",
+                  mt: 2,
+                  fontSize: 3,
+                  textAlign: "left",
+                }}
+                as="p"
+              >
+                We’ll send you an email no more than once a month, when we work
+                on something cool for you. Check out{" "}
+                <Link href="/newsletter">past issues</Link>.
+              </Text>
             </Box>
-            <div>
-              <Input
-                autofillBackgroundColor="highlight"
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Your Email"
-                required
-                sx={{
-                  width: "100%",
-                  textAlign: "center",
-                  fontSize: 2,
-                }}
-              />
-            </div>
-            <Button type="submit" sx={{ mt: [2, 0], fontSize: 2 }}>
+            <Grid
+              as="form"
+              ref={formRef}
+              onSubmit={handleSubmit}
+              gap={[2, 3]}
+              sx={{
+                textAlign: "center",
+                alignItems: "end",
+                input: { bg: "sunken" },
+                width: "100%",
+              }}
+            >
+              <Box sx={{ width: "100%" }}>
+                <Input
+                  autofillBackgroundColor="highlight"
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Your Name"
+                  required
+                  sx={{
+                    width: "100%",
+                    textAlign: "center",
+                    fontSize: 2,
+                  }}
+                />
+              </Box>
+              <div>
+                <Input
+                  autofillBackgroundColor="highlight"
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Your Email"
+                  required
+                  sx={{
+                    width: "100%",
+                    textAlign: "center",
+                    fontSize: 2,
+                  }}
+                />
+              </div>
+              <Button type="submit" sx={{ mt: [2, 0], fontSize: 2 }}>
                 {submitting ? (
-                    <>
-                        <Loading />
-                        {' '} Subscribe
-                    </>
+                  <>
+                    <Loading /> Subscribe
+                  </>
+                ) : submitted ? (
+                  <>
+                    <Icon glyph="send" /> You're on the list!
+                  </>
                 ) : (
-                    submitted ? (
-                        <>
-                            <Icon glyph="send" />
-                            {' '} You're on the list!
-                        </>
-                    ) : (
-                        "Subscribe"
-                    )
+                  "Subscribe"
                 )}
-            </Button>
-          </Grid>
+              </Button>
+            </Grid>
             {/*{submitted && (
                 <Alert variant="primary" sx={{ bg: "green", mt: [2, 3] }}>
                     <Icon glyph="send" />
                     <Text sx={{ ml: 2 }}>You're on the list!</Text>
                 </Alert>
             )}*/}
+          </Flex>
+          <Box
+            sx={{
+              display: "grid",
+              gridGap: 4,
+              width: "100%",
+            }}
+          >
+            <MailCard
+              subject="👁️ What’s up at Hack Club? 👁️"
+              date="July 2022"
+              body="Hey friends! I'm Ishan (@Ishan), I'm 17, and I'm a Hack Clubber emailing you from Vermont! This email is a 5-minute monthly rundown of what your fellow Hack Clubbers have been up to this past month! You're receiving this email because you're in the Hack Club Slack."
+            />
+            <MailCard
+              subject="Community Newsletter"
+              date="September 2022"
+              body={`Hey friends! Sam, Hack Clubber from Singapore, here. The last few weeks have been pretty busy at Hack Club, and this is a rundown of what your fellow Hack Clubbers have been up to lately. To start, last week, we shared 🎬 The Assemble Documentary: a film on Assemble to help answer the question "what is a hackathon?". It showcases the magical experience of attending a hackathon, and our hope is that it can help organisers share what’s in store for attendees at their hackathons.`}
+            />
+          </Box>
         </Flex>
-        <Box sx={{
-            display: "grid",
-            gridGap: 4,
-            width: "100%"
-        }}>
-            <MailCard subject="👁️ What’s up at Hack Club? 👁️" date="July 2022" body="Hey friends! I'm Ishan (@Ishan), I'm 17, and I'm a Hack Clubber emailing you from Vermont! This email is a 5-minute monthly rundown of what your fellow Hack Clubbers have been up to this past month! You're receiving this email because you're in the Hack Club Slack."/>
-            <MailCard subject="Community Newsletter" date="September 2022" body={`Hey friends! Sam, Hack Clubber from Singapore, here. The last few weeks have been pretty busy at Hack Club, and this is a rundown of what your fellow Hack Clubbers have been up to lately. To start, last week, we shared 🎬 The Assemble Documentary: a film on Assemble to help answer the question "what is a hackathon?". It showcases the magical experience of attending a hackathon, and our hope is that it can help organisers share what’s in store for attendees at their hackathons.`}/>
-        </Box>
       </Card>
-        {/*<Box
+      {/*<Box
             sx={{
                 position: "absolute",
                 display: "block",
@@ -200,12 +199,12 @@ const MailingList = () => {
                 background: "linear-gradient(-180deg, #f1c40f 14%, #ff8c37 82%)",
             }}
         />*/}
-       <BGImg
-            src={FooterImgFile}
-            alt="Hack Clubbers gather in the great outdoors of Cabot, VT, for an experience unlike any other: Outernet. 📸 Photo by Matt Gleich, Hack Clubber in NH!"
-            priority
-            gradient="linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.45))"
-        />
+      <BGImg
+        src={FooterImgFile}
+        alt="Hack Clubbers gather in the great outdoors of Cabot, VT, for an experience unlike any other: Outernet. 📸 Photo by Matt Gleich, Hack Clubber in NH!"
+        priority
+        gradient="linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.45))"
+      />
     </Box>
   );
 };
