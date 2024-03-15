3efd61eb55a451762373ebb81321f0e83bf8095c
Max Wofford <max@maxwofford.com>
2024-03-15 16:44:05 -0400

    Apply suggestions from code review
    Co-authored-by: Caleb Denio <cjdenio44@gmail.com>

--- components/onboard/youtube-video.js
+++ components/onboard/youtube-video.js
@@ -1,15 +1,17 @@
 const YoutubeVideo = ({
   'youtube-id': youtubeID = 'dQw4w9WgXcQ',
-  list=null,
-  title="YouTube video player",
-  width=null,
-  height=null,
+  list,
+  title = "YouTube video player",
+  width,
+  height,
 }) => {
-  const src = new URL(`https://www.youtube.com/embed/${youtubeID}`)
-
-  if (list) {
-    src.searchParams.set('list', list)
-  }
+  const src = useMemo(() => {
+    const url = new URL(`https://www.youtube.com/embed/${youtubeID}`)
+    if (list) {
+      url.searchParams.set('list', list)
+    }
+    return url
+  }, [youtubeID, list])
 
   const allowlist = [
     'accelerometer',
