4eca7f671be5938ac671fca83db8bd4088e20218
Gaurav Pandey <yednapg@gmail.com>
2024-03-24 19:58:57 +0530

    onboard announcement

--- pages/index.js
+++ pages/index.js
@@ -203,10 +203,10 @@ function Page({
             gradient="linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.45))"
           />
           <Announcement
-            copy="AMA w/ Apple PCB Designers"
-            caption="Join a call with two of the designers behind the iPhone's PCB!"
-            href="https://hack.club/apple-ama-invite"
-            iconLeft="event-add"
+            copy="Hop OnBoard and create your first PCB"
+            caption="Join 1,000 others to create your first circuit board."
+            href="https://hackclub.com/onboard/"
+            iconLeft="idea"
             color="primary"
           />
           <Box
