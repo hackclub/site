a08743ea9c46da7cd8e866af898aa658f784a6f9
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-03-25 10:24:38 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {
+ {
   Badge,
   Box,
   Button,
@@ -204,9 +204,9 @@ function Page({
             gradient="linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.45))"
           />
           <Announcement
-            copy="AMA w/ Apple PCB Designers"
-            caption="Join a call with two of the designers behind the iPhone's PCB!"
-            href="https://hack.club/apple-ama-invite"
+            copy="Hop OnBoard and create your first PCB"
+            caption="Join 1,000 others to create your first circuit board."
+            href="https://hackclub.com/onboard/"
             iconLeft="event-add"
             color="primary"
           />
