79b903600d29cff5b84c2bd620aebd4ade8b0620
Max Wofford <max@maxwofford.com>
2024-04-03 18:55:58 -0400

    Fix bin footer formatting

--- pages/bin.js
+++ pages/bin.js
@@ -123,33 +123,34 @@ export default function Bin() {
           </Box>
         </Container>
       </Box>
-      <Box sx={{ a: { color: 'blue' }, pb: 4 }}>
-        <Heading as="h3" variant="subheadline" mb={2}>
-          A project by <a href="https://hackclub.com/">Hack Club</a>.
-        </Heading>
-        <Text as="p" variant="caption" mb={3} sx={{ width: ['85%', '75%', '60%'] }}>
-          Hack Club is a registered 501(c)3 nonprofit organization that supports a
-          network of 20k+ technical high schoolers. We believe you learn best by
-          building so we're removing barriers to hardware access so any teenager can
-          explore. In the past few years, we've{' '}
-          <Link href="https://winter.hackclub.com" target="_blank">
-            partnered with GitHub to give away $50k of hardware
-          </Link>
-          ,{' '}
-          <Link
-            href="https://github.com/hackclub/the-hacker-zephyr"
-            target="_blank"
-          >
-            hosted the world's longest hackathon on land
-          </Link>
-          , and{' '}
-          <Link href="https://onboard.hackclub.com" target="_blank">
-            fabricated custom PCBs designed by <OnboardCount /> teenagers
-          </Link>
-          .
-        </Text>
-      </Box>
-      <Footer />
+      <Footer>
+        <Box sx={{ a: { color: 'blue' }, pb: 4 }}>
+          <Heading as="h3" variant="subheadline" mb={2}>
+            A project by <a href="https://hackclub.com/">Hack Club</a>.
+          </Heading>
+          <Text as="p" variant="caption" mb={3} sx={{ width: ['85%', '75%', '60%'] }}>
+            Hack Club is a registered 501(c)3 nonprofit organization that supports a
+            network of 20k+ technical high schoolers. We believe you learn best by
+            building so we're removing barriers to hardware access so any teenager can
+            explore. In the past few years, we've{' '}
+            <Link href="https://winter.hackclub.com" target="_blank">
+              partnered with GitHub to give away $50k of hardware
+            </Link>
+            ,{' '}
+            <Link
+              href="https://github.com/hackclub/the-hacker-zephyr"
+              target="_blank"
+            >
+              hosted the world's longest hackathon on land
+            </Link>
+            , and{' '}
+            <Link href="https://onboard.hackclub.com" target="_blank">
+              fabricated custom PCBs designed by <OnboardCount /> teenagers
+            </Link>
+            .
+          </Text>
+        </Box>
+      </Footer>
     </>
   )
 }
