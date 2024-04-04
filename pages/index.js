190630ad0fd7ed28906a223dff31149ab8785b87
Max Wofford <max@maxwofford.com>
2024-04-04 16:29:44 -0400

    Add updated announcement card on home page

--- pages/index.js
+++ pages/index.js
@@ -203,10 +203,10 @@ function Page({
             gradient="linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.45))"
           />
 	  <Announcement
-            copy="Hop OnBoard and create your first PCB"
-            caption="Join 1,000 others to create your first circuit board."
-            href="https://hackclub.com/onboard/"
-            iconLeft="idea"
+            copy="Hey coder, want to learn electronics?"
+            caption="RSVP for the release of our free starter kit. Also, get stickers!"
+            href="https://hackclub.com/bin/"
+            iconLeft="bolt"
           />
           <Box
             sx={{
