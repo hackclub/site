05ab34cbc066daaba0938d60bab077e858595b1a
Sam Poder <39828164+sampoder@users.noreply.github.com>
2024-05-02 09:06:45 -0700

    Revert "Add postal code field to `/fiscal-sponsorship/apply` (#1185)"
    This reverts commit 6e3349a818fbc8e07ff92f0d3077aa5052bae8e4.

--- pages/fiscal-sponsorship/apply/index.js
+++ pages/fiscal-sponsorship/apply/index.js
@@ -23,7 +23,6 @@ export default function Apply() {
   const requiredFields = [
     'eventName',
     'eventLocation',
-    'eventPostalCode',
     'eventDescription',
     'firstName',
     'lastName',
