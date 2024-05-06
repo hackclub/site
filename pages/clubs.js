339d81b9765b3151672060b8227b38b30f98930c
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2024-05-06 10:29:33 -0400

    Update clubs.js

--- pages/clubs.js
+++ pages/clubs.js
@@ -82,7 +82,7 @@ const Page = () => (
   <>
     <Meta
       as={Head}
-      title="Clubs"
+      title="Coding / Computer Science Clubs"
       description="Hack Club is a global nonprofit network of high school makers & student-led computer science clubs where young people build the agency, the network, & the technical talent to think big & do big things in the world."
       image="https://cloud-epiki4yvg.vercel.app/2020-09-09_drbp62kayjuyyy0ek89mf9fwcp5t4kuz.jpeg"
     />
