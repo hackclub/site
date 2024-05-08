5e157010fd23c9007aad615d2ba31403c54dd7fa
24c02 <163450896+24c02@users.noreply.github.com>
2024-05-07 23:18:25 -0400

    awawawa

--- public/bin/landing-new/gambling.js
+++ public/bin/landing-new/gambling.js
@@ -1,6 +1,49 @@
 var fetchedParts;
 var selectedParts = []
 var rolled = false;
+
+
+let yap_sounds = {
+    // ty caleb!
+    thinking: [
+        new Howl({ src: '/bin/yapping/thonk1.wav' }),
+        new Howl({ src: '/bin/yapping/thonk2.wav' }),
+        new Howl({ src: '/bin/yapping/thonk3.wav' }),
+    ],
+    talking: {
+        // these sounds and most of the yapping code are adapted from https://github.com/equalo-official/animalese-generator
+        a: new Howl({ src: '/bin/yapping/a.wav' }),
+        b: new Howl({ src: '/bin/yapping/b.wav' }),
+        c: new Howl({ src: '/bin/yapping/c.wav' }),
+        d: new Howl({ src: '/bin/yapping/d.wav' }),
+        e: new Howl({ src: '/bin/yapping/e.wav' }),
+        f: new Howl({ src: '/bin/yapping/f.wav' }),
+        g: new Howl({ src: '/bin/yapping/g.wav' }),
+        h: new Howl({ src: '/bin/yapping/h.wav' }),
+        i: new Howl({ src: '/bin/yapping/i.wav' }),
+        j: new Howl({ src: '/bin/yapping/j.wav' }),
+        k: new Howl({ src: '/bin/yapping/k.wav' }),
+        l: new Howl({ src: '/bin/yapping/l.wav' }),
+        m: new Howl({ src: '/bin/yapping/m.wav' }),
+        n: new Howl({ src: '/bin/yapping/n.wav' }),
+        o: new Howl({ src: '/bin/yapping/o.wav' }),
+        p: new Howl({ src: '/bin/yapping/p.wav' }),
+        q: new Howl({ src: '/bin/yapping/q.wav' }),
+        r: new Howl({ src: '/bin/yapping/r.wav' }),
+        s: new Howl({ src: '/bin/yapping/s.wav' }),
+        t: new Howl({ src: '/bin/yapping/t.wav' }),
+        u: new Howl({ src: '/bin/yapping/u.wav' }),
+        v: new Howl({ src: '/bin/yapping/v.wav' }),
+        w: new Howl({ src: '/bin/yapping/w.wav' }),
+        x: new Howl({ src: '/bin/yapping/x.wav' }),
+        y: new Howl({ src: '/bin/yapping/y.wav' }),
+        z: new Howl({ src: '/bin/yapping/z.wav' }),
+        th: new Howl({ src: '/bin/yapping/th.wav' }),
+        sh: new Howl({ src: '/bin/yapping/sh.wav' }),
+        _: new Howl({ src: '/bin/yapping/_.wav' })
+    }
+};
+
 async function fetchParts() {
     const response = await fetch('https://hackclub.com/api/bin/wokwi/parts/');
     if (!response.ok) {
@@ -144,6 +187,7 @@ window.addEventListener("load", (e) => {
 
     document.querySelector("#generate-project-idea").addEventListener("click", async (e) => {
         document.querySelector('#project-idea').innerText = "Thinking..."
+        yap_sounds.thinking[getRandomInt(yap_sounds.thinking.length)].play();
         const res = await fetch('/api/bin/openai/', {
             method: 'POST',
             headers: {
@@ -153,5 +197,48 @@ window.addEventListener("load", (e) => {
         })
         const json = await res.json()
         document.querySelector('#project-idea').innerText = json.recommendation
+        yap(json.recommendation)
     })
 })
+
+async function yap(text) {
+    text = text.toLowerCase();
+    const yap_queue = [];
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+        try {
+            if (char === 's' && text[i + 1] === 'h') { // test for 'sh' sound
+                yap_queue.push(yap_sounds.talking['sh']);
+                continue;
+            } else if (char === 't' && text[i + 1] === 'h') { // test for 'th' sound
+                yap_queue.push(yap_sounds.talking['th']);
+                continue;
+            } else if (char === 'h' && (text[i - 1] === 's' || text[i - 1] === 't')) { // test if previous letter was 's' or 't' and current letter is 'h'
+                continue;
+            } else if (char === ',' || char === '?' || char === '.') {
+                yap_queue.push(yap_sounds.talking['_']);
+                continue;
+            } else if (char === text[i - 1]) { // skip repeat letters
+                continue;
+            }
+        } catch (e) {
+            // who cares. pick up a foot ball
+        }
+        if (!char.match(/[a-zA-Z.]/)) { 
+            continue; // skip characters that are not letters or periods
+        }
+        yap_queue.push(yap_sounds.talking[char]);
+    }
+
+    function next_yap() {
+        if (yap_queue.length === 0) return;
+        let noise = yap_queue.shift();
+        noise.rate(2 * (Math.random() * .50 + 1.9));
+        noise.once('end', next_yap)
+        console.log(noise)
+        noise.play();
+    }
+
+    next_yap();
+}
+
