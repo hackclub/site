c53b6b1a45470d7c7d77649778047985138657d1
Max Wofford <max@maxwofford.com>
2024-05-24 21:23:10 -0400

    Add data loading to page!

---
+++ pages/api/arcade/inventory.js
@@ -0,0 +1,47 @@
+import AirtablePlus from "airtable-plus"
+
+const flavorText = async () => {
+  const airtable = new AirtablePlus({
+    apiKey: process.env.AIRTABLE_API_KEY,
+    baseID: "app3ODCEuTL5iGjb3",
+    tableName: "Flavor Text"
+  })
+
+  const records = airtable.read()
+  return records
+}
+
+const inventoryParts = async () => {
+  const airtable = new AirtablePlus({
+    apiKey: process.env.AIRTABLE_API_KEY,
+    baseID: "app3ODCEuTL5iGjb3",
+    tableName: "Inventory"
+  })
+
+  const records = await airtable.read()
+  return records
+}
+
+export default async function handler(req, res) {
+  const data = {}
+  await Promise.all([
+    inventoryParts().then(d => data.inventory = d),
+    flavorText().then(d => data.flavor = d),
+  ])
+
+  const result = data.inventory.map(record => {
+    return {
+      name: record.fields['Name'],
+      hours: record.fields['Hours'],
+      imageURL: record.fields['Image URL'],
+      flavorText: record?.fields['Flavor text']?.map(recordID => {
+        return {
+          message: data.flavor.find(f => f.id == recordID).fields["Message"],
+          character: data.flavor.find(f => f.id == recordID).fields["Character"]
+        }
+      })
+    }
+  })
+
+  res.status(200).json(result)
+}
---
+++ public/arcade/data-loader.js
@@ -0,0 +1,60 @@
+let fetchedDataCache = {}
+
+async function pullFromStorage() {
+  const ts = parseInt(localStorage.getItem('fetchedDataTimestamp'))
+  if (!ts) {
+    console.log("No timestamp found in storage")
+    return null
+  }
+
+  const now = new Date().getTime()
+  const expiration = 1000 * 60 * 5 // 5 min
+  const isExpired = now - ts > expiration
+
+  if (isExpired) {
+    console.log("Data in storage is expired")
+    return null
+  }
+
+  return JSON.parse(localStorage.getItem('fetchedData'))
+}
+
+async function fetchData() {
+  return await fetch('/api/arcade/inventory').then(d => d.json())
+}
+
+async function setToStorage(data) {
+  localStorage.setItem('fetchedData', JSON.stringify(data))
+  localStorage.setItem('fetchedDataTimestamp', new Date().getTime())
+}
+
+async function fetchedDataLoader() {
+  if (Object.keys(fetchedDataCache) > 0) {
+    console.log("Found data in cache")
+    return fetchedDataCache
+  }
+  const storage = await pullFromStorage()
+  if (storage) {
+    console.log("Found parts data in storage")
+    return storage
+  }
+
+  const data = await fetchData()
+  if (data) {
+    console.log("found parts data in fetch")
+    setToStorage(data)
+    fetchedDataCache = data
+    return data
+  }
+}
+
+let fetchDataJob
+async function fetchedData() {
+  if (!fetchDataJob) {
+    fetchDataJob = fetchedDataLoader()
+  }
+  return await fetchDataJob
+}
+
+// start loading data as soon as this file loads
+fetchedData()
