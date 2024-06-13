85f71b60b3c1610a3498ecd3dc7a300fd47ca70d
Max Wofford <max@maxwofford.com>
2024-06-13 19:41:57 -0400

    Post to both arcadius & airtable!

--- pages/api/arcade/slack.js
+++ pages/api/arcade/slack.js
@@ -1,31 +1,52 @@
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-      try {
-        const data = JSON.parse(req.body);
-  
-        const response = await fetch('https://8-bit-heart.hackclub.com/slack-invite', {
-          body: JSON.stringify({
-            email: data.userEmail,
-          }),
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.SLACK_KEY}`
-          }
-        });
-  
-        if (response.ok) {
-          res.json({ status: 'Success', message: 'Invitation sent!' });
-        } else {
-          const errorData = await response.json();
-          res.status(response.status).json({ status: 'error', error: errorData });
-        }
-      } catch (error) {
-        console.error(error);
-        res.json({ status: 'Uh oh!', error });
-      }
-    } else {
-      res.status(405).json({ status: 'error', error: 'POST method required' });
+import AirtablePlus from 'airtable-plus'
+
+async function inviteToArcadius({ email }) {
+  const response = await fetch('https://arcadius.hackclub.com/slack-invite', {
+    body: JSON.stringify({
+      email
+    }),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.SLACK_KEY}`
     }
+  })
+
+  return response
+}
+async function inviteToAirtable({ email, ip }) {
+  const airtable = new AirtablePlus({
+    baseID: 'appaqcJtn33vb59Au',
+    apiKey: process.env.AIRTABLE_API_KEY,
+    tableName: 'Arcade Joins'
+  })
+  return await airtable.create({ Email: email, IP: ip })
+}
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const data = req.body
+
+      const email = data.userEmail
+      const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+
+      const result = await Promise.all([
+        inviteToArcadius({ email }),
+        inviteToAirtable({ email, ip })
+      ])
+
+      if (result[0]?.response?.ok) {
+        res.json({ status: 200, message: 'Invitation sent!' })
+      } else {
+        const errorData = await result[0]?.response?.json()
+        res.json({ status: 400, error: errorData })
+      }
+    } catch (error) {
+      console.error(error)
+      res.json({ status: 400, error })
+    }
+  } else {
+    res.json({ status: 405, error: 'POST method required' })
   }
-  
+}
