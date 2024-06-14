c89819b33dbb019d0b574cf8343ee3432037e96c
belle <bellesee1212@gmail.com>
2024-06-14 11:49:15 -0400

    responsive

--- components/arcade/join.js
+++ components/arcade/join.js
@@ -169,7 +169,7 @@ const Join = ({ fold, last, showForm, setForm, formSent, setFormSent }) => {
                   htmlFor="email"
                   sx={{
                     position: 'absolute',
-                    width: ['100%', '100%', '180%', '180%'],
+                    width: ['100%', '100%', '180%', '200%'],
                     bottom: ['-120px', '-120px', '-40px', '-40px'],
                     color: '#FF5C00',
                     opacity: 0.4
