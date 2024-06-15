b6b5f2ed7b91bda8ed9f6f4171daf319ebb2aced
Max Wofford <max@maxwofford.com>
2024-06-15 14:39:52 -0400

    Update announcement copy

--- pages/index.js
+++ pages/index.js
@@ -203,6 +203,12 @@ function Page({
             priority
             gradient="linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.45))"
           />
+	  <Announcement
+            copy="Build in public this summer!"
+            caption="Get domains, breadboards & multimeters, and drawing tablets."
+            href="/arcade/"
+            imgSrc="https://cloud-gyu8zgkki-hack-club-bot.vercel.app/0_______.png"
+          />
           <Box
             sx={{
               width: '90vw',
