c1070efa266a50022db80a1d4a05fc7cc75d0017
Greg Mat. <65409906+itsvyle@users.noreply.github.com>
2024-06-18 15:20:41 +0000

    The header should now only go to the width of the page, removing the appearance of a scrollbar on the bottom

--- pages/arcade/[userAirtableID]/shop.js
+++ pages/arcade/[userAirtableID]/shop.js
@@ -1,8 +1,8 @@
-import ShopComponent from "../../../components/arcade/shop-component"
-import { getArcadeUser } from "../../api/arcade/[userAirtableID]"
-import { shopParts } from "../../api/arcade/shop"
+import ShopComponent from '../../../components/arcade/shop-component'
+import { getArcadeUser } from '../../api/arcade/[userAirtableID]'
+import { shopParts } from '../../api/arcade/shop'
 import { Image, Link, Text } from 'theme-ui'
-import { Balancer } from "react-wrap-balancer"
+import { Balancer } from 'react-wrap-balancer'
 import Meta from '@hackclub/meta'
 import Head from 'next/head'
 
@@ -25,34 +25,53 @@ body {
 
 `
 
-export default function Shop({ availableItems, userAirtableID = null, hoursBalance = 0 }) {
-
+export default function Shop({
+  availableItems,
+  userAirtableID = null,
+  hoursBalance = 0
+}) {
   return (
     <>
-    <Meta
+      <Meta
         as={Head}
         title="Arcade Shop"
         description="Redeem prizes at your own Arcade Shop."
         image="https://cloud-luaw423i2-hack-club-bot.vercel.app/0frame_33__1_.png"
       />
-    <Balancer>
-      <h1
-        sx={{
-          textAlign: 'center',
-          fontSize: 5,
-          color: '#FF8C37',
-          my: 0,
-          pt: 5,
-          display:'block',
-          width: '100vw'
-        }}
-        className="slackey"
-      >
-        Welcome to the shop
-      </h1>
+      <style>
+        {`
+        ._title-container {
+          width: 100%;
+        }
+        `}
+      </style>
+      <Balancer className="_title-container">
+        <h1
+          sx={{
+            textAlign: 'center',
+            fontSize: 5,
+            color: '#FF8C37',
+            my: 0,
+            pt: 5,
+            display: 'block',
+            width: '100%'
+          }}
+          className="slackey"
+        >
+          Welcome to the shop
+        </h1>
       </Balancer>
-      <Text sx={{ display: 'block', textAlign: 'center', color: '#35290F' }} className='gaegu' variant='subtitle' >Your current balance is {Math.floor(hoursBalance)} 🎟️</Text>
-      <ShopComponent availableItems={availableItems} userAirtableID={userAirtableID} />
+      <Text
+        sx={{ display: 'block', textAlign: 'center', color: '#35290F' }}
+        className="gaegu"
+        variant="subtitle"
+      >
+        Your current balance is {Math.floor(hoursBalance)} 🎟️
+      </Text>
+      <ShopComponent
+        availableItems={availableItems}
+        userAirtableID={userAirtableID}
+      />
     </>
   )
 }
@@ -64,28 +83,30 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({params}) {
+export async function getStaticProps({ params }) {
   const { userAirtableID } = params
 
   const props = { userAirtableID }
 
   await Promise.all([
     shopParts().then(items => {
-      const availableItems = items.filter(item => item['Enabled']).map(item => ({
-        'Name': item['Name'] || null,
-        'Description': item['Description'] || null,
-        'Cost Hours': item['Cost Hours'] || 0,
-        id: item.id,
-        'Image URL': item['Image URL'] || null,
-        'Max Order Quantity': item['Max Order Quantity'] || 1
-      }))
+      const availableItems = items
+        .filter(item => item['Enabled'])
+        .map(item => ({
+          Name: item['Name'] || null,
+          Description: item['Description'] || null,
+          'Cost Hours': item['Cost Hours'] || 0,
+          id: item.id,
+          'Image URL': item['Image URL'] || null,
+          'Max Order Quantity': item['Max Order Quantity'] || 1
+        }))
       props.availableItems = availableItems
     }),
     getArcadeUser(userAirtableID).then(user => {
-      const hoursBalance = user.fields["Balance (Hours)"] || 0
+      const hoursBalance = user.fields['Balance (Hours)'] || 0
       props.hoursBalance = hoursBalance
     })
   ])
-  
+
   return { props }
 }
