a081186886e0a10027a280e6a12e1aa7a733f907
SkyfallWasTaken <mahadkalam1@proton.me>
2024-06-23 16:34:13 +0100

    Undo formatting in shop.js

--- pages/api/arcade/shop.js
+++ pages/api/arcade/shop.js
@@ -1,28 +1,28 @@
 import AirtablePlus from "airtable-plus"
 
 export const shopParts = async () => {
-    const airtable = new AirtablePlus({
-        apiKey: process.env.AIRTABLE_API_KEY,
-        baseID: "app4kCWulfB02bV8Q",
-        tableName: "Shop Items"
-    })
+  const airtable = new AirtablePlus({
+    apiKey: process.env.AIRTABLE_API_KEY,
+    baseID: "app4kCWulfB02bV8Q",
+    tableName: "Shop Items"
+  })
 
-    const records = await airtable.read()
-    return records.map(record => ({ id: record.id, ...record.fields }))
+  const records = await airtable.read()
+  return records.map(record => ({id: record.id, ...record.fields}))
 }
 
 export default async function handler(req, res) {
-    const data = await shopParts()
+  const data = await shopParts()
 
-    const filteredData = data.filter(record => record["Enabled"]).map(record => {
-        return {
-            name: record['Name'],
-            smallName: record['Small Name'],
-            description: record['Description'],
-            hours: record['Cost Hours'],
-            imageURL: record['Image URL'],
-        }
-    })
+  const filteredData = data.filter(record => record["Enabled"]).map(record => {
+    return {
+      name: record['Name'],
+      smallName: record['Small Name'],
+      description: record['Description'],
+      hours: record['Cost Hours'],
+      imageURL: record['Image URL'],
+    }
+  })
 
-    res.json(filteredData)
+  res.json(filteredData)
 }
