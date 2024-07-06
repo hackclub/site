ec6dbcb7fe3fa3b0af43694a7816605c4036b16d
SkyfallWasTaken <mahadkalam1@proton.me>
2024-07-06 09:34:04 +0100

    add realtime stock tracking

--- pages/api/arcade/shop.js
+++ pages/api/arcade/shop.js
@@ -1,14 +1,30 @@
 import AirtablePlus from "airtable-plus"
 
 export const shopParts = async () => {
-  const airtable = new AirtablePlus({
+  const shopItemsTable = new AirtablePlus({
     apiKey: process.env.AIRTABLE_API_KEY,
     baseID: "app4kCWulfB02bV8Q",
     tableName: "Shop Items"
   })
 
-  const records = await airtable.read()
-  return records.map(record => ({id: record.id, ...record.fields}))
+  const records = await shopItemsTable.read()
+  return records.map(record => {
+    const fields = record.fields;
+    let stock = fields["Stock"]
+
+    if (stock) {
+      // This is a limited item
+      const orders = fields["Orders"]
+      orders.forEach(order => {
+        const orderFields = order.fields;
+        const status = orderFields["Status"]
+        if (status === "Fulfilled" || status === "Awaiting Fulfillment") {
+          stock -= 1
+        }
+      })
+    }
+    return { id: record.id, ...record.fields, "Stock": stock }
+  })
 }
 
 export default async function handler(req, res) {
