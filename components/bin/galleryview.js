17c48d380e19471e918fea4376e3328256151b75
Clay Nicholson <clayanicholson@gmail.com>
2024-08-10 01:24:32 -0400

    Bin Gallery

---
+++ components/bin/galleryview.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from '../../public/bin/style/gallery.module.css'
+
+const BinPost = ({title, desc, slack, link, image}) => {
+
+  const handleClick = () => {
+    window.open("www.example.com", '_blank', 'noopener,noreferrer');
+  };
+
+return (
+    <div className={styles.gallery_card} 
+      onClick={handleClick}>
+      <h1 className={styles.card_title}>
+        {title}<br/>
+      </h1>
+      <p className={styles.card_desc}>{desc}</p>
+      <p>{slack}</p>
+
+    </div>
+  )
+}
+
+export default BinPost;
---
+++ pages/bin/gallery.js
@@ -0,0 +1,152 @@
+import React from 'react'
+import BinPost from '../../components/bin/galleryView'
+import styles from '../../public/bin/style/gallery.module.css'
+
+
+function Gallery() {
+
+   return( 
+    <section className='page'>
+        <div className={styles.background}></div>
+
+        <h1 className={styles.title}>Bin Gallery</h1>
+        <p className={styles.sub_title}>A display of all of bin's projects</p>
+
+
+    <div className={styles.feed}>
+        <BinPost 
+            title="Raspberry Pi Pico"
+            desc="This is a Raspberry Pi Pico based Temperature and Humidity display that uses a combination of a motion sensor, photo resistor and IR remote/receiver to add some helpful The motion sensor turns on the display when it detects movement, but the photo resistor makes sure it stays off during the night unless you use the IR remote to turn it on in the dark. This way, it won't disrupt your sleep or work at night with accidental triggers of the motion sensor!"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+<BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+
+        <BinPost 
+            title="title"
+            desc="lore ipsum"
+            slack="@CAN"
+            link="https://www.google.com"
+            image="https://www.google.com"
+        />
+    </div>
+      
+    </section>
+   )
+}
+
+export default Gallery
