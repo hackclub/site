b9790f1ef731027f08defb1f1f31fd5102e758ba
Clay Nicholson <clayanicholson@gmail.com>
2024-08-15 15:40:55 -0400

    Added gallery

--- pages/arcade/gallery.js
+++ pages/arcade/gallery.js
@@ -1,19 +1,129 @@
 import React from 'react'
 import Nav from '../../components/Nav'
-import Footer from '../../components/Footer'
+import Footer from '../../components/arcade/Footer'
 import BGImg from '../../components/background-image'
-import AssembleImgFile from '../../public/home/assemble.jpg'
+import background from '../../public/home/assemble.jpg'
+import { Badge, Box, Button, Card, Container, Grid, Heading, Link, Text } from 'theme-ui'
+import SlideDown from '../../components/slide-down'
+import Post from '../../components/arcade/post'
+import styles from '../../components/arcade/post.module.css'
+
+export async function getStaticProps() {
+    const host = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://hackclub.com';
+    const res = await fetch(`${host}/api/arcade/gallery`);
+    const posts = await res.json();
+    
+    const filteredPosts = posts;
+  
+    return {
+      props: { posts: filteredPosts, 
+      },
+    };
+  }
 
 
-const gallery = () => {
+const gallery = ({ posts }) => {
+    console.log(posts);
   return (
     <section>
         <Nav />
         <BGImg 
-            src={AssembleImgFile}
-            alt="Hack Clubbers assemble at Figma HQ for the first IRL hackathon in SF since 2020: Assemble. 📸 Photo by Kunal Botla, Hack Clubber in MA!"
+            src={background}
+            alt="Arcade Gallery BG Img"
             priority
         />
+
+
+        <SlideDown duration={768}>
+        <Heading
+          as="h1"
+          variant="ultratitle"
+          sx={{
+            color: 'white',
+            textShadow: 'text',
+            filter: 'drop-shadow(0 -2px 4px rgba(0,0,0,0.5))',
+            WebkitFilter: 'drop-shadow(0 -2px 4px rgba(0,0,0,0.5))',
+            maxWidth: [null, 'copyUltra'],
+            my: [3, 4],
+            mx: 'auto',
+            zIndex: 1
+          }}
+        >
+          <Text
+            as="span"
+            sx={{
+              WebkitTextStroke: 'currentColor',
+              WebkitTextStrokeWidth: ['2px', '3px'],
+              WebkitTextFillColor: 'transparent'
+            }}
+          >
+            Arcade Gallery
+          </Text>
+          <br />
+        <Button
+          as="a"
+          variant="ctaLg"
+          href="https://apply.hackclub.com"
+          target="_blank"
+          rel="noopener"
+        >
+          Add a Project
+        </Button>
+        </Heading>
+      </SlideDown>
+
+        <div className={styles.feed}>
+            {posts.map(post => {
+                return (
+                <Post
+                    id={post.id}
+                    title={post.name}
+                    desc={post.desc}
+                    slack={post.slack}
+                    codeLink={post.codeLink}
+                    playable={post.playable}
+                    images={post.images}
+                    githubProf={""}
+                />)
+
+            })}
+            <Post
+                title="My project"
+
+                />
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+        </div>      
         <Footer />
 
     </section>
