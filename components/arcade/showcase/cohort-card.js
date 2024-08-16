0f61eefff5e689937c65b1815b6c746ceb78fe3f
Max Wofford <max@maxwofford.com>
2024-08-16 18:26:34 -0400

    Switch to next link for cohort cards

--- components/arcade/showcase/cohort-card.js
+++ components/arcade/showcase/cohort-card.js
@@ -8,14 +8,14 @@ const CohortCard = ({ id, title = "Title Not Found", desc = "Description Not Fou
   console.log(images)
   return (
     <>
-      <a href={`/arcade/showcase/project/${id}`} className={styles.linkWrapper} target="_blank" rel="noopener noreferrer">
+      <Link href={`/arcade/showcase/project/${id}`} className={styles.linkWrapper} rel="noopener noreferrer">
         <div className={styles.card}>
           <img src={images ? (images[0]) : ("https://img.buzzfeed.com/buzzfeed-static/static/2020-05/21/17/asset/19f3032de0de/sub-buzz-1010-1590082675-7.png")} alt="Project Image" className={styles.card_img} />
           <h1 className={styles.card_title}>{title}</h1>
           <p className={styles.card_description}>{desc}</p>
 
         </div>
-      </a>
+      </Link>
     </>
   )
 }
