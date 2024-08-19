4c5ab9f3584ebd4d0b307db3758f94af2a215cb3
Clay Nicholson <clayanicholson@gmail.com>
2024-08-19 15:47:43 -0400

    Added colored cards

--- pages/arcade/showcase/my.js
+++ pages/arcade/showcase/my.js
@@ -109,6 +109,8 @@ const ProjectGallery = ({ projects, loadProjects }) => {
           imageLink={project.imageLink}
           personal={true}
           reload={loadProjects}
+          color={project.color}
+          textColor={project.textColor}
         />
       ))}
     </div>
