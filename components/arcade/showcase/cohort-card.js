3551b9e04c5ea667c331ca16f67864e945f0c3c4
Max Wofford <max@maxwofford.com>
2024-08-20 16:56:15 -0400

    Fix cohort card deletion bug & run formatter

--- components/arcade/showcase/cohort-card.js
+++ components/arcade/showcase/cohort-card.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useRef } from 'react'
 import { Text, Close } from 'theme-ui'
 import styles from './cohort-card.module.css'
 import { useState } from 'react'
@@ -13,13 +13,12 @@ const CohortCard = ({
   desc = 'Description Not Found',
   imageLink = '',
   personal = false,
-  reload,
   color = '#09AFB4',
-  textColor="#000000"
+  textColor = '#000000'
 }) => {
   const [isHovered, setIsHovered] = useState(false)
-  const [isVisible, setIsVisible] = useState(true);
-
+  const [isDeleted, setIsDeleted] = useState(false)
+  const modalRef = useRef()
 
   async function handleDelete() {
     try {
@@ -34,10 +33,8 @@ const CohortCard = ({
         {
           method: 'PATCH',
           headers: {
-            Authorization: `Bearer ${authToken}`,
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({}) // Empty body since your API expects some body content
+            Authorization: `Bearer ${authToken}`
+          }
         }
       )
 
@@ -51,149 +48,136 @@ const CohortCard = ({
     } catch (error) {
       console.error('Error deleting project:', error)
     }
-    }
+  }
 
   const firstImage = imageLink || randomNotFoundImg(id)
+  const editLink = `/arcade/showcase/project/${id}/edit`
 
-  function red() {
-    window.location.href = '/arcade/showcase/project/' + id + '/edit'
+  if (isDeleted) {
+    return null
   }
 
   return (
     <>
-    {isVisible?  (<div
-      sx={{
-        backgroundColor: color,
-      }}
-      className={styles.card}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      {/* attempt to make blob... not working rn */}
-      {/* <div sx={{
-        background: 'black',
-        clipPath: `
-    path('M36.8,-68.8C45.8,-58.6,49.9,-44.9,51.7,-32.8C53.5,-20.8,53.1,-10.4,56.4,1.9C59.6,14.2,66.6,28.3,63.8,38.6C60.9,48.9,48.3,55.3,36,62.2C23.8,69.1,11.9,76.4,-1.7,79.4C-15.3,82.3,-30.7,81,-38.4,71.6C-46.2,62.1,-46.5,44.5,-54,31.3C-61.4,18,-76.2,9,-78.4,-1.3C-80.6,-11.5,-70.2,-23.1,-63.1,-36.9C-55.9,-50.7,-51.9,-66.8,-41.9,-76.4C-31.9,-86.1,-15.9,-89.3,-1,-87.6C13.9,-85.8,27.8,-79.1,36.8,-68.8Z')
-  `,
-            width: '130%', 
-            height: '400px',
-            transform: "translate(-50%, -50%) scale(1.5)",
-      }}>
-
-        </div> */}
-      {personal && isHovered && (
-        <div
-          sx={{
-            position: 'absolute',
-            top: '10px',
-            left: '10px',
-            display: 'flex',
-            gap: '5px'
-          }}
-        >
-          <div
-            as="a"
-            onClick={() => red()}
-            sx={{
-              color: 'white',
-              bg: '#09AFB4',
-              borderRadius: '10px',
-              height: '32px',
-              cursor: 'pointer',
-              transitionDuration: '0.4s',
-              '&:hover': {
-                transform: 'scale(1.15)'
-              }
-            }}
-          >
-            <Icon glyph="edit" />{' '}
-          </div>
-          <div
-            onClick={e => {
-              document.getElementById('delete-project').showModal()
-            }}
-            sx={{
-              color: 'white',
-              bg: '#09AFB4',
-              borderRadius: '10px',
-              height: '32px',
-              cursor: 'pointer',
-              transitionDuration: '0.4s',
-              '&:hover': {
-                transform: 'scale(1.15)'
-              }
-            }}
-          >
-            <Icon glyph="delete" />{' '}
-          </div>
-        </div>
-      )}
-      <a
-        href={`/arcade/showcase/project/${id}`}
-        className={styles.linkWrapper}
-        target="_blank"
-        rel="noopener noreferrer"
+      <div
+        sx={{
+          backgroundColor: color
+        }}
+        className={styles.card}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       >
-        <img
-          src={firstImage}
-          alt="Project Image"
-          className={styles.card_img}
-        />
-        <h1 
-          sx={{color: textColor}}
-          className={styles.card_title}>
+        {personal && isHovered && (
+          <div
+            sx={{
+              position: 'absolute',
+              top: '10px',
+              left: '10px',
+              display: 'flex',
+              gap: '5px'
+            }}
+          >
+            <div
+              as="a"
+              href={editLink}
+              sx={{
+                color: 'white',
+                bg: '#09AFB4',
+                borderRadius: '10px',
+                height: '32px',
+                cursor: 'pointer',
+                transitionDuration: '0.4s',
+                '&:hover': {
+                  transform: 'scale(1.15)'
+                }
+              }}
+            >
+              <Icon glyph="edit" />{' '}
+            </div>
+            <div
+              onClick={e => {
+                modalRef.current?.showModal()
+              }}
+              sx={{
+                color: 'white',
+                bg: '#09AFB4',
+                borderRadius: '10px',
+                height: '32px',
+                cursor: 'pointer',
+                transitionDuration: '0.4s',
+                '&:hover': {
+                  transform: 'scale(1.15)'
+                }
+              }}
+            >
+              <Icon glyph="delete" />{' '}
+            </div>
+          </div>
+        )}
+        <a
+          href={`/arcade/showcase/project/${id}`}
+          className={styles.linkWrapper}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={firstImage}
+            alt="Project Image"
+            className={styles.card_img}
+          />
+          <h1 sx={{ color: textColor }} className={styles.card_title}>
             {title}
-        </h1>
+          </h1>
 
-        <p sx={{color: textColor}} className={styles.card_description}>{desc}</p>
-      </a>
-      <dialog
-        id="delete-project"
-        sx={{ borderRadius: '10px', border: '3px dashed #09AFB4' }}
-        className="gaegu"
-      >
-        <Text>Are you sure you want to delete this project?</Text>
-        <br />
-        <Button
-          sx={{
-            backgroundColor: '#FF5C00',
-            color: '#FAEFD6',
-            borderRadius: '5px',
-            border: 'none',
-            px: '20px',
-            transitionDuration: '0.3s',
-            '&:hover': {
-              transform: 'scale(1.05)'
-            },
-            width: 'fit-content'
-          }}
-          onClick={e => {
-            setIsVisible(false)
-            document.getElementById('delete-project').close()
-            handleDelete()
-          }}
+          <p sx={{ color: textColor }} className={styles.card_description}>
+            {desc}
+          </p>
+        </a>
+
+        <dialog
+          ref={modalRef}
+          sx={{ borderRadius: '10px', border: '3px dashed #09AFB4' }}
+          className="gaegu"
         >
-          Yes
-        </Button>
-        <Close
-          sx={{
-            '&:hover': { cursor: 'pointer' },
-            position: 'absolute',
-            top: '10px',
-            right: '10px',
-            zIndex: 2,
-            color: '#09AFB4'
-          }}
-          onClick={e => {
-            document.getElementById('delete-project').close()
-          }}
-        />
-      </dialog>
-    </div>) : null
-    }
+          <Text>Are you sure you want to delete this project?</Text>
+          <br />
+          <Button
+            sx={{
+              backgroundColor: '#FF5C00',
+              color: '#FAEFD6',
+              borderRadius: '5px',
+              border: 'none',
+              px: '20px',
+              transitionDuration: '0.3s',
+              '&:hover': {
+                transform: 'scale(1.05)'
+              },
+              width: 'fit-content'
+            }}
+            onClick={e => {
+              modalRef.current?.close()
+              setIsDeleted(true)
+              handleDelete()
+            }}
+          >
+            Yes
+          </Button>
+          <Close
+            sx={{
+              '&:hover': { cursor: 'pointer' },
+              position: 'absolute',
+              top: '10px',
+              right: '10px',
+              zIndex: 2,
+              color: '#09AFB4'
+            }}
+            onClick={e => {
+              modalRef.current?.close()
+            }}
+          />
+        </dialog>
+      </div>
     </>
-    
-    
   )
 }
 
