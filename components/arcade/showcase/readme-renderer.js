2fcfc03766b43ecb60aa9987b6db9b6dc6ffbe16
Max Wofford <max@maxwofford.com>
2024-08-20 12:26:46 -0400

    Allow rendering sanitized HTML tags in arcade showcase

--- components/arcade/showcase/readme-renderer.js
+++ components/arcade/showcase/readme-renderer.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
+import rehypeSanitize from 'rehype-sanitize'
 import style from './readme-renderer.module.css'
 
 const ReadmeRenderer = ({ markdown }) => {
@@ -7,6 +9,7 @@ const ReadmeRenderer = ({ markdown }) => {
     <ReactMarkdown
       className={style.reactMarkDown}
       remarkPlugins={[remarkGfm]}
+      rehypePlugins={[rehypeRaw, rehypeSanitize]}
       linkTarget={'_blank'}
       >
       {markdown}
