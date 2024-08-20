3a70545f3f4e119cc6d9511aad212e1d1bce183a
Max Wofford <max@maxwofford.com>
2024-08-20 15:54:54 -0400

    Speed up page loadtime by indexing authtoken

--- pages/api/arcade/showcase/projects/my.js
+++ pages/api/arcade/showcase/projects/my.js
@@ -1,21 +1,46 @@
-import AirtablePlus from "airtable-plus";
-import { ensureAuthed } from "../login/test";
+import AirtablePlus from 'airtable-plus'
+import { ensureAuthed } from '../login/test'
+
+const airtable = new AirtablePlus({
+  apiKey: process.env.AIRTABLE_API_KEY,
+  baseID: 'app4kCWulfB02bV8Q',
+  tableName: 'Showcase'
+})
+
+const getProjects = async function (authToken) {
+  if (!authToken || authToken === '') {
+    return []
+  }
+  const records = await airtable.read({
+    filterByFormula: `AND({Creator Auth Token} = '${authToken}', NOT({deleted}))`,
+    fields: [
+      'Name',
+      'Description',
+      'Slack Link',
+      'Code Link',
+      'Play Link',
+      'ScreenshotLink',
+      'color',
+      'textColor'
+    ]
+  })
+
+  return records
+}
 
 export default async function handler(req, res) {
-  const user = await ensureAuthed(req)
+  const authToken = req.headers.authorization
+  const safeAuthToken = authToken
+    .replace(/Bearer /, '')
+    .replace(/[^a-zA-Z0-9-]/g, '')
+
+  const [user, projects] = await Promise.all([
+    ensureAuthed(req),
+    getProjects(safeAuthToken)
+  ])
   if (user.error) {
     return res.status(401).json(user)
   }
-
-  const airtable = new AirtablePlus({
-    apiKey: process.env.AIRTABLE_API_KEY,
-    baseID: 'app4kCWulfB02bV8Q',
-    tableName: "Showcase"
-  })
-
-  const projects = await airtable.read({
-    filterByFormula: `AND({User} = '${user.fields['Name']}', NOT({deleted}))`
-  })
 
   const results = projects.map(p => ({
     id: p.id,
@@ -25,12 +50,10 @@ export default async function handler(req, res) {
     codeLink: p.fields['Code Link'] || '',
     slackLink: p.fields['Slack Link'] || '',
     playLink: p.fields['Play Link'] || '',
-    // images: (p.fields['Screenshot'] || []).map(i => i.url),
     imageLink: p.fields['ScreenshotLink'] || '',
-    githubProf: p.fields['Github Profile'] || '',
-    user: user.fields['Name'], 
+    user: user.fields['Name'],
     color: p.fields['color'] || '',
     textColor: p.fields['textColor'] || ''
   }))
-  return res.status(200).json({ projects: results, name: user.fields['Name']})
+  return res.status(200).json({ projects: results, name: user.fields['Name'] })
 }
