deabd4f913af9641e049a8577e59266acdee86d4
vic <74506415+v1ctorio@users.noreply.github.com>
2024-08-22 18:21:39 +0200

    Variable name back

--- components/arcade/showcase/project-view.js
+++ components/arcade/showcase/project-view.js
@@ -41,7 +41,7 @@ const ProjectView = ({
   description = 'Description Not Found',
   slack = 'Slack Not Found',
   scrapbook = '',
-  playLink: TryNowLink,
+  playLink,
   images = [],
   githubProf,
   user = 'User Not Found',
@@ -134,7 +134,7 @@ const ProjectView = ({
           className={styles.buttonGroup}
           sx={{ width: '90%', margin: 'auto', pt: 2, pb: 2 }}
         >
-          {TryNowLink && (
+          {playLink && (
             <Button
               as="a"
               sx={{
@@ -143,7 +143,7 @@ const ProjectView = ({
                 textSizeAdjust: '16px',
                 borderRadius: '10px'
               }}
-              href={TryNowLink}
+              href={playLink}
               target="_blank"
               rel="noopener"
             >
