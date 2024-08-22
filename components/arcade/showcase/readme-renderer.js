22f2a8622ff36b61a237a82e2fe5d68df524727f
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-21 20:45:57 -0400

    feat: add syntax highlighting with react-syntax-highlighter

--- components/arcade/showcase/readme-renderer.js
+++ components/arcade/showcase/readme-renderer.js
@@ -4,6 +4,9 @@ import rehypeRaw from 'rehype-raw'
 import rehypeSanitize from 'rehype-sanitize'
 import style from './readme-renderer.module.css'
 
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { solarizedlight } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
 const ReadmeRenderer = ({ markdown }) => {
   return (
     <ReactMarkdown
@@ -11,7 +14,27 @@ const ReadmeRenderer = ({ markdown }) => {
       remarkPlugins={[remarkGfm]}
       rehypePlugins={[rehypeRaw, rehypeSanitize]}
       linkTarget={'_blank'}
-      >
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '')
+
+          return !inline && match ? (
+            <SyntaxHighlighter
+              style={solarizedlight}
+              PreTag="div"
+              language={match[1]}
+              {...props}
+            >
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          )
+        }
+      }}
+    >
       {markdown}
     </ReactMarkdown>
   )
