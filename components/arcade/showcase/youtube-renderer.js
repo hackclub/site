88dc7ff0d0e7dde6e68e9c427c3c08a66cc00e40
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-08-25 00:16:27 -0500

    Update YoutubeRenderer component to fallback to playing from URL

--- components/arcade/showcase/youtube-renderer.js
+++ components/arcade/showcase/youtube-renderer.js
@@ -2,17 +2,28 @@ import LiteYouTubeEmbed from 'react-lite-youtube-embed';
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 
 const YoutubeRenderer = ({ youtubeLink }) => {
-  if (!youtubeLink) return null
-  const youtubeID = youtubeLink.split('v=')[1]
-  if (!youtubeID) return <p>Invalid YouTube link: "{youtubeLink}"</p>
+  if (!youtubeLink) return null;
 
+  const isYouTubeLink = youtubeLink.includes('youtube.com') || youtubeLink.includes('youtu.be');
+  if (isYouTubeLink) {
+    const youtubeID = youtubeLink.split('v=')[1];
+    if (!youtubeID) return <p>Invalid YouTube link: "{youtubeLink}"</p>;
+
+    return (
+      <LiteYouTubeEmbed
+        id={youtubeID}
+        adNetwork={false}
+        noCookie={true}
+      />
+    );
+  }
   return (
-    <LiteYouTubeEmbed
-      id={youtubeID}
-      adNetwork={false}
-      noCookie={true}
-    />
-  )
-}
+    <video controls>
+      <source src={youtubeLink} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  );
+};
+
 
 export default YoutubeRenderer
