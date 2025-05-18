06bc2c47230f42a11626e8a35a0ac3aa380b4b27
Anish <22670462+anishanne@users.noreply.github.com>
2025-05-18 07:55:44 -0700

    Add tub_program support (#1504)

--- pages/api/fiscal-sponsorship/apply.js
+++ pages/api/fiscal-sponsorship/apply.js
@@ -7,6 +7,8 @@ const applicationsTable = new AirtablePlus({
   tableName: 'Events'
 })
 
+const tub_programs = ['ftcscout', 'GFGS'] // Pre-approved TUB programs
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).json({ status: 'error', error: 'Must send POST request' })
@@ -62,7 +64,9 @@ export default async function handler(req, res) {
         '(Adults) Annual Budget': parseInt(data.eventAnnualBudget),
         'HCB ID': r.id,
         'Referral Code': data.referralCode,
-        Tubs: data.referralCode ? 'GFGS' : undefined
+        Tubs: tub_programs.includes(data.tub_program)
+          ? data.tub_program
+          : undefined
       })
       res.status(200).end()
     })
