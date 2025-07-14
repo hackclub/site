0b92e008ea3af8272db57bb58ce72c3ad841f107
phoebe <phoebe.contactme@protonmail.com>
2025-07-14 14:20:24 -0400

    Remove renderer

--- components/arcade/showcase/youtube-renderer.js
+++ components/arcade/showcase/youtube-renderer.js
@@ -1,4 +1,4 @@
-import LiteYouTubeEmbed from 'react-lite-youtube-embed';
+import {LiteYouTubeEmbed} from 'react-lite-youtube-embed';
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 
 const YoutubeRenderer = ({ youtubeLink }) => {
