a9b091fa9dce19dd241c62a60a1435cde8046d34
Clay Nicholson <82461712+claynicholson@users.noreply.github.com>
2025-10-03 14:03:49 -0400

    Sorry Zach

--- pages/index.js
+++ pages/index.js
@@ -210,7 +210,7 @@ function Page({
           <Announcement
             width="90vw"
             copy="Get up to $400 to make a hardware project!"
-            caption="Design a project and get a grant to make it real with Blueprint, Hackclub's largest hardware program"
+            caption="Design a project and get a grant to make it real with Blueprint, Hack Club's largest hardware program"
             href="https://blueprint.hackclub.com/?ref=site-announcement"
             imgSrc="https://hc-cdn.hel1.your-objectstorage.com/s/v3/db8d0fd43bb664a8b07431b0262a7ca13c1602c7_blueprint_logo__img_.png"/>
           <Box
