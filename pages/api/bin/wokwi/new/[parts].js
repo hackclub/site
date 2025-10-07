bc44243ea86ea12edbf5cb2e90e45c2dd41a24b3
Elijah <78573365+elijahrs-gh@users.noreply.github.com>
2025-10-07 12:52:25 -0500

    Delete pages directory

--- pages/api/bin/wokwi/new/[parts].js
+++
@@ -1,7 +0,0 @@
-import { findOrCreateProject } from "."
-
-export default async function handler(req, res) {
-  const parts = req.query.parts.split('|')
-  const shareLink = await findOrCreateProject(parts)
-  res.redirect(shareLink)
-}
