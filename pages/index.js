b2b9136b38005d06305c0c888352c69fd3a95504
Hafsa <116444403+slippyishappy@users.noreply.github.com>
2025-10-10 16:49:04 +0100

    Added moonshot card

--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,8 @@ import Haxidraw from '../components/index/cards/haxidraw'
 import Onboard from '../components/index/cards/onboard'
 import Blueprint from '../components/index/cards/blueprint'
 import Athena from '../components/index/cards/athena'
+
+import Moonshot from '../components/index/cards/moonshot'
 import Highway from '../components/index/cards/highway'
 import Shipwrecked from '../components/index/cards/shipwrecked'
 /** @jsxImportSource theme-ui */
@@ -731,6 +733,7 @@ function Page({
               blueprintData={blueprintData}
               stars={stars.onboard.stargazerCount}
             />
+            <Moonshot />
             <Athena />
             <Slack slackKey={slackKey} data={slackData} events={events} />
             <Pizza />
