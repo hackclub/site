520f0ed53b8c0e733131fbf522decfe1c091e6ef
rluo.dev <34289582+rluodev@users.noreply.github.com>
2025-12-02 17:54:12 -0800

    [HCB] Get ready for mobile app launch! (#1720)
    Co-authored-by: Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
    Co-authored-by: Mohamad Mortada <mmortada685@gmail.com>
    Co-authored-by: Gary Tou <gary@garytou.com>
    Co-authored-by: polypixeldev <me@sfernandez.dev>

---
+++ pages/fiscal-sponsorship/mobile/index.js
@@ -0,0 +1,102 @@
+import { Box, Container, Heading } from 'theme-ui'
+import {
+  PillHolder,
+  AuthorPill,
+  DatePill
+} from '../../../components/announcements/pills'
+import Head from 'next/head'
+import NextLink from 'next/link'
+import styled from '@emotion/styled'
+import theme from '../../../lib/theme'
+import Meta from '@hackclub/meta'
+import Nav from '../../../components/nav'
+import ForceTheme from '../../../components/force-theme'
+import Footer from '../../../components/footer'
+import Copy from '../../../components/announcements/hcb-mobile.mdx'
+import HCBCTA from '../../../components/announcements/hcb_cta'
+import AnnouncementHolder from '../../../components/announcements/holder'
+import Balancer from 'react-wrap-balancer'
+
+const StyledLink = styled.a`
+  text-decoration: underline;
+  color: ${theme.colors.white};
+`
+
+const Link = props => {
+  const { href } = props
+  return (
+    <NextLink href={href} passHref>
+      <StyledLink>{props.children}</StyledLink>
+    </NextLink>
+  )
+}
+
+const MobilePage = () => (
+  <>
+    <Meta
+      as={Head}
+      title="HCB Mobile is here!"
+      description="Manage your HCB organizations on the go. Issue cards, view transactions, and more!"
+      image="https://hc-cdn.hel1.your-objectstorage.com/s/v3/9fc32ee8cbc1a6a1ce93609b9822d6bae394dc7a_opengraph.png"
+    />
+    <ForceTheme theme="light" />
+    <Nav />
+    <Box
+      as="section"
+      sx={{
+        pt: [5, 6],
+        pb: [4, 5],
+        bg: 'rgb(104, 41, 205)',
+        backgroundImage: theme => theme.util.gx('purple', 'orange')
+      }}
+    >
+      <Container sx={{ textAlign: 'center', color: 'white' }}>
+        <Heading
+          as="h1"
+          variant="title"
+          sx={{
+            fontSize: [5, 6, null, 7],
+            span: {
+              WebkitTextStroke: 'currentColor',
+              WebkitTextStrokeWidth: ['2px', '3px'],
+              WebkitTextFillColor: 'transparent'
+            }
+          }}
+        >
+          <Link href="/fiscal-sponsorship">HCB Mobile</Link> is here!
+        </Heading>
+        <Balancer>
+          <Heading
+            as="h2"
+            variant="title"
+            sx={{
+              fontSize: [3, 4, 4, 4],
+              fontWeight: 400,
+              marginTop: '24px',
+              maxWidth: '900px',
+              mx: 'auto'
+            }}
+          >
+            Manage your HCB organizations on the go. Issue cards, view
+            transactions, and more!
+          </Heading>
+        </Balancer>
+      </Container>
+    </Box>
+    <AnnouncementHolder>
+      <PillHolder>
+        <AuthorPill
+          firstName="Mohamad"
+          tag="Mohamad Mortada"
+          image="https://github.com/thedev132.png"
+        />
+        <DatePill tag="Dec 2, 2025" />
+      </PillHolder>
+      <Copy />
+    </AnnouncementHolder>
+    <HCBCTA />
+    <Footer />
+  </>
+)
+
+export default MobilePage
