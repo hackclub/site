1eccd0ec110ab4adacec72cacad4312fa2fddae9
PianoMan0 <180116946+PianoMan0@users.noreply.github.com>
2025-12-19 13:39:45 -0500

    Remove Pizza component from index page

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ import ForceTheme from '../components/force-theme'
 import Footer from '../components/footer'
 import Stage from '../components/stage'
 import Carousel from '../components/index/carousel'
-import Pizza from '../components/index/cards/pizza'
 import Sprig from '../components/index/cards/sprig'
 import Sinerider from '../components/index/cards/sinerider'
 import SprigConsole from '../components/index/cards/sprig-console'
@@ -842,7 +841,6 @@ function Page({
               stars={stars.onboard.stargazerCount}
             />
             <Slack slackKey={slackKey} data={slackData} events={events} />
-            <Pizza />
           </Box>
         </Box>
         <Box>
