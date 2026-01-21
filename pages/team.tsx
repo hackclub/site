c808095be4c62661d389c19b75eaed0ac692fc18
Celeste Roselli <72556836+celesteroselli@users.noreply.github.com>
2026-01-21 14:25:04 -0500

    more photos

--- pages/team.tsx
+++ pages/team.tsx
@@ -434,7 +434,7 @@ email="zach"
                         as="h4"
                         sx={{ fontSize: 3 }}
                       >
-                        Teen Contractors:
+                        Teen Contractors/Volunteers:
                       </Text>
                       <Grid columns={[1, null, 2]} gap={3} m={10}>
                         {team.current
@@ -523,7 +523,7 @@ email="zach"
                         as="h4"
                         sx={{ fontSize: 3 }}
                       >
-                        Teen Contractors:
+                        Teen Contractors/Volunteers:
                       </Text>
                       <Grid columns={[1, null, 2]} gap={3} m={10}>
                         {team.current
@@ -612,7 +612,7 @@ email="zach"
                         as="h4"
                         sx={{ fontSize: 3 }}
                       >
-                        Teen Contractors:
+                        Teen Contractors/Volunteers:
                       </Text>
                       <Grid columns={[1, null, 2]} gap={3} m={10}>
                         {team.current
