6e9cc0d1e5d4ab22a78bd47568f604194152c514
Tanishq Goyal <146565444+TaniWanKenobi@users.noreply.github.com>
2026-03-02 10:57:06 -0500

    Remove Flavortown link from navigation
    Removed the Flavortown link from the navigation.

--- components/nav.js
+++ components/nav.js
@@ -147,7 +147,6 @@ const Navigation = props => (
     <NextLink href="https://toolbox.hackclub.com/" passHref>
       <Link>Toolbox</Link>
     </NextLink>
-    <Link href="https://flavortown.hackclub.com/?ref=site-nav">Flavortown</Link>
     <NextLink href="/philanthropy" passHref>
       <Link>Donors</Link>
     </NextLink>
