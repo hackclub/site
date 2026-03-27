89cdd689c7fab53a3aca6f88a0fb801528ed1b30
Mat Manna <91392083+matmanna@users.noreply.github.com>
2026-03-27 13:00:45 +0000

    more formatting

--- components/footer.tsx
+++ components/footer.tsx
@@ -114,9 +114,12 @@ const Footer = ({
         <Box sx={{ gridColumn: ["span 2", "span 1"] }}>
           <Box sx={{ display: "flex", alignItems: "end", flexDirection: "row" }}>
             <Logo aria-label="Hack Club logo" width={128} height={45} />
-            <br />
             <Text as="span" color="muted" sx={{ marginBottom: '-.5em' }}>
-              <Link sx={{ fontSize: 'inherit' }} href={`https://github.com/hackclub/site/commit/${getGitSha()}`} target="_blank" rel="noopener noreferrer"><code style={{ fontFamily: 'monospace', fontSize: '13px' }}>{getGitShaShort()}</code></Link>
+              <Link sx={{ fontSize: 'inherit' }} href={`https://github.com/hackclub/site/commit/${getGitSha()}`} target="_blank" rel="noopener noreferrer">
+                <code style={{ fontFamily: 'monospace', fontSize: '13px' }}>
+                  {getGitShaShort()}
+                </code>
+              </Link>
             </Text>
           </Box>
           <Grid
