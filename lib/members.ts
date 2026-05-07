49b38f74f7a5637c1aa70d0651dcd8125a3524be
Wally <wallybensallam@gmail.com>
2026-05-07 14:31:52 +0100

    Update count value to 112858

--- lib/members.ts
+++ lib/members.ts
@@ -1,4 +1,4 @@
 // this could use the slackData lib, but apparently top level awaits are risky
-export const count: number = 103897
+export const count: number = 112858
 export const formatted = count.toLocaleString('en-US')
 export const thousands = Math.round(count / 1000)
