b7abc7a3cb40675c30571b2d1ee4e2c5b8ca4d44
yodalightsabr <yoda@yodacode.xyz>
2026-05-07 19:27:03 -0700

    Adjust padding in IssuingNotice component
    Reduced vertical padding in the IssuingNotice component.

--- components/fiscal-sponsorship/issuing.tsx
+++ components/fiscal-sponsorship/issuing.tsx
@@ -2,7 +2,7 @@ import { Container, Text } from 'theme-ui'
 
 export default function IssuingNotice({ sx }) {
   return (
-    <Container sx={{ py: 4, px: 3, textAlign: 'center', bg: 'sunken', ...sx }}>
+    <Container sx={{ py: 3, px: 3, textAlign: 'center', bg: 'sunken', ...sx }}>
       {/* Stripe requires this to be present on every page that mentions our card program */}
 
       <Text variant="caption" sx={{ color: 'muted' }}>
