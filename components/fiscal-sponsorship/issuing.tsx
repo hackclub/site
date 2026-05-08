4778528aa0ce1f0857f70f099bbbf1625c7b8df7
yodalightsabr <yoda@yodacode.xyz>
2026-05-07 19:25:21 -0700

    Stripe issuing notice

---
+++ components/fiscal-sponsorship/issuing.tsx
@@ -0,0 +1,14 @@
+import { Container, Text } from 'theme-ui'
+
+export default function IssuingNotice({ sx }) {
+  return (
+    <Container sx={{ py: 4, px: 3, textAlign: 'center', bg: 'sunken', ...sx }}>
+      {/* Stripe requires this to be present on every page that mentions our card program */}
+
+      <Text variant="caption" sx={{ color: 'muted' }}>
+        HCB Visa® Commercial cards are powered by Stripe and issued by Celtic
+        Bank.
+      </Text>
+    </Container>
+  )
+}
