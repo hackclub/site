6f0ffc293cad176c37c712320adb94c17160f4dc
Echo <github@3kh0.net>
2026-05-22 16:06:20 -0400

    404 fallbacks

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const h = new Headers(request.headers);
+  h.set("x-pathname", request.nextUrl.pathname);
+
+  return NextResponse.next({
+    request: { headers: h },
+  });
+}
+
+export const config = {
+  matcher: [
+    // 404 matching on valid paths
+    "/((?!_next/|api/|.*\\.[^/]+$).*)",
+  ],
+};
