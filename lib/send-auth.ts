acae081310244e4791eccf972a4333ea12b28b03
Echo <github@3kh0.net>
2026-07-23 15:17:07 -0400

    fix redirect

--- lib/send-auth.ts
+++ lib/send-auth.ts
@@ -1,5 +1,5 @@
 export function getAuthUrl(email?: string) {
-  const url = new URL("https://slack.hackclub.com/slides/1");
+  const url = new URL("https://slack.hackclub.com/slides/0");
   const t = email?.trim();
   if (t) url.searchParams.set("email", t);
   return url.toString();
